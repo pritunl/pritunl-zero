ae99e52d7e05e5cf629aa85fb92bb047a185bc42
Zachary Huff <zach.huff.386@gmail.com>
2017-06-21 18:07:58 -0400

    Update constants

--- constants/constants.go
+++ constants/constants.go
@@ -5,13 +5,15 @@ import (
 )
 
 const (
-	Version           = "1.0.0"
-	ConfPath          = "/etc/pritunl_zero.json"
-	LogPath           = "/var/log/pritunl_zero.log"
-	Production        = false
-	BuildTest         = false
-	StaticRoot        = "www/dist"
-	StaticTestingRoot = "www"
-	StaticCache       = false
-	RetryDelay        = 3 * time.Second
+	Version                = "1.0.0"
+	ConfPath               = "/etc/pritunl_zero.json"
+	LogPath                = "/var/log/pritunl_zero.log"
+	Production             = false
+	BuildTest              = false
+	StaticRoot             = "www/dist"
+	StaticTestingRoot      = "www"
+	ProxyStaticRoot        = "www_proxy/dist"
+	ProxyStaticTestingRoot = "www_proxy"
+	StaticCache            = false
+	RetryDelay             = 3 * time.Second
 )
