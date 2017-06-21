53cefeec66f93dca0fc996ae10d2b0fed008b7fd
Zachary Huff <zach.huff.386@gmail.com>
2017-06-21 01:40:16 -0400

    Add header

--- www/app/components/Main.tsx
+++ www/app/components/Main.tsx
@@ -3,10 +3,32 @@ import * as React from 'react';
 
 document.body.className = 'root pt-dark';
 
+const css = {
+	nav: {
+		overflowX: 'auto',
+		overflowY: 'hidden',
+	} as React.CSSProperties,
+	heading: {
+		marginRight: '11px',
+	} as React.CSSProperties,
+};
+
 export default class Main extends React.Component<{}, {}> {
 	render(): JSX.Element {
 		return <div>
-			test
+			<nav className="pt-navbar layout horizontal" style={css.nav}>
+				<div className="pt-navbar-group pt-align-left flex">
+					<div className="pt-navbar-heading"
+						style={css.heading}
+					>Pritunl Zero</div>
+				</div>
+				<div className="pt-navbar-group pt-align-right">
+					<button
+						className="pt-button pt-minimal pt-icon-refresh"
+						onClick={() => {}}
+					>Refresh</button>
+				</div>
+			</nav>
 		</div>;
 	}
 }
