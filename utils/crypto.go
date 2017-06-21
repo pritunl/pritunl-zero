1713b0ac32c440e21ccb58f2818f19cbefe1c80f
Zachary Huff <zach.huff.386@gmail.com>
2017-06-21 18:52:01 -0400

    Add rand str to utils

--- utils/crypto.go
+++ utils/crypto.go
@@ -2,11 +2,39 @@ package utils
 
 import (
 	"crypto/rand"
+	"encoding/base64"
 	"github.com/dropbox/godropbox/errors"
 	"github.com/pritunl/pritunl-zero/errortypes"
+	"math"
 	"math/big"
 	mathrand "math/rand"
+	"regexp"
 )
+
+var (
+	randRe = regexp.MustCompile("[^a-zA-Z0-9]+")
+)
+
+func RandStr(n int) (str string, err error) {
+	input, err := RandBytes(int(math.Ceil(float64(n) * 1.5)))
+	if err != nil {
+		return
+	}
+
+	output := base64.RawStdEncoding.EncodeToString(input)
+	output = randRe.ReplaceAllString(output, "")
+
+	if len(output) < n {
+		err = &errortypes.UnknownError{
+			errors.Wrap(err, "utils: Random generate error"),
+		}
+		return
+	}
+
+	str = output[:n]
+
+	return
+}
 
 func RandBytes(size int) (bytes []byte, err error) {
 	bytes = make([]byte, size)
