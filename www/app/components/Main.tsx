68fd5e041166fd3b39fbcfad3c57773689b8ed06
Zachary Huff <zach.huff.386@gmail.com>
2017-06-22 17:44:41 -0400

    Add router

--- www/app/components/Main.tsx
+++ www/app/components/Main.tsx
@@ -1,6 +1,8 @@
 /// <reference path="../References.d.ts"/>
 import * as React from 'react';
+import * as ReactRouter from 'react-router-dom';
 import Loading from './Loading';
+import Settings from './Settings';
 
 document.body.className = 'root pt-dark';
 
@@ -9,6 +11,9 @@ const css = {
 		overflowX: 'auto',
 		overflowY: 'hidden',
 	} as React.CSSProperties,
+	link: {
+		color: 'inherit',
+	} as React.CSSProperties,
 	heading: {
 		marginRight: '11px',
 	} as React.CSSProperties,
@@ -16,21 +21,32 @@ const css = {
 
 export default class Main extends React.Component<{}, {}> {
 	render(): JSX.Element {
-		return <div>
-			<nav className="pt-navbar layout horizontal" style={css.nav}>
-				<div className="pt-navbar-group pt-align-left flex">
-					<div className="pt-navbar-heading"
-						style={css.heading}
-					>Pritunl Zero</div>
-					<Loading size="small"/>
-				</div>
-				<div className="pt-navbar-group pt-align-right">
-					<button
-						className="pt-button pt-minimal pt-icon-refresh"
-						onClick={() => {}}
-					>Refresh</button>
-				</div>
-			</nav>
-		</div>;
+		return <ReactRouter.HashRouter>
+			<div>
+				<nav className="pt-navbar layout horizontal" style={css.nav}>
+					<div className="pt-navbar-group pt-align-left flex">
+						<div className="pt-navbar-heading"
+							style={css.heading}
+						>Pritunl Zero</div>
+						<Loading size="small"/>
+					</div>
+					<div className="pt-navbar-group pt-align-right">
+						<ReactRouter.Link to="/console/settings" style={css.link}>
+							<button
+								className="pt-button pt-minimal pt-icon-cog"
+							>Settings</button>
+						</ReactRouter.Link>
+						<button
+							className="pt-button pt-minimal pt-icon-refresh"
+							onClick={() => {}}
+						>Refresh</button>
+					</div>
+				</nav>
+
+				<ReactRouter.Route path="/console/settings" render={() => (
+					<Settings/>
+				)}/>
+			</div>
+		</ReactRouter.HashRouter>;
 	}
 }
