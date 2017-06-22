3ca6170e793207da16645a084b5f26dfded46459
Zachary Huff <zach.huff.386@gmail.com>
2017-06-22 19:02:27 -0400

    Add settings component

---
+++ www/app/components/Settings.tsx
@@ -0,0 +1,10 @@
+/// <reference path="../References.d.ts"/>
+import * as React from 'react';
+
+export default class Settings extends React.Component<{}, {}> {
+	render(): JSX.Element {
+		return <div>
+			Settings
+		</div>;
+	}
+}
