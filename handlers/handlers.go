0805846c5b4dfd497ff97f84b6dc79a100a2b90a
Zachary Huff <zach.huff.386@gmail.com>
2017-06-23 04:32:11 -0400

    Add session group

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -6,7 +6,10 @@ import (
 	"github.com/dropbox/godropbox/errors"
 	"github.com/gin-gonic/gin"
 	"github.com/pritunl/pritunl-zero/constants"
+	"github.com/pritunl/pritunl-zero/cookie"
 	"github.com/pritunl/pritunl-zero/database"
+	"github.com/pritunl/pritunl-zero/errortypes"
+	"github.com/pritunl/pritunl-zero/session"
 	"github.com/pritunl/pritunl-zero/static"
 	"github.com/pritunl/pritunl-zero/utils"
 	"net/http"
@@ -17,21 +20,47 @@ var (
 	fileServer http.Handler
 )
 
-// Limit size of request body
-func Limiter(c *gin.Context) {
+func limiterHand(c *gin.Context) {
 	c.Request.Body = http.MaxBytesReader(c.Writer, c.Request.Body, 1000000)
 }
 
-// Get database from session
-func Database(c *gin.Context) {
+func databaseHand(c *gin.Context) {
 	db := database.GetDatabase()
 	c.Set("db", db)
 	c.Next()
 	db.Close()
 }
 
-// Recover panics
-func Recovery(c *gin.Context) {
+func sessionHand(required bool) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		db := c.MustGet("db").(*database.Database)
+
+		var sess *session.Session
+
+		cook, err := cookie.Get(c)
+		if err == nil {
+			sess, err = cook.GetSession(db)
+			switch err.(type) {
+			case nil:
+			case *errortypes.NotFoundError:
+				sess = nil
+				err = nil
+			default:
+				c.AbortWithError(500, err)
+				return
+			}
+		}
+
+		if required && sess == nil {
+			c.AbortWithStatus(401)
+			return
+		}
+
+		c.Set("session", sess)
+	}
+}
+
+func recoveryHand(c *gin.Context) {
 	defer func() {
 		if r := recover(); r != nil {
 			logrus.WithFields(logrus.Fields{
@@ -45,15 +74,23 @@ func Recovery(c *gin.Context) {
 	c.Next()
 }
 
-// Register all endpoint handlers
 func Register(engine *gin.Engine) {
-	engine.Use(Limiter)
-	engine.Use(Recovery)
+	engine.Use(limiterHand)
+	engine.Use(recoveryHand)
 
 	dbGroup := engine.Group("")
-	dbGroup.Use(Database)
+	dbGroup.Use(databaseHand)
+
+	sessGroup := dbGroup.Group("")
+	sessGroup.Use(sessionHand(false))
+
+	authGroup := dbGroup.Group("")
+	authGroup.Use(sessionHand(true))
 
 	engine.GET("/check", checkGet)
+
+	dbGroup.POST("/auth/session", authSessionPost)
+	authGroup.GET("/logout", logoutGet)
 
 	if constants.Production {
 		stre, err := static.NewStore(constants.StaticRoot)
@@ -62,21 +99,21 @@ func Register(engine *gin.Engine) {
 		}
 		store = stre
 
-		engine.GET("/", staticIndexGet)
+		sessGroup.GET("/", staticIndexGet)
 		engine.GET("/login", staticLoginGet)
-		engine.GET("/static/*path", staticGet)
+		authGroup.GET("/static/*path", staticGet)
 	} else {
 		fs := gin.Dir(constants.StaticTestingRoot, false)
 		fileServer = http.FileServer(fs)
 
-		engine.GET("/", staticTestingGet)
+		sessGroup.GET("/", staticTestingGet)
 		engine.GET("/login", staticTestingGet)
-		engine.GET("/config.js", staticTestingGet)
-		engine.GET("/build.js", staticTestingGet)
-		engine.GET("/app/*path", staticTestingGet)
-		engine.GET("/dist/*path", staticTestingGet)
-		engine.GET("/styles/*path", staticTestingGet)
-		engine.GET("/node_modules/*path", staticTestingGet)
-		engine.GET("/jspm_packages/*path", staticTestingGet)
+		authGroup.GET("/config.js", staticTestingGet)
+		authGroup.GET("/build.js", staticTestingGet)
+		authGroup.GET("/app/*path", staticTestingGet)
+		authGroup.GET("/dist/*path", staticTestingGet)
+		authGroup.GET("/styles/*path", staticTestingGet)
+		authGroup.GET("/node_modules/*path", staticTestingGet)
+		authGroup.GET("/jspm_packages/*path", staticTestingGet)
 	}
 }
