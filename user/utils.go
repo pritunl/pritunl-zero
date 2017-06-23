ad591f6ccb86d795d208d1762aea91ff9d198fe6
Zachary Huff <zach.huff.386@gmail.com>
2017-06-23 04:34:05 -0400

    Add find username

--- user/utils.go
+++ user/utils.go
@@ -19,6 +19,23 @@ func Find(db *database.Database, userId bson.ObjectId) (
 	return
 }
 
+func FindUsername(db *database.Database, typ, username string) (
+	usr *User, err error) {
+
+	coll := db.Users()
+	usr = &User{}
+
+	err = coll.FindOne(&bson.M{
+		"type":     typ,
+		"username": username,
+	}, usr)
+	if err != nil {
+		return
+	}
+
+	return
+}
+
 func HasSuper(db *database.Database) (exists bool, err error) {
 	coll := db.Users()
 
