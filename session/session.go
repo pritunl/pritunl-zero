29d46d3d4d4c8a008c159ba53423b1e827c2642f
Zachary Huff <zach.huff.386@gmail.com>
2017-06-23 03:24:38 -0400

    Add session

---
+++ session/session.go
@@ -0,0 +1,33 @@
+// Stores sessions in cookies.
+package session
+
+import (
+	"github.com/pritunl/pritunl-zero/database"
+	"github.com/pritunl/pritunl-zero/user"
+	"gopkg.in/mgo.v2/bson"
+	"time"
+)
+
+type Session struct {
+	Id        string        `bson:"_id" json:"-"`
+	UserId    bson.ObjectId `bson:"user_id" json:"-"`
+	Timestamp time.Time     `bson:"timestamp" json:"-"`
+}
+
+func (s *Session) Remove(db *database.Database) (err error) {
+	err = Remove(db, s.Id)
+	if err != nil {
+		return
+	}
+
+	return
+}
+
+func (s *Session) GetUser(db *database.Database) (usr *user.User, err error) {
+	usr, err = user.Get(db, s.UserId)
+	if err != nil {
+		return
+	}
+
+	return
+}
---
+++ session/utils.go
@@ -0,0 +1,58 @@
+package session
+
+import (
+	"github.com/pritunl/pritunl-zero/database"
+	"github.com/pritunl/pritunl-zero/utils"
+	"gopkg.in/mgo.v2/bson"
+	"time"
+)
+
+func Get(db *database.Database, id string) (
+	sess *Session, err error) {
+
+	coll := db.Sessions()
+	sess = &Session{}
+
+	err = coll.FindOneId(id, sess)
+	if err != nil {
+		return
+	}
+
+	return
+}
+
+func New(db *database.Database, userId bson.ObjectId) (
+	sess *Session, err error) {
+
+	id, err := utils.RandStr(32)
+	if err != nil {
+		return
+	}
+
+	coll := db.Sessions()
+	sess = &Session{
+		Id:        id,
+		UserId:    userId,
+		Timestamp: time.Now(),
+	}
+
+	err = coll.Insert(sess)
+	if err != nil {
+		err = database.ParseError(err)
+		return
+	}
+
+	return
+}
+
+func Remove(db *database.Database, id string) (err error) {
+	coll := db.Sessions()
+
+	err = coll.RemoveId(id)
+	if err != nil {
+		err = database.ParseError(err)
+		return
+	}
+
+	return
+}
