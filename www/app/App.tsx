13fee12f28224cc5f13c6ecad88e391c9a4e234e
Zachary Huff <zach.huff.386@gmail.com>
2017-06-24 01:31:24 -0400

    Add event to www

--- www/app/App.tsx
+++ www/app/App.tsx
@@ -3,8 +3,10 @@ import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import * as Blueprint from '@blueprintjs/core';
 import Main from './components/Main';
+import * as Event from './Event';
 
 Blueprint.FocusStyleManager.onlyShowFocusOnTabs();
+Event.init();
 
 ReactDOM.render(
 	<div><Main/></div>,
