ab522f2a40d04f5780dfc854e86d7eda2becb097
Zachary Huff <zach.huff.386@gmail.com>
2017-06-25 00:54:51 -0400

    Add users component

---
+++ www/app/components/Users.tsx
@@ -0,0 +1,52 @@
+/// <reference path="../References.d.ts"/>
+import * as React from 'react';
+import Styles from '../Styles';
+import * as UserTypes from '../types/UserTypes';
+import UserStore from '../stores/UserStore';
+import * as UserActions from '../actions/UserActions';
+
+interface State {
+	users: UserTypes.Users;
+}
+
+export default class Users extends React.Component<{}, State> {
+	constructor(props: any, context: any) {
+		super(props, context);
+		this.state = {
+			users: UserStore.users,
+		};
+	}
+
+	componentDidMount(): void {
+		UserActions.sync();
+		UserStore.addChangeListener(this.onChange);
+	}
+
+	componentWillUnmount(): void {
+		UserStore.removeChangeListener(this.onChange);
+	}
+
+	onChange = (): void => {
+		this.setState({
+			...this.state,
+			users: UserStore.users,
+		});
+	}
+
+	render(): JSX.Element {
+		let usersDom: JSX.Element[] = [];
+
+		for (let user of this.state.users) {
+			usersDom.push(<div>{user.username}</div>)
+		}
+
+		return <div style={Styles.page}>
+			<div className="pt-border" style={Styles.pageHeader}>
+				<h2>Users</h2>
+			</div>
+			<div className="layout horizontal">
+				{usersDom}
+			</div>
+		</div>;
+	}
+}
