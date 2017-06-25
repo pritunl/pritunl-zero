581fbfad7204bcbc5e6af16575934f6e4206666f
Zachary Huff <zach.huff.386@gmail.com>
2017-06-25 00:58:45 -0400

    Cache user in session

--- session/session.go
+++ session/session.go
@@ -12,6 +12,7 @@ type Session struct {
 	Id        string        `bson:"_id" json:"-"`
 	UserId    bson.ObjectId `bson:"user_id" json:"-"`
 	Timestamp time.Time     `bson:"timestamp" json:"-"`
+	user      *user.User    `bson:"-" json:"-"`
 }
 
 func (s *Session) Remove(db *database.Database) (err error) {
@@ -24,10 +25,17 @@ func (s *Session) Remove(db *database.Database) (err error) {
 }
 
 func (s *Session) GetUser(db *database.Database) (usr *user.User, err error) {
+	if s.user != nil {
+		usr = s.user
+		return
+	}
+
 	usr, err = user.Get(db, s.UserId)
 	if err != nil {
 		return
 	}
+
+	s.user = usr
 
 	return
 }
