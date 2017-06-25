b7cb779e6c2e5aee661a9ec8a75585fa472bb567
Zachary Huff <zach.huff.386@gmail.com>
2017-06-25 00:55:59 -0400

    Update format date

--- www/app/utils/MiscUtils.ts
+++ www/app/utils/MiscUtils.ts
@@ -10,15 +10,92 @@ export function zeroPad(num: number, width: number): string {
 	return num.toString();
 }
 
-export function formatDate(date: Date): string {
+export function formatDate(dateStr: string): string {
+	let date = new Date(dateStr);
 	let str = '';
 
-	str += zeroPad(date.getHours(), 2) + ':';
+	let hours = date.getHours();
+	let period = 'AM';
+
+	if (hours > 12) {
+		period = 'PM';
+		hours -= 12;
+	} else if (hours === 0) {
+		hours = 12;
+	}
+
+	let day;
+	switch (date.getDay()) {
+		case 0:
+			day = 'Sun';
+			break;
+		case 1:
+			day = 'Mon';
+			break;
+		case 2:
+			day = 'Tue';
+			break;
+		case 3:
+			day = 'Wed';
+			break;
+		case 4:
+			day = 'Thu';
+			break;
+		case 5:
+			day = 'Fre';
+			break;
+		case 6:
+			day = 'Sat';
+			break;
+	}
+
+	let month;
+	switch (date.getMonth()) {
+		case 0:
+			month = 'Jan';
+			break;
+		case 1:
+			month = 'Feb';
+			break;
+		case 2:
+			month = 'Mar';
+			break;
+		case 3:
+			month = 'Apr';
+			break;
+		case 4:
+			month = 'May';
+			break;
+		case 5:
+			month = 'Jun';
+			break;
+		case 6:
+			month = 'Jul';
+			break;
+		case 7:
+			month = 'Aug';
+			break;
+		case 8:
+			month = 'Sep';
+			break;
+		case 9:
+			month = 'Oct';
+			break;
+		case 10:
+			month = 'Nov';
+			break;
+		case 11:
+			month = 'Dec';
+			break;
+	}
+
+	str += day + ' ';
+	str += date.getDate() + ' ';
+	str += month + ', ';
+	str += hours + ':';
 	str += zeroPad(date.getMinutes(), 2) + ':';
 	str += zeroPad(date.getSeconds(), 2) + ' ';
-	str += zeroPad(date.getMonth() + 1, 2) + '-';
-	str += zeroPad(date.getDate(), 2) + '-';
-	str += date.getFullYear().toString();
+	str += period;
 
 	return str;
 }
