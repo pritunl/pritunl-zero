5ff46a4313c6b09e27c35031548e777e02588546
Zachary Huff <zach.huff.386@gmail.com>
2017-06-26 04:47:02 -0400

    Add user types set

--- user/constants.go
+++ user/constants.go
@@ -1,8 +1,21 @@
 package user
 
+import (
+	"github.com/dropbox/godropbox/container/set"
+)
+
 const (
 	Local    = "local"
 	Google   = "google"
 	OneLogin = "onelogin"
 	Okta     = "okta"
+)
+
+var (
+	types = set.NewSet(
+		Local,
+		Google,
+		OneLogin,
+		Okta,
+	)
 )
