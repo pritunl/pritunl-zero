76984b9b36f8a15b54aa010c7063f272cdede993
Zachary Huff <zach.huff.386@gmail.com>
2017-06-27 05:48:05 -0400

    Add page date time

---
+++ www/app/components/PageDateTime.tsx
@@ -0,0 +1,59 @@
+/// <reference path="../References.d.ts"/>
+import * as React from 'react';
+import * as BlueprintDateTime from '@blueprintjs/datetime';
+
+type OnChange = (val: string) => void;
+
+interface Props {
+	label: string;
+	value: string;
+	onChange: OnChange;
+}
+
+const css = {
+	group: {
+		display: 'inline-block',
+	} as React.CSSProperties,
+	label: {
+		marginBottom: '5px',
+	} as React.CSSProperties,
+};
+
+export default class PageDateTime extends React.Component<Props, {}> {
+	render(): JSX.Element {
+		let dateStyle = {} as React.CSSProperties;
+
+		let date = new Date(this.props.value);
+		if (!this.props.value || this.props.value === '0001-01-01T00:00:00Z') {
+			date = null;
+		}
+
+		if (!date) {
+			dateStyle.opacity = 0.5;
+		}
+
+		return <div style={css.group}>
+			<label className="pt-label" style={css.label}>
+				{this.props.label}
+			</label>
+			<div style={dateStyle}>
+				<BlueprintDateTime.DateTimePicker
+					value={date}
+					timePickerProps={{
+						showArrowButtons: true,
+					}}
+					datePickerProps={{
+						showActionsBar: true,
+					}}
+					onChange={(date: Date): void => {
+						if (date) {
+							this.props.onChange(date.toJSON());
+						} else {
+							this.props.onChange(null);
+						}
+					}}
+				/>
+			</div>
+		</div>;
+	}
+}
