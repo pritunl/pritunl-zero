3e65963cf3ec7f477325d809a498be319907ee13
Zachary Huff <zach.huff.386@gmail.com>
2017-06-27 01:12:08 -0400

    Add delete selected to users

--- www/app/components/Users.tsx
+++ www/app/components/Users.tsx
@@ -8,8 +8,12 @@ import User from './User';
 import Page from './Page';
 import PageHeader from './PageHeader';
 
+type Selected = {[key: string]: boolean};
+
 interface State {
 	users: UserTypes.Users;
+	selected: Selected;
+	disabled: boolean;
 }
 
 const css = {
@@ -19,6 +23,9 @@ const css = {
 		display: 'table',
 		borderSpacing: '0 5px',
 	} as React.CSSProperties,
+	button: {
+		marginLeft: '10px',
+	} as React.CSSProperties,
 };
 
 export default class Users extends React.Component<{}, State> {
@@ -26,12 +33,23 @@ export default class Users extends React.Component<{}, State> {
 		super(props, context);
 		this.state = {
 			users: UsersStore.users,
+			selected: {},
+			disabled: false,
 		};
 	}
 
+	get selected(): boolean {
+		for (let val in this.state.selected) {
+			if (this.state.selected[val]) {
+				return true;
+			}
+		}
+		return false;
+	}
+
 	componentDidMount(): void {
-		UserActions.sync();
 		UsersStore.addChangeListener(this.onChange);
+		UserActions.sync();
 	}
 
 	componentWillUnmount(): void {
@@ -39,9 +57,39 @@ export default class Users extends React.Component<{}, State> {
 	}
 
 	onChange = (): void => {
+		let users = UsersStore.users;
+		let selected = {} as Selected;
+		let curSelected = this.state.selected;
+
+		for (let user of users) {
+			if (curSelected[user.id]) {
+				selected[user.id] = true;
+			}
+		}
+
 		this.setState({
 			...this.state,
-			users: UsersStore.users,
+			users: users,
+			selected: selected,
+		});
+	}
+
+	onDelete = (): void => {
+		this.setState({
+			...this.state,
+			disabled: true,
+		});
+		UserActions.remove(Object.keys(this.state.selected)).then((): void => {
+			this.setState({
+				...this.state,
+				selected: {},
+				disabled: false,
+			});
+		}).catch((): void => {
+			this.setState({
+				...this.state,
+				disabled: false,
+			});
 		});
 	}
 
@@ -49,7 +97,25 @@ export default class Users extends React.Component<{}, State> {
 		let usersDom: JSX.Element[] = [];
 
 		for (let user of this.state.users) {
-			usersDom.push(<User key={user.id} user={user}/>)
+			usersDom.push(<User
+				key={user.id}
+				user={user}
+				selected={!!this.state.selected[user.id]}
+				onSelect={(): void => {
+					let selected = this.state.selected;
+
+					if (selected[user.id]) {
+						delete selected[user.id];
+					} else {
+						selected[user.id] = true;
+					}
+
+					this.setState({
+						...this.state,
+						selected: selected,
+					});
+				}}
+			/>)
 		}
 
 		return <Page>
@@ -58,8 +124,18 @@ export default class Users extends React.Component<{}, State> {
 					<h2>Users</h2>
 					<div className="flex"/>
 					<div>
+						<button
+							className="pt-button pt-intent-danger pt-icon-delete"
+							style={css.button}
+							type="button"
+							disabled={!this.selected || this.state.disabled}
+							onClick={this.onDelete}
+						>
+							Delete Selected
+						</button>
 						<ReactRouter.Link
 							className="pt-button pt-intent-success pt-icon-add"
+							style={css.button}
 							to="/user"
 						>
 							New
