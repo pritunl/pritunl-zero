6a2abfcf0f63ae234fea54da380e96d559400579
Zachary Huff <zach.huff.386@gmail.com>
2017-06-29 04:36:50 -0400

    Add page select component

---
+++ www/app/components/PageSelect.tsx
@@ -0,0 +1,35 @@
+/// <reference path="../References.d.ts"/>
+import * as React from 'react';
+
+interface Props {
+	hidden?: boolean;
+	label: string;
+	value: string;
+	onChange: (val: string) => void;
+}
+
+const css = {
+	label: {
+		display: 'inline-block',
+	} as React.CSSProperties,
+};
+
+export default class PageSelect extends React.Component<Props, {}> {
+	render(): JSX.Element {
+		return <div hidden={this.props.hidden}>
+			<label className="pt-label" style={css.label}>
+				{this.props.label}
+				<div className="pt-select">
+					<select
+						value={this.props.value}
+						onChange={(evt): void => {
+							this.props.onChange(evt.target.value);
+						}}
+					>
+						{this.props.children}
+					</select>
+				</div>
+			</label>
+		</div>;
+	}
+}
