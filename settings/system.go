78cf0071749a170e6a57f6beb7f0f5616be12108
Zachary Huff <zach.huff.386@gmail.com>
2017-06-29 20:38:23 -0400

    Add license to system settings

--- settings/system.go
+++ settings/system.go
@@ -13,6 +13,7 @@ type system struct {
 	Id                   string `bson:"_id"`
 	Name                 string `bson:"name"`
 	DatabaseVersion      string `bson:"database_version"`
+	License              string `bson:"license"`
 	CookieAuthKey        []byte `bson:"cookie_auth_key"`
 	CookieCryptoKey      []byte `bson:"cookie_crypto_key"`
 	ProxyCookieAuthKey   []byte `bson:"proxy_cookie_auth_key"`
