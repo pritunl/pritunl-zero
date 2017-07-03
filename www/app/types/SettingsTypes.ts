88900e04fdcb01d891f0d9ec7113d788f7d70f01
Zachary Huff <zach.huff.386@gmail.com>
2017-07-03 18:32:20 -0400

    Add provider types to settings

--- www/app/types/SettingsTypes.ts
+++ www/app/types/SettingsTypes.ts
@@ -2,7 +2,21 @@
 export const SYNC = 'settings.sync';
 export const CHANGE = 'settings.change';
 
+export interface Provider {
+	type: string;
+	label: string;
+	default_roles: string[];
+}
+
+export interface GoogleProvider extends Provider {
+	domain?: string;
+}
+
+export type ProviderAny = Provider & GoogleProvider;
+export type Providers = ProviderAny[];
+
 export interface Settings {
+	auth_providers: Providers;
 	elastic_address: string;
 }
 
