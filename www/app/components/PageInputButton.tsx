f35c2c945ac69e2dfd84fc40c3f261dde206fc45
Zachary Huff <zach.huff.386@gmail.com>
2017-07-04 03:14:05 -0400

    Improve page input button

--- www/app/components/PageInputButton.tsx
+++ www/app/components/PageInputButton.tsx
@@ -16,32 +16,48 @@ interface Props {
 const css = {
 	group: {
 		marginBottom: '15px',
+		width: '100%',
+		maxWidth: '310px',
+	} as React.CSSProperties,
+	input: {
+		width: '100%',
+	} as React.CSSProperties,
+	inputBox: {
+		flex: '1',
 	} as React.CSSProperties,
 };
 
 export default class PageInputButton extends React.Component<Props, {}> {
 	render(): JSX.Element {
-		return <div className="pt-control-group" style={css.group}>
-			<input
-				className="pt-input"
-				type={this.props.type}
-				autoCapitalize="off"
-				spellCheck={false}
-				placeholder={this.props.placeholder}
-				value={this.props.value || ''}
-				onChange={(evt): void => {
-					this.props.onChange(evt.target.value);
-				}}
-				onKeyPress={(evt): void => {
-					if (evt.key === 'Enter') {
-						this.props.onSubmit();
-					}
-				}}
-			/>
-			<button
-				className="pt-button"
-				onClick={this.props.onSubmit}
-			>{this.props.label}</button>
+		return <div
+			className="pt-control-group"
+			style={css.group}
+		>
+			<div style={css.inputBox}>
+				<input
+					className="pt-input"
+					style={css.input}
+					type={this.props.type}
+					autoCapitalize="off"
+					spellCheck={false}
+					placeholder={this.props.placeholder}
+					value={this.props.value || ''}
+					onChange={(evt): void => {
+						this.props.onChange(evt.target.value);
+					}}
+					onKeyPress={(evt): void => {
+						if (evt.key === 'Enter') {
+							this.props.onSubmit();
+						}
+					}}
+				/>
+			</div>
+			<div>
+				<button
+					className="pt-button"
+					onClick={this.props.onSubmit}
+				>{this.props.label}</button>
+			</div>
 		</div>;
 	}
 }
