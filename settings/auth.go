7d8cc0d1bdc6d5c02b4c9d8cc8327729c5cc5bc2
Zachary Huff <zach.huff.386@gmail.com>
2017-07-06 02:16:49 -0400

    Add id to auth provider

--- settings/auth.go
+++ settings/auth.go
@@ -1,13 +1,18 @@
 package settings
 
+import (
+	"gopkg.in/mgo.v2/bson"
+)
+
 var Auth *auth
 
 type Provider struct {
-	Type         string   `bson:"type" json:"type"`
-	Label        string   `bson:"label" json:"label"`
-	DefaultRoles []string `bson:"default_roles" json:"default_roles"`
-	AutoCreate   bool     `bson:"auto_create" json:"auto_create"`
-	Domain       string   `bson:"domain" json:"domain"` // google
+	Id           bson.ObjectId `bson:"id" json:"id"`
+	Type         string        `bson:"type" json:"type"`
+	Label        string        `bson:"label" json:"label"`
+	DefaultRoles []string      `bson:"default_roles" json:"default_roles"`
+	AutoCreate   bool          `bson:"auto_create" json:"auto_create"`
+	Domain       string        `bson:"domain" json:"domain"` // google
 }
 
 type auth struct {
