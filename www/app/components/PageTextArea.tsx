13fe3f8f0c26bf12354f0ebd95e4ce3a0821adbf
Zachary Huff <zach.huff.386@gmail.com>
2017-07-10 17:46:03 -0400

    Add page text area component

---
+++ www/app/components/PageTextArea.tsx
@@ -0,0 +1,48 @@
+/// <reference path="../References.d.ts"/>
+import * as React from 'react';
+
+interface Props {
+	hidden?: boolean;
+	label: string;
+	type: string;
+	placeholder: string;
+	rows: number;
+	value: string;
+	onChange: (val: string) => void;
+}
+
+const css = {
+	label: {
+		width: '100%',
+		maxWidth: '280px',
+	} as React.CSSProperties,
+	textarea: {
+		width: '100%',
+		resize: 'none',
+	} as React.CSSProperties,
+};
+
+export default class PageTextArea extends React.Component<Props, {}> {
+	render(): JSX.Element {
+		return <label
+			className="pt-label"
+			style={css.label}
+			hidden={this.props.hidden}
+		>
+			{this.props.label}
+			<textarea
+				className="pt-input"
+				style={css.textarea}
+				type={this.props.type}
+				autoCapitalize="off"
+				spellCheck={false}
+				placeholder={this.props.placeholder}
+				rows={this.props.rows}
+				value={this.props.value || ''}
+				onChange={(evt): void => {
+					this.props.onChange(evt.target.value);
+				}}
+			/>
+		</label>;
+	}
+}
