23f0a641dc6a8501c1cdc0635da2d725c23bbe1e
Zachary Huff <zach.huff.386@gmail.com>
2017-07-11 21:51:34 -0400

    Add service actions

---
+++ www/app/actions/ServiceActions.ts
@@ -0,0 +1,119 @@
+/// <reference path="../References.d.ts"/>
+import * as SuperAgent from 'superagent';
+import Dispatcher from '../dispatcher/Dispatcher';
+import EventDispatcher from '../dispatcher/EventDispatcher';
+import * as Alert from '../Alert';
+import Loader from '../Loader';
+import * as ServiceTypes from '../types/ServiceTypes';
+import * as MiscUtils from '../utils/MiscUtils';
+
+let syncId: string;
+
+export function sync(): Promise<void> {
+	let curSyncId = MiscUtils.uuid();
+	syncId = curSyncId;
+
+	let loader = new Loader().loading();
+
+	return new Promise<void>((resolve, reject): void => {
+		SuperAgent
+			.get('/service')
+			.set('Accept', 'application/json')
+			.end((err: any, res: SuperAgent.Response): void => {
+				loader.done();
+
+				if (curSyncId !== syncId) {
+					resolve();
+					return;
+				}
+
+				if (err) {
+					Alert.errorRes(res, 'Failed to load services');
+					reject(err);
+					return;
+				}
+
+				Dispatcher.dispatch({
+					type: ServiceTypes.SYNC,
+					data: {
+						services: res.body,
+					},
+				});
+
+				resolve();
+			});
+	});
+}
+
+export function commit(service: ServiceTypes.Service): Promise<void> {
+	let loader = new Loader().loading();
+
+	return new Promise<void>((resolve, reject): void => {
+		SuperAgent
+			.put('/service/' + service.id)
+			.send(service)
+			.set('Accept', 'application/json')
+			.end((err: any, res: SuperAgent.Response): void => {
+				loader.done();
+
+				if (err) {
+					Alert.errorRes(res, 'Failed to save service');
+					reject(err);
+					return;
+				}
+
+				resolve();
+			});
+	});
+}
+
+export function create(service: ServiceTypes.Service): Promise<void> {
+	let loader = new Loader().loading();
+
+	return new Promise<void>((resolve, reject): void => {
+		SuperAgent
+			.post('/service')
+			.send(service)
+			.set('Accept', 'application/json')
+			.end((err: any, res: SuperAgent.Response): void => {
+				loader.done();
+
+				if (err) {
+					Alert.errorRes(res, 'Failed to create service');
+					reject(err);
+					return;
+				}
+
+				resolve();
+			});
+	});
+}
+
+export function remove(serviceId: string): Promise<void> {
+	let loader = new Loader().loading();
+
+	return new Promise<void>((resolve, reject): void => {
+		SuperAgent
+			.delete('/service/' + serviceId)
+			.set('Accept', 'application/json')
+			.end((err: any, res: SuperAgent.Response): void => {
+				loader.done();
+
+				if (err) {
+					Alert.errorRes(res, 'Failed to delete services');
+					reject(err);
+					return;
+				}
+
+				resolve();
+			});
+	});
+}
+
+EventDispatcher.register((action: ServiceTypes.ServiceDispatch) => {
+	switch (action.type) {
+		case ServiceTypes.CHANGE:
+			sync();
+			break;
+	}
+});
