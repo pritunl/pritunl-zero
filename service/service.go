0b27b6d7329b6817f0fd8000e570068b6440a8c2
Zachary Huff <zach.huff.386@gmail.com>
2017-07-12 19:47:58 -0400

    Add service

---
+++ service/service.go
@@ -0,0 +1,42 @@
+package service
+
+import (
+	"github.com/dropbox/godropbox/errors"
+	"github.com/pritunl/pritunl-zero/database"
+	"github.com/pritunl/pritunl-zero/errortypes"
+	"gopkg.in/mgo.v2/bson"
+)
+
+type Service struct {
+	Id   bson.ObjectId `bson:"_id,omitempty" json:"id"`
+	Name string        `bson:"name" json:"name"`
+}
+
+func (s *Service) Commit(db *database.Database) (err error) {
+	coll := db.Services()
+
+	err = coll.Commit(s.Id, s)
+	if err != nil {
+		return
+	}
+
+	return
+}
+
+func (s *Service) Insert(db *database.Database) (err error) {
+	coll := db.Services()
+
+	if s.Id != "" {
+		err = &errortypes.DatabaseError{
+			errors.New("service: Service already exists"),
+		}
+		return
+	}
+
+	err = coll.Insert(s)
+	if err != nil {
+		return
+	}
+
+	return
+}
