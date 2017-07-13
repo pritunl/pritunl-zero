8dc5e9f92090acdedac2ef4d68787bcc03e351e6
Zachary Huff <zach.huff.386@gmail.com>
2017-07-13 00:23:37 -0400

    Update service handler

--- handlers/service.go
+++ handlers/service.go
@@ -3,6 +3,7 @@ package handlers
 import (
 	"github.com/gin-gonic/gin"
 	"github.com/pritunl/pritunl-zero/database"
+	"github.com/pritunl/pritunl-zero/service"
 	"gopkg.in/mgo.v2/bson"
 )
 
@@ -11,20 +12,14 @@ type serviceData struct {
 	Name string        `json:"name"`
 }
 
-var temp = []*serviceData{
-	&serviceData{
-		Id:   bson.NewObjectId(),
-		Name: "test1",
-	},
-	&serviceData{
-		Id:   bson.NewObjectId(),
-		Name: "test2",
-	},
-}
-
 func servicesGet(c *gin.Context) {
 	db := c.MustGet("db").(*database.Database)
-	_ = db
 
-	c.JSON(200, temp)
+	services, err := service.GetAll(db)
+	if err != nil {
+		c.AbortWithError(500, err)
+		return
+	}
+
+	c.JSON(200, services)
 }
