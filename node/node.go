dc9f985c52a612d7c963483d9ca2126947002b23
Zachary Huff <zach.huff.386@gmail.com>
2017-07-17 00:55:48 -0400

    Add commit to node

--- node/node.go
+++ node/node.go
@@ -2,6 +2,7 @@ package node
 
 import (
 	"github.com/Sirupsen/logrus"
+	"github.com/dropbox/godropbox/container/set"
 	"github.com/pritunl/pritunl-zero/database"
 	"github.com/pritunl/pritunl-zero/utils"
 	"gopkg.in/mgo.v2"
@@ -20,6 +21,30 @@ type Node struct {
 	Load1     float64       `bson:"load1" json:"load1"`
 	Load5     float64       `bson:"load5" json:"load5"`
 	Load15    float64       `bson:"load15" json:"load15"`
+}
+
+func (n *Node) Commit(db *database.Database) (err error) {
+	coll := db.Nodes()
+
+	err = coll.Commit(n.Id, n)
+	if err != nil {
+		return
+	}
+
+	return
+}
+
+func (n *Node) CommitFields(db *database.Database, fields set.Set) (
+	err error) {
+
+	coll := db.Nodes()
+
+	err = coll.CommitFields(n.Id, n, fields)
+	if err != nil {
+		return
+	}
+
+	return
 }
 
 func (n *Node) update(db *database.Database) (err error) {
