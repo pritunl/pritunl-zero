639790b7005739e4d27594bde2e3669086ae2bbf
Zachary Huff <zach.huff.386@gmail.com>
2017-07-16 21:31:58 -0400

    Change type in node

--- node/node.go
+++ node/node.go
@@ -12,13 +12,13 @@ import (
 var Self *Node
 
 type Node struct {
-	Id        string    `bson:"_id" json:"id"`
-	Type      string    `bson:"type" json:"type"`
-	Timestamp time.Time `bson:"timestamp" json:"timestamp"`
-	Memory    float64   `bson:"memory" json:"memory"`
-	Load1     float64   `bson:"load1" json:"load1"`
-	Load5     float64   `bson:"load5" json:"load5"`
-	Load15    float64   `bson:"load15" json:"load15"`
+	Id        bson.ObjectId `bson:"_id" json:"id"`
+	Type      string        `bson:"type" json:"type"`
+	Timestamp time.Time     `bson:"timestamp" json:"timestamp"`
+	Memory    float64       `bson:"memory" json:"memory"`
+	Load1     float64       `bson:"load1" json:"load1"`
+	Load5     float64       `bson:"load5" json:"load5"`
+	Load15    float64       `bson:"load15" json:"load15"`
 }
 
 func (n *Node) update(db *database.Database) (err error) {
