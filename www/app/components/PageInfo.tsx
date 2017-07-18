8db843ab4206072d9d6495fd016d27316e3c30e5
Zachary Huff <zach.huff.386@gmail.com>
2017-07-18 19:14:16 -0400

    Add page info

---
+++ www/app/components/PageInfo.tsx
@@ -0,0 +1,72 @@
+/// <reference path="../References.d.ts"/>
+import * as React from 'react';
+
+interface Field {
+	className?: string;
+	label: string;
+	value: string;
+}
+
+interface Bar {
+	progressClass?: string;
+	label: string;
+	value: number;
+}
+
+interface Props {
+	hidden?: boolean;
+	fields?: Field[];
+	bars?: Bar[];
+}
+
+const css = {
+	label: {
+		width: '100%',
+		maxWidth: '280px',
+	} as React.CSSProperties,
+	item: {
+		marginBottom: '5px',
+	} as React.CSSProperties,
+};
+
+export default class PageInfo extends React.Component<Props, {}> {
+	render(): JSX.Element {
+		let fields: JSX.Element[] = [];
+		let bars: JSX.Element[] = [];
+
+		for (let field of this.props.fields || []) {
+			fields.push(
+				<div key={field.label} style={css.item}>
+					{field.label}
+					<div className={'pt-text-muted ' + (field.className || '')}>
+						{field.value}
+					</div>
+				</div>,
+			);
+		}
+
+		for (let bar of this.props.bars || []) {
+			let style: React.CSSProperties = {
+				width: (bar.value || 0) + '%',
+			};
+
+			bars.push(
+				<div key={bar.label} style={css.item}>
+					{bar.label}
+					<div className={'pt-progress-bar ' + (bar.progressClass || '')}>
+						<div className="pt-progress-meter" style={style}/>
+					</div>
+				</div>,
+			);
+		}
+
+		return <label
+			className="pt-label"
+			style={css.label}
+			hidden={this.props.hidden}
+		>
+			{fields}
+			{bars}
+		</label>;
+	}
+}
