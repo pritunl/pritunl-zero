ebe0bb618247b1fae26b3fd5c6b8bf6a727d8a34
Zachary Huff <zach.huff.386@gmail.com>
2017-07-18 20:48:06 -0400

    Update node cmd

--- cmd/node.go
+++ cmd/node.go
@@ -1,58 +1,27 @@
 package cmd
 
 import (
-	"github.com/Sirupsen/logrus"
-	"github.com/dropbox/godropbox/errors"
-	"github.com/gin-gonic/gin"
 	"github.com/pritunl/pritunl-zero/config"
-	"github.com/pritunl/pritunl-zero/constants"
-	"github.com/pritunl/pritunl-zero/errortypes"
-	"github.com/pritunl/pritunl-zero/mhandlers"
 	"github.com/pritunl/pritunl-zero/node"
+	"github.com/pritunl/pritunl-zero/router"
 	"gopkg.in/mgo.v2/bson"
-	"net/http"
-	"time"
 )
 
 func Node() (err error) {
-	if constants.Production {
-		gin.SetMode(gin.ReleaseMode)
-	} else {
-		gin.SetMode(gin.DebugMode)
-	}
-
-	router := gin.New()
-
-	if !constants.Production {
-		router.Use(gin.Logger())
-	}
-
-	mhandlers.Register(router)
-
-	nde := node.Node{
-		Id:   bson.ObjectIdHex(config.Config.ManagementNodeId),
+	nde := &node.Node{
+		Id:   bson.ObjectIdHex(config.Config.NodeId),
 		Type: node.Management,
 	}
-	nde.Init()
-
-	server := &http.Server{
-		Addr:           "0.0.0.0:8443",
-		Handler:        router,
-		ReadTimeout:    10 * time.Second,
-		WriteTimeout:   10 * time.Second,
-		MaxHeaderBytes: 4096,
+	err = nde.Init()
+	if err != nil {
+		return
 	}
 
-	logrus.WithFields(logrus.Fields{
-		"type":       nde.Type,
-		"production": constants.Production,
-	}).Info("cmd.node: Starting node")
-
-	err = server.ListenAndServe()
+	routr := &router.Router{
+		Node: nde,
+	}
+	err = routr.Run()
 	if err != nil {
-		err = &errortypes.UnknownError{
-			errors.Wrap(err, "node: Server listen failed"),
-		}
 		return
 	}
 
