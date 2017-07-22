8da8bbd07d6d142df0451b224e9d56c368d1f395
Zachary Huff <zach.huff.386@gmail.com>
2017-07-21 23:32:49 -0400

    Add csrf

---
+++ www/app/Csrf.ts
@@ -0,0 +1,21 @@
+/// <reference path="./References.d.ts"/>
+import * as SuperAgent from 'superagent';
+
+export let token = '';
+
+export function load(): Promise<void> {
+	return new Promise<void>((resolve, reject): void => {
+		SuperAgent
+			.get('/csrf')
+			.set('Accept', 'application/json')
+			.end((err: any, res: SuperAgent.Response): void => {
+				if (err) {
+					reject(err);
+					return;
+				}
+
+				token = res.body.token;
+				resolve();
+			});
+	});
+}
