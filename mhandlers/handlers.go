46519e0c723467492f4605ca2f53e75182d0f27f
Zachary Huff <zach.huff.386@gmail.com>
2017-07-22 17:37:39 -0400

    Use middlewear in mhandlers

--- mhandlers/handlers.go
+++ mhandlers/handlers.go
@@ -1,19 +1,11 @@
 package mhandlers
 
 import (
-	"fmt"
-	"github.com/Sirupsen/logrus"
-	"github.com/dropbox/godropbox/errors"
 	"github.com/gin-contrib/location"
 	"github.com/gin-gonic/gin"
 	"github.com/pritunl/pritunl-zero/constants"
-	"github.com/pritunl/pritunl-zero/cookie"
-	"github.com/pritunl/pritunl-zero/csrf"
-	"github.com/pritunl/pritunl-zero/database"
-	"github.com/pritunl/pritunl-zero/errortypes"
-	"github.com/pritunl/pritunl-zero/session"
+	"github.com/pritunl/pritunl-zero/middlewear"
 	"github.com/pritunl/pritunl-zero/static"
-	"github.com/pritunl/pritunl-zero/utils"
 	"net/http"
 )
 
@@ -22,146 +14,27 @@ var (
 	fileServer http.Handler
 )
 
-func limiterHand(c *gin.Context) {
-	c.Request.Body = http.MaxBytesReader(c.Writer, c.Request.Body, 1000000)
-}
-
-func databaseHand(c *gin.Context) {
-	db := database.GetDatabase()
-	c.Set("db", db)
-	c.Next()
-	db.Close()
-}
-
-func sessionHand(c *gin.Context) {
-	db := c.MustGet("db").(*database.Database)
-
-	var sess *session.Session
-
-	cook, err := cookie.Get(c.Writer, c.Request)
-	if err == nil {
-		sess, err = cook.GetSession(db)
-		switch err.(type) {
-		case nil:
-		case *errortypes.NotFoundError:
-			sess = nil
-			err = nil
-		default:
-			c.AbortWithError(500, err)
-			return
-		}
-	}
-
-	c.Set("session", sess)
-	c.Set("cookie", cook)
-}
-
-func authHand(c *gin.Context) {
-	db := c.MustGet("db").(*database.Database)
-	sess := c.MustGet("session").(*session.Session)
-	cook := c.MustGet("cookie").(*cookie.Cookie)
-
-	if sess == nil {
-		c.AbortWithStatus(401)
-		return
-	}
-
-	usr, err := sess.GetUser(db)
-	if err != nil {
-		c.AbortWithError(500, err)
-		return
-	}
-
-	if usr.Disabled || usr.Administrator != "super" {
-		sess = nil
-
-		err = cook.Remove(db)
-		if err != nil {
-			c.AbortWithError(500, err)
-			return
-		}
-
-		c.AbortWithStatus(401)
-		return
-	}
-}
-
-func csrfHand(c *gin.Context) {
-	db := c.MustGet("db").(*database.Database)
-	sess := c.MustGet("session").(*session.Session)
-
-	if sess == nil {
-		c.AbortWithStatus(401)
-		return
-	}
-
-	token := c.Request.Header.Get("Csrf-Token")
-
-	valid, err := csrf.ValidateToken(db, sess.Id, token)
-	if err != nil {
-		c.AbortWithError(500, err)
-		return
-	}
-
-	if !valid {
-		c.AbortWithStatus(401)
-		return
-	}
-}
-
-func activeHand(c *gin.Context) {
-	db := c.MustGet("db").(*database.Database)
-	sess := c.MustGet("session").(*session.Session)
-
-	usr, err := sess.GetUser(db)
-	if err != nil {
-		c.AbortWithError(500, err)
-		return
-	}
-
-	err = usr.SetActive(db)
-	if err != nil {
-		return
-	}
-
-	c.Next()
-}
-
-func recoveryHand(c *gin.Context) {
-	defer func() {
-		if r := recover(); r != nil {
-			logrus.WithFields(logrus.Fields{
-				"client": utils.GetRemoteAddr(c),
-				"error":  errors.New(fmt.Sprintf("%s", r)),
-			}).Error("handlers: Handler panic")
-			c.Writer.WriteHeader(http.StatusInternalServerError)
-		}
-	}()
-
-	c.Next()
-}
-
 func Register(protocol string, engine *gin.Engine) {
-	engine.Use(limiterHand)
-	engine.Use(recoveryHand)
+	engine.Use(middlewear.Limiter)
+	engine.Use(middlewear.Recovery)
 	engine.Use(location.New(location.Config{
 		Scheme: protocol,
 	}))
 
 	dbGroup := engine.Group("")
-	dbGroup.Use(databaseHand)
+	dbGroup.Use(middlewear.Database)
 
 	sessGroup := dbGroup.Group("")
-	sessGroup.Use(sessionHand)
+	sessGroup.Use(middlewear.Session)
 
 	authGroup := sessGroup.Group("")
-	authGroup.Use(authHand)
+	authGroup.Use(middlewear.Auth)
 
 	csrfGroup := authGroup.Group("")
-	csrfGroup.Use(csrfHand)
+	csrfGroup.Use(middlewear.CsrfToken)
 
 	activeCsrfGroup := csrfGroup.Group("")
-	activeCsrfGroup.Use(activeHand)
+	activeCsrfGroup.Use(middlewear.UserActive)
 
 	engine.GET("/check", checkGet)
 
