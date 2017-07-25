6e6c142f47efc913b46e19a107b60af6e122f811
Zachary Huff <zach.huff.386@gmail.com>
2017-07-25 01:46:32 -0400

    Add certificate actions

---
+++ www/app/actions/CertificateActions.ts
@@ -0,0 +1,124 @@
+/// <reference path="../References.d.ts"/>
+import * as SuperAgent from 'superagent';
+import Dispatcher from '../dispatcher/Dispatcher';
+import EventDispatcher from '../dispatcher/EventDispatcher';
+import * as Alert from '../Alert';
+import * as Csrf from '../Csrf';
+import Loader from '../Loader';
+import * as CertificateTypes from '../types/CertificateTypes';
+import * as MiscUtils from '../utils/MiscUtils';
+
+let syncId: string;
+
+export function sync(): Promise<void> {
+	let curSyncId = MiscUtils.uuid();
+	syncId = curSyncId;
+
+	let loader = new Loader().loading();
+
+	return new Promise<void>((resolve, reject): void => {
+		SuperAgent
+			.get('/certificate')
+			.set('Accept', 'application/json')
+			.set('Csrf-Token', Csrf.token)
+			.end((err: any, res: SuperAgent.Response): void => {
+				loader.done();
+
+				if (curSyncId !== syncId) {
+					resolve();
+					return;
+				}
+
+				if (err) {
+					Alert.errorRes(res, 'Failed to load certificates');
+					reject(err);
+					return;
+				}
+
+				Dispatcher.dispatch({
+					type: CertificateTypes.SYNC,
+					data: {
+						certificates: res.body,
+					},
+				});
+
+				resolve();
+			});
+	});
+}
+
+export function commit(cert: CertificateTypes.Certificate): Promise<void> {
+	let loader = new Loader().loading();
+
+	return new Promise<void>((resolve, reject): void => {
+		SuperAgent
+			.put('/certificate/' + cert.id)
+			.send(cert)
+			.set('Accept', 'application/json')
+			.set('Csrf-Token', Csrf.token)
+			.end((err: any, res: SuperAgent.Response): void => {
+				loader.done();
+
+				if (err) {
+					Alert.errorRes(res, 'Failed to save certificate');
+					reject(err);
+					return;
+				}
+
+				resolve();
+			});
+	});
+}
+
+export function create(cert: CertificateTypes.Certificate): Promise<void> {
+	let loader = new Loader().loading();
+
+	return new Promise<void>((resolve, reject): void => {
+		SuperAgent
+			.post('/certificate')
+			.send(cert)
+			.set('Accept', 'application/json')
+			.set('Csrf-Token', Csrf.token)
+			.end((err: any, res: SuperAgent.Response): void => {
+				loader.done();
+
+				if (err) {
+					Alert.errorRes(res, 'Failed to create certificate');
+					reject(err);
+					return;
+				}
+
+				resolve();
+			});
+	});
+}
+
+export function remove(certId: string): Promise<void> {
+	let loader = new Loader().loading();
+
+	return new Promise<void>((resolve, reject): void => {
+		SuperAgent
+			.delete('/certificate/' + certId)
+			.set('Accept', 'application/json')
+			.set('Csrf-Token', Csrf.token)
+			.end((err: any, res: SuperAgent.Response): void => {
+				loader.done();
+
+				if (err) {
+					Alert.errorRes(res, 'Failed to delete certificates');
+					reject(err);
+					return;
+				}
+
+				resolve();
+			});
+	});
+}
+
+EventDispatcher.register((action: CertificateTypes.CertificateDispatch) => {
+	switch (action.type) {
+		case CertificateTypes.CHANGE:
+			sync();
+			break;
+	}
+});
