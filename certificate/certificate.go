3accc22095c0a52158ba96368103d22deeefffb0
Zachary Huff <zach.huff.386@gmail.com>
2017-07-26 00:57:30 -0400

    Add certificate info

--- certificate/certificate.go
+++ certificate/certificate.go
@@ -2,6 +2,8 @@ package certificate
 
 import (
 	"crypto/md5"
+	"crypto/x509"
+	"encoding/pem"
 	"fmt"
 	"github.com/dropbox/godropbox/container/set"
 	"github.com/dropbox/godropbox/errors"
@@ -11,7 +13,17 @@ import (
 	"github.com/pritunl/pritunl-zero/utils"
 	"gopkg.in/mgo.v2/bson"
 	"io"
+	"time"
 )
+
+type Info struct {
+	Hash         string    `bson:"hash" json:"hash"`
+	SignatureAlg string    `bson:"signature_alg" json:"signature_alg"`
+	PublicKeyAlg string    `bson:"public_key_alg" json:"public_key_alg"`
+	IssuedOn     time.Time `bson:"issued_on" json:"issued_on"`
+	ExpiresOn    time.Time `bson:"expires_on" json:"expires_on"`
+	DnsNames     []string  `bson:"dns_names" json:"dns_names"`
+}
 
 type Certificate struct {
 	Id          bson.ObjectId `bson:"_id,omitempty" json:"id"`
@@ -19,6 +31,7 @@ type Certificate struct {
 	Type        string        `bson:"type" json:"type"`
 	Key         string        `bson:"key" json:"key"`
 	Certificate string        `bson:"certificate" json:"certificate"`
+	Info        *Info         `bson:"info" json:"info"`
 	AcmeHash    string        `bson:"acme_hash" json:"acme_hash"`
 	AcmeAccount string        `bson:"acme_account" json:"acme_account"`
 	AcmeDomains []string      `bson:"acme_domains" json:"acme_domains"`
@@ -35,6 +48,64 @@ func (c *Certificate) Validate(db *database.Database) (
 	if c.AcmeDomains == nil {
 		c.AcmeDomains = []string{}
 	}
+
+	return
+}
+
+func (c *Certificate) UpdateInfo() (err error) {
+	hash := c.Hash()
+
+	if c.Info != nil && hash == c.Info.Hash {
+		return
+	}
+
+	certBlock, _ := pem.Decode([]byte(c.Certificate))
+	if certBlock == nil {
+		c.Info = nil
+		err = &errortypes.ParseError{
+			errors.New("certificate: Failed to decode certificate"),
+		}
+		return
+	}
+
+	cert, err := x509.ParseCertificate(certBlock.Bytes)
+	if err != nil {
+		c.Info = nil
+		err = &errortypes.ParseError{
+			errors.Wrap(err, "certificate: Failed to parse certificate"),
+		}
+		return
+	}
+
+	publicKeyAlg := ""
+	switch cert.PublicKeyAlgorithm {
+	case x509.RSA:
+		publicKeyAlg = "RSA"
+		break
+	case x509.DSA:
+		publicKeyAlg = "DSA"
+		break
+	case x509.ECDSA:
+		publicKeyAlg = "ECDSA"
+		break
+	default:
+		publicKeyAlg = "Unknown"
+	}
+
+	dnsNames := cert.DNSNames
+	if len(dnsNames) == 0 && cert.Subject.CommonName != "" {
+		dnsNames = append(dnsNames, cert.Subject.CommonName)
+	}
+
+	info := &Info{
+		Hash:         hash,
+		SignatureAlg: cert.SignatureAlgorithm.String(),
+		PublicKeyAlg: publicKeyAlg,
+		IssuedOn:     cert.NotBefore,
+		ExpiresOn:    cert.NotAfter,
+		DnsNames:     dnsNames,
+	}
+	c.Info = info
 
 	return
 }
