a2d6a58aaac77e710f3ab5c48dfb22ab9877ea71
Zachary Huff <zach.huff.386@gmail.com>
2017-08-02 07:17:53 -0400

    Add fitlers to logger writer

--- logger/writer.go
+++ logger/writer.go
@@ -2,17 +2,29 @@ package logger
 
 import (
 	"github.com/Sirupsen/logrus"
+	"strings"
 )
 
 type ErrorWriter struct {
 	Message string
 	Fields  logrus.Fields
+	Filters []string
 }
 
 func (w *ErrorWriter) Write(input []byte) (n int, err error) {
 	n = len(input)
 
-	w.Fields["err"] = string(input)
+	inputStr := string(input)
+
+	if w.Filters != nil {
+		for _, filter := range w.Filters {
+			if strings.Contains(inputStr, filter) {
+				return
+			}
+		}
+	}
+
+	w.Fields["err"] = inputStr
 	logrus.WithFields(w.Fields).Error(w.Message)
 
 	return
