26d5eaa7afd71349973853d936033ac618732236
Zachary Huff <zach.huff.386@gmail.com>
2017-08-03 02:27:26 -0400

    Change static root

--- constants/constants.go
+++ constants/constants.go
@@ -6,17 +6,17 @@ import (
 )
 
 const (
-	Version                = "1.0.618.26"
-	DatabaseVersion        = 1
-	ConfPath               = "/etc/pritunl-zero.json"
-	LogPath                = "/var/log/pritunl-zero.log"
-	TempPath               = "/tmp/pritunl-zero"
-	Production             = true
-	BuildTest              = false
-	StaticRoot             = "www/dist"
-	StaticTestingRoot      = "www"
-	StaticCache            = false
-	RetryDelay             = 3 * time.Second
+	Version           = "1.0.618.26"
+	DatabaseVersion   = 1
+	ConfPath          = "/etc/pritunl-zero.json"
+	LogPath           = "/var/log/pritunl-zero.log"
+	TempPath          = "/tmp/pritunl-zero"
+	Production        = true
+	BuildTest         = false
+	StaticRoot        = "/usr/share/pritunl-zero/www"
+	StaticTestingRoot = "www"
+	StaticCache       = false
+	RetryDelay        = 3 * time.Second
 )
 
 var (
