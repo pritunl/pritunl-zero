f04edc57832fd48146acea2057765e0c4a36a930
Zachary Huff <zach.huff.386@gmail.com>
2017-08-05 04:41:24 -0400

    Disable cache for phandler index

--- phandlers/static.go
+++ phandlers/static.go
@@ -2,22 +2,16 @@ package phandlers
 
 import (
 	"github.com/gin-gonic/gin"
-	"github.com/pritunl/pritunl-zero/constants"
 	"strings"
 )
 
 func staticIndexGet(c *gin.Context) {
 	file := index
 
-	if constants.StaticCache {
-		c.Writer.Header().Add("Cache-Control", "public, max-age=86400")
-		c.Writer.Header().Add("ETag", file.Hash)
-	} else {
-		c.Writer.Header().Add("Cache-Control",
-			"no-cache, no-store, must-revalidate")
-		c.Writer.Header().Add("Pragma", "no-cache")
-		c.Writer.Header().Add("Expires", "0")
-	}
+	c.Writer.Header().Add("Cache-Control",
+		"no-cache, no-store, must-revalidate")
+	c.Writer.Header().Add("Pragma", "no-cache")
+	c.Writer.Header().Add("Expires", "0")
 
 	if strings.Contains(c.Request.Header.Get("Accept-Encoding"), "gzip") {
 		c.Writer.Header().Add("Content-Encoding", "gzip")
