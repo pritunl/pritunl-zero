d3e8dbbdfae39887f0a8e38630ca2fa3c2ee5fc8
Zachary Huff <zach.huff.386@gmail.com>
2017-08-07 23:34:03 -0400

    Add audit

---
+++ audit/audit.go
@@ -0,0 +1,15 @@
+package audit
+
+import (
+	"github.com/pritunl/pritunl-zero/agent"
+	"gopkg.in/mgo.v2/bson"
+	"time"
+)
+
+type Event struct {
+	Id        bson.ObjectId `bson:"_id" json:"id"`
+	User      bson.ObjectId `bson:"user" json:"user"`
+	Timestamp time.Time     `bson:"timestamp" json:"timestamp"`
+	Type      string        `bson:"type" json:"type"`
+	Agent     *agent.Agent  `bson:"agent" json:"agent"`
+}
