abda97782023ed32a7fd0dcb69f3e74fdde1dbaa
Zachary Huff <zach.huff.386@gmail.com>
2017-08-12 18:20:08 -0400

    Add agent types

---
+++ www/app/types/AgentTypes.ts
@@ -0,0 +1,17 @@
+/// <reference path="../References.d.ts"/>
+
+export interface Agent {
+	operating_system?: string;
+	browser?: string;
+	ip?: string;
+	isp?: string;
+	continent?: string;
+	continent_code?: string;
+	country?: string;
+	country_code?: string;
+	region?: string;
+	region_code?: string;
+	city?: string;
+	latitude?: number;
+	longitude?: number;
+}
