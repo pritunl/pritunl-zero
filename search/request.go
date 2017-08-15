c30c7e30c7b436f130971fffe72866312b8edd62
Zachary Huff <zach.huff.386@gmail.com>
2017-08-15 18:29:12 -0400

    Add search request

---
+++ search/request.go
@@ -0,0 +1,27 @@
+package search
+
+import (
+	"net/http"
+	"net/url"
+	"time"
+)
+
+type Request struct {
+	User      string      `json:"user"`
+	Session   string      `json:"session"`
+	Address   string      `json:"address"`
+	Timestamp time.Time   `json:"timestamp"`
+	Path      string      `json:"path"`
+	Query     url.Values  `json:"query"`
+	Header    http.Header `json:"header"`
+	Body      string      `json:"body"`
+}
+
+func (r *Request) Index() (err error) {
+	err = Index("zero-requests", "request", r)
+	if err != nil {
+		return
+	}
+
+	return
+}
