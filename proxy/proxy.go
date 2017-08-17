c859e7a6e94275b7a07af42cfb99045c567ad450
Zachary Huff <zach.huff.386@gmail.com>
2017-08-17 02:52:10 -0400

    Check settings in proxy elasticsearch

--- proxy/proxy.go
+++ proxy/proxy.go
@@ -7,6 +7,7 @@ import (
 	"github.com/pritunl/pritunl-zero/node"
 	"github.com/pritunl/pritunl-zero/search"
 	"github.com/pritunl/pritunl-zero/service"
+	"github.com/pritunl/pritunl-zero/settings"
 	"github.com/pritunl/pritunl-zero/utils"
 	"gopkg.in/mgo.v2/bson"
 	"math/rand"
@@ -126,21 +127,23 @@ func (p *Proxy) ServeHTTP(w http.ResponseWriter, r *http.Request) bool {
 		return false
 	}
 
-	index := search.Request{
-		User:      usr.Id.Hex(),
-		Session:   sess.Id,
-		Address:   clientIp.String(),
-		Timestamp: time.Now(),
-		Path:      r.URL.Path,
-		Query:     r.URL.Query(),
-		Header:    r.Header,
-		Body:      "", // TODO
-	}
+	if settings.Elastic.ProxyRequests {
+		index := search.Request{
+			User:      usr.Id.Hex(),
+			Session:   sess.Id,
+			Address:   clientIp.String(),
+			Timestamp: time.Now(),
+			Path:      r.URL.Path,
+			Query:     r.URL.Query(),
+			Header:    r.Header,
+			Body:      "", // TODO
+		}
 
-	err = index.Index()
-	if err != nil {
-		WriteError(w, r, 500, err)
-		return true
+		err = index.Index()
+		if err != nil {
+			WriteError(w, r, 500, err)
+			return true
+		}
 	}
 
 	if wsProxies != nil && r.Header.Get("Upgrade") == "websocket" {
