f87aeea9b4d5e6d584b90847b9bdef4db35fe917
Zachary Huff <zach.huff.386@gmail.com>
2017-08-29 00:40:49 -0400

    Change default max duration in auth settings

--- settings/auth.go
+++ settings/auth.go
@@ -26,7 +26,7 @@ type auth struct {
 	Id          string      `bson:"_id"`
 	Server      string      `bson:"server" default:"https://auth.pritunl.com"`
 	Expire      int         `bson:"expire" json:"expire" default:"72"`
-	MaxDuration int         `bson:"max_duration" json:"max_duration" default:"0"`
+	MaxDuration int         `bson:"max_duration" json:"max_duration" default:"24"`
 	Providers   []*Provider `bson:"providers"`
 }
 
