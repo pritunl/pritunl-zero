43bff4905ff2d83eb16b0a0ded04650a0a106fd3
Zachary Huff <zach.huff.386@gmail.com>
2017-09-13 22:16:42 -0400

    Change help in certificate

--- www/app/components/Certificate.tsx
+++ www/app/components/Certificate.tsx
@@ -296,7 +296,7 @@ export default class Certificate extends React.Component<Props, State> {
 					<PageTextArea
 						readOnly={cert.type !== 'text'}
 						label="Certificate Chain"
-						help="Certificate followed by certificate chain"
+						help="Certificate followed by certificate chain in PEM formatg"
 						placeholder="Certificate chain"
 						rows={6}
 						value={cert.certificate}
