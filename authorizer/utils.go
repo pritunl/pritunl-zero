01bd3f34c58e064e9de37c6a3a77f1760b77775d
Zachary Huff <zach.huff.386@gmail.com>
2017-10-13 23:13:46 -0400

    Add authorizer utils

---
+++ authorizer/utils.go
@@ -0,0 +1,42 @@
+package authorizer
+
+import (
+	"github.com/pritunl/pritunl-zero/auth"
+	"github.com/pritunl/pritunl-zero/database"
+	"github.com/pritunl/pritunl-zero/service"
+	"net/http"
+)
+
+func Authorize(db *database.Database, w http.ResponseWriter,
+	r *http.Request) (authr *Authorizer, err error) {
+
+	cook, sess, err := auth.CookieSession(db, w, r)
+	if err != nil {
+		return
+	}
+
+	authr = &Authorizer{
+		isProxy: false,
+		cook:    cook,
+		sess:    sess,
+	}
+
+	return
+}
+
+func AuthorizeProxy(db *database.Database, srvc *service.Service,
+	w http.ResponseWriter, r *http.Request) (authr *Authorizer, err error) {
+
+	cook, sess, err := auth.CookieSessionProxy(db, srvc, w, r)
+	if err != nil {
+		return
+	}
+
+	authr = &Authorizer{
+		isProxy: true,
+		cook:    cook,
+		sess:    sess,
+	}
+
+	return
+}
