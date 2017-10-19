381ae9265f0a2c9a46504ca8e1758de5b40ec60c
Zachary Huff <zach.huff.386@gmail.com>
2017-10-18 20:42:06 -0400

    Add signature parse

---
+++ signature/utils.go
@@ -0,0 +1,33 @@
+package signature
+
+import (
+	"github.com/dropbox/godropbox/errors"
+	"github.com/pritunl/pritunl-zero/errortypes"
+	"strconv"
+	"time"
+)
+
+func Parse(token, sigStr, timeStr, nonce, method, path string) (
+	sig *Signature, err error) {
+
+	timestampInt, _ := strconv.ParseInt(timeStr, 10, 64)
+	if timestampInt == 0 {
+		err = errortypes.ApiError{
+			errors.New("signature: Invalid authentication timestamp"),
+		}
+		return
+	}
+
+	timestamp := time.Unix(timestampInt, 0)
+
+	sig = &Signature{
+		Token:     token,
+		Nonce:     nonce,
+		Timestamp: timestamp,
+		Signature: sigStr,
+		Method:    method,
+		Path:      path,
+	}
+
+	return
+}
