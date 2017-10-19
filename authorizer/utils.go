6b445a89cc69b8dbe1a75feec2f9e60321c93519
Zachary Huff <zach.huff.386@gmail.com>
2017-10-18 21:55:14 -0400

    Check api headers in authorizer utils

--- authorizer/utils.go
+++ authorizer/utils.go
@@ -4,11 +4,44 @@ import (
 	"github.com/pritunl/pritunl-zero/auth"
 	"github.com/pritunl/pritunl-zero/database"
 	"github.com/pritunl/pritunl-zero/service"
+	"github.com/pritunl/pritunl-zero/signature"
 	"net/http"
 )
 
 func Authorize(db *database.Database, w http.ResponseWriter,
 	r *http.Request) (authr *Authorizer, err error) {
+
+	token := r.Header.Get("Pritunl-Zero-Token")
+	sigStr := r.Header.Get("Pritunl-Zero-Signature")
+
+	if token != "" && sigStr != "" {
+		timestamp := r.Header.Get("Pritunl-Zero-Timestamp")
+		nonce := r.Header.Get("Pritunl-Zero-Nonce")
+
+		sig, e := signature.Parse(
+			token,
+			sigStr,
+			timestamp,
+			nonce,
+			r.Method,
+			r.URL.Path,
+		)
+		if e != nil {
+			err = e
+			return
+		}
+
+		err = sig.Validate(db)
+		if err != nil {
+			return
+		}
+
+		authr = &Authorizer{
+			isProxy: false,
+			sig:     sig,
+		}
+		return
+	}
 
 	cook, sess, err := auth.CookieSession(db, w, r)
 	if err != nil {
