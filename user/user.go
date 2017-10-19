9ace38d9524d789c9d25e9437cf736b3f7406dbb
Zachary Huff <zach.huff.386@gmail.com>
2017-10-18 20:35:12 -0400

    Change secret json in user

--- user/user.go
+++ user/user.go
@@ -21,7 +21,7 @@ type User struct {
 	Username      string        `bson:"username" json:"username"`
 	Password      string        `bson:"password" json:"-"`
 	Token         string        `bson:"token" json:"token"`
-	Secret        string        `bson:"secret" json:"-"`
+	Secret        string        `bson:"secret" json:"secret"`
 	Theme         string        `bson:"theme" json:"-"`
 	LastActive    time.Time     `bson:"last_active" json:"last_active"`
 	LastSync      time.Time     `bson:"last_sync" json:"last_sync"`
