dc5cc4e6ccadcf1ec8c80876727a069af2d96ae3
Zachary Huff <zach.huff.386@gmail.com>
2017-10-23 03:23:01 -0400

    Move auth state

--- auth/handler.go
+++ auth/handler.go
@@ -20,64 +20,8 @@ import (
 	"gopkg.in/mgo.v2/bson"
 	"net/http"
 	"net/url"
-	"sort"
 	"strings"
 )
-
-type StateProvider struct {
-	Id    interface{} `json:"id"`
-	Type  string      `json:"type"`
-	Label string      `json:"label"`
-}
-
-type StateProviders []*StateProvider
-
-func (s StateProviders) Len() int {
-	return len(s)
-}
-
-func (s StateProviders) Swap(i, j int) {
-	s[i], s[j] = s[j], s[i]
-}
-
-func (s StateProviders) Less(i, j int) bool {
-	return s[i].Label < s[j].Label
-}
-
-type State struct {
-	Providers StateProviders `json:"providers"`
-}
-
-func GetState() (state *State) {
-	state = &State{
-		Providers: StateProviders{},
-	}
-
-	google := false
-
-	for _, provider := range settings.Auth.Providers {
-		prv := &StateProvider{
-			Type:  provider.Type,
-			Label: provider.Label,
-		}
-
-		if provider.Type == Google {
-			if google {
-				continue
-			}
-			google = true
-			prv.Id = Google
-		} else {
-			prv.Id = provider.Id
-		}
-
-		state.Providers = append(state.Providers, prv)
-	}
-
-	sort.Sort(state.Providers)
-
-	return
-}
 
 func Local(db *database.Database, username, password string) (
 	usr *user.User, errData *errortypes.ErrorData, err error) {
---
+++ auth/state.go
@@ -0,0 +1,61 @@
+package auth
+
+import (
+	"github.com/pritunl/pritunl-zero/settings"
+	"sort"
+)
+
+type StateProvider struct {
+	Id    interface{} `json:"id"`
+	Type  string      `json:"type"`
+	Label string      `json:"label"`
+}
+
+type StateProviders []*StateProvider
+
+func (s StateProviders) Len() int {
+	return len(s)
+}
+
+func (s StateProviders) Swap(i, j int) {
+	s[i], s[j] = s[j], s[i]
+}
+
+func (s StateProviders) Less(i, j int) bool {
+	return s[i].Label < s[j].Label
+}
+
+type State struct {
+	Providers StateProviders `json:"providers"`
+}
+
+func GetState() (state *State) {
+	state = &State{
+		Providers: StateProviders{},
+	}
+
+	google := false
+
+	for _, provider := range settings.Auth.Providers {
+		prv := &StateProvider{
+			Type:  provider.Type,
+			Label: provider.Label,
+		}
+
+		if provider.Type == Google {
+			if google {
+				continue
+			}
+			google = true
+			prv.Id = Google
+		} else {
+			prv.Id = provider.Id
+		}
+
+		state.Providers = append(state.Providers, prv)
+	}
+
+	sort.Sort(state.Providers)
+
+	return
+}
