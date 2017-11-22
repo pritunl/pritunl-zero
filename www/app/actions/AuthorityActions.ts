4b7ed263a7cf9c6f1739b47e4fd87d3c07b39d21
Zachary Huff <zach.huff.386@gmail.com>
2017-11-21 19:27:46 -0500

    Add authority actions

---
+++ www/app/actions/AuthorityActions.ts
@@ -0,0 +1,148 @@
+/// <reference path="../References.d.ts"/>
+import * as SuperAgent from 'superagent';
+import Dispatcher from '../dispatcher/Dispatcher';
+import EventDispatcher from '../dispatcher/EventDispatcher';
+import * as Alert from '../Alert';
+import * as Csrf from '../Csrf';
+import Loader from '../Loader';
+import * as AuthorityTypes from '../types/AuthorityTypes';
+import * as MiscUtils from '../utils/MiscUtils';
+
+let syncId: string;
+
+export function sync(): Promise<void> {
+	let curSyncId = MiscUtils.uuid();
+	syncId = curSyncId;
+
+	let loader = new Loader().loading();
+
+	return new Promise<void>((resolve, reject): void => {
+		SuperAgent
+			.get('/authority')
+			.set('Accept', 'application/json')
+			.set('Csrf-Token', Csrf.token)
+			.end((err: any, res: SuperAgent.Response): void => {
+				loader.done();
+
+				if (res.status === 401) {
+					window.location.href = '/login';
+					resolve();
+					return;
+				}
+
+				if (curSyncId !== syncId) {
+					resolve();
+					return;
+				}
+
+				if (err) {
+					Alert.errorRes(res, 'Failed to load authorities');
+					reject(err);
+					return;
+				}
+
+				Dispatcher.dispatch({
+					type: AuthorityTypes.SYNC,
+					data: {
+						authorities: res.body,
+					},
+				});
+
+				resolve();
+			});
+	});
+}
+
+export function commit(cert: AuthorityTypes.Authority): Promise<void> {
+	let loader = new Loader().loading();
+
+	return new Promise<void>((resolve, reject): void => {
+		SuperAgent
+			.put('/authority/' + cert.id)
+			.send(cert)
+			.set('Accept', 'application/json')
+			.set('Csrf-Token', Csrf.token)
+			.end((err: any, res: SuperAgent.Response): void => {
+				loader.done();
+
+				if (res.status === 401) {
+					window.location.href = '/login';
+					resolve();
+					return;
+				}
+
+				if (err) {
+					Alert.errorRes(res, 'Failed to save authority');
+					reject(err);
+					return;
+				}
+
+				resolve();
+			});
+	});
+}
+
+export function create(cert: AuthorityTypes.Authority): Promise<void> {
+	let loader = new Loader().loading();
+
+	return new Promise<void>((resolve, reject): void => {
+		SuperAgent
+			.post('/authority')
+			.send(cert)
+			.set('Accept', 'application/json')
+			.set('Csrf-Token', Csrf.token)
+			.end((err: any, res: SuperAgent.Response): void => {
+				loader.done();
+
+				if (res.status === 401) {
+					window.location.href = '/login';
+					resolve();
+					return;
+				}
+
+				if (err) {
+					Alert.errorRes(res, 'Failed to create authority');
+					reject(err);
+					return;
+				}
+
+				resolve();
+			});
+	});
+}
+
+export function remove(certId: string): Promise<void> {
+	let loader = new Loader().loading();
+
+	return new Promise<void>((resolve, reject): void => {
+		SuperAgent
+			.delete('/authority/' + certId)
+			.set('Accept', 'application/json')
+			.set('Csrf-Token', Csrf.token)
+			.end((err: any, res: SuperAgent.Response): void => {
+				loader.done();
+
+				if (res.status === 401) {
+					window.location.href = '/login';
+					resolve();
+					return;
+				}
+
+				if (err) {
+					Alert.errorRes(res, 'Failed to delete authorities');
+					reject(err);
+					return;
+				}
+
+				resolve();
+			});
+	});
+}
+
+EventDispatcher.register((action: AuthorityTypes.AuthorityDispatch) => {
+	switch (action.type) {
+		case AuthorityTypes.CHANGE:
+			sync();
+			break;
+	}
+});
