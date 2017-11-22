22738202c81afa21b62498ce88a796ec662b44e7
Zachary Huff <zach.huff.386@gmail.com>
2017-11-21 19:51:44 -0500

    Add authority component

---
+++ www/app/components/Authority.tsx
@@ -0,0 +1,326 @@
+/// <reference path="../References.d.ts"/>
+import * as React from 'react';
+import * as AuthorityTypes from '../types/AuthorityTypes';
+import * as AuthorityActions from '../actions/AuthorityActions';
+import PageInput from './PageInput';
+import PageInputButton from './PageInputButton';
+import PageInfo from './PageInfo';
+import PageSave from './PageSave';
+import ConfirmButton from './ConfirmButton';
+import Help from './Help';
+
+interface Props {
+	authority: AuthorityTypes.AuthorityRo;
+}
+
+interface State {
+	disabled: boolean;
+	changed: boolean;
+	message: string;
+	authority: AuthorityTypes.Authority;
+	addRole: string;
+}
+
+const css = {
+	card: {
+		position: 'relative',
+		padding: '10px 10px 0 10px',
+		marginBottom: '5px',
+	} as React.CSSProperties,
+	remove: {
+		position: 'absolute',
+		top: '5px',
+		right: '5px',
+	} as React.CSSProperties,
+	item: {
+		margin: '9px 5px 0 5px',
+		height: '20px',
+	} as React.CSSProperties,
+	itemsLabel: {
+		display: 'block',
+	} as React.CSSProperties,
+	itemsAdd: {
+		margin: '8px 0 15px 0',
+	} as React.CSSProperties,
+	group: {
+		flex: 1,
+		minWidth: '250px',
+	} as React.CSSProperties,
+	save: {
+		paddingBottom: '10px',
+	} as React.CSSProperties,
+	label: {
+		width: '100%',
+		maxWidth: '280px',
+	} as React.CSSProperties,
+	inputGroup: {
+		width: '100%',
+	} as React.CSSProperties,
+	protocol: {
+		flex: '0 1 auto',
+	} as React.CSSProperties,
+	port: {
+		flex: '1',
+	} as React.CSSProperties,
+};
+
+export default class Authority extends React.Component<Props, State> {
+	constructor(props: any, context: any) {
+		super(props, context);
+		this.state = {
+			disabled: false,
+			changed: false,
+			message: '',
+			authority: null,
+			addRole: null,
+		};
+	}
+
+	set(name: string, val: any): void {
+		let authority: any;
+
+		if (this.state.changed) {
+			authority = {
+				...this.state.authority,
+			};
+		} else {
+			authority = {
+				...this.props.authority,
+			};
+		}
+
+		authority[name] = val;
+
+		this.setState({
+			...this.state,
+			changed: true,
+			authority: authority,
+		});
+	}
+
+	onSave = (): void => {
+		this.setState({
+			...this.state,
+			disabled: true,
+		});
+		AuthorityActions.commit(this.state.authority).then((): void => {
+			this.setState({
+				...this.state,
+				message: 'Your changes have been saved',
+				changed: false,
+				disabled: false,
+			});
+
+			setTimeout((): void => {
+				if (!this.state.changed) {
+					this.setState({
+						...this.state,
+						message: '',
+						changed: false,
+						authority: null,
+					});
+				}
+			}, 3000);
+		}).catch((): void => {
+			this.setState({
+				...this.state,
+				message: '',
+				disabled: false,
+			});
+		});
+	}
+
+	onDelete = (): void => {
+		this.setState({
+			...this.state,
+			disabled: true,
+		});
+		AuthorityActions.remove(this.props.authority.id).then((): void => {
+			this.setState({
+				...this.state,
+				disabled: false,
+			});
+		}).catch((): void => {
+			this.setState({
+				...this.state,
+				disabled: false,
+			});
+		});
+	}
+
+	onAddRole = (): void => {
+		let authority: AuthorityTypes.Authority;
+
+		if (this.state.changed) {
+			authority = {
+				...this.state.authority,
+			};
+		} else {
+			authority = {
+				...this.props.authority,
+			};
+		}
+
+		let roles = [
+			...authority.roles,
+		];
+
+		if (!this.state.addRole) {
+			return;
+		}
+
+		if (roles.indexOf(this.state.addRole) === -1) {
+			roles.push(this.state.addRole);
+		}
+
+		roles.sort();
+
+		authority.roles = roles;
+
+		this.setState({
+			...this.state,
+			changed: true,
+			message: '',
+			addRole: '',
+			authority: authority,
+		});
+	}
+
+	onRemoveRole(role: string): void {
+		let authority: AuthorityTypes.Authority;
+
+		if (this.state.changed) {
+			authority = {
+				...this.state.authority,
+			};
+		} else {
+			authority = {
+				...this.props.authority,
+			};
+		}
+
+		let roles = [
+			...authority.roles,
+		];
+
+		let i = roles.indexOf(role);
+		if (i === -1) {
+			return;
+		}
+
+		roles.splice(i, 1);
+
+		authority.roles = roles;
+
+		this.setState({
+			...this.state,
+			changed: true,
+			message: '',
+			addRole: '',
+			authority: authority,
+		});
+	}
+
+	render(): JSX.Element {
+		let authority: AuthorityTypes.Authority = this.state.authority ||
+			this.props.authority;
+
+		let roles: JSX.Element[] = [];
+		for (let role of authority.roles) {
+			roles.push(
+				<div
+					className="pt-tag pt-tag-removable pt-intent-primary"
+					style={css.item}
+					key={role}
+				>
+					{role}
+					<button
+						className="pt-tag-remove"
+						onMouseUp={(): void => {
+							this.onRemoveRole(role);
+						}}
+					/>
+				</div>,
+			);
+		}
+
+		return <div
+			className="pt-card"
+			style={css.card}
+		>
+			<div className="layout horizontal wrap">
+				<div style={css.group}>
+					<div style={css.remove}>
+						<ConfirmButton
+							className="pt-minimal pt-intent-danger pt-icon-cross"
+							progressClassName="pt-intent-danger"
+							confirmMsg="Confirm authority remove"
+							disabled={this.state.disabled}
+							onConfirm={this.onDelete}
+						/>
+					</div>
+					<PageInput
+						label="Name"
+						help="Name of authority"
+						type="text"
+						placeholder="Enter name"
+						value={authority.name}
+						onChange={(val): void => {
+							this.set('name', val);
+						}}
+					/>
+					<label className="pt-label">
+						Roles
+						<Help
+							title="Roles"
+							content="Roles associated with this authority."
+						/>
+						<div>
+							{roles}
+						</div>
+					</label>
+					<PageInputButton
+						buttonClass="pt-intent-success pt-icon-add"
+						label="Add"
+						type="text"
+						placeholder="Add role"
+						value={this.state.addRole}
+						onChange={(val): void => {
+							this.setState({
+								...this.state,
+								addRole: val,
+							});
+						}}
+						onSubmit={this.onAddRole}
+					/>
+				</div>
+				<div style={css.group}>
+					<PageInfo
+						fields={[
+							{
+								label: 'ID',
+								value: authority.id || 'None',
+							},
+						]}
+					/>
+				</div>
+			</div>
+			<PageSave
+				style={css.save}
+				hidden={!this.state.authority}
+				message={this.state.message}
+				changed={this.state.changed}
+				disabled={this.state.disabled}
+				light={true}
+				onCancel={(): void => {
+					this.setState({
+						...this.state,
+						changed: false,
+						authority: null,
+					});
+				}}
+				onSave={this.onSave}
+			/>
+		</div>;
+	}
+}
