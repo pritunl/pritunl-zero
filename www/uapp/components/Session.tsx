cb1ba7611c27e209cd409796b2f0298c355756be
Zachary Huff <zach.huff.386@gmail.com>
2017-11-23 04:12:08 -0500

    Add session component

---
+++ www/uapp/components/Session.tsx
@@ -0,0 +1,52 @@
+/// <reference path="../References.d.ts"/>
+import * as React from 'react';
+
+const css = {
+	body: {
+		padding: '0 10px',
+	} as React.CSSProperties,
+	description: {
+		opacity: 0.7,
+	} as React.CSSProperties,
+	buttons: {
+		marginTop: '15px',
+	} as React.CSSProperties,
+	button: {
+		margin: '5px',
+		width: '160px',
+	} as React.CSSProperties,
+};
+
+export default class Session extends React.Component<{}, {}> {
+	render(): JSX.Element {
+		return <div>
+			<div className="pt-non-ideal-state" style={css.body}>
+				<h4 className="pt-non-ideal-state-title">
+					Pritunl Zero User Console
+				</h4>
+				<span style={css.description}>
+					To validate an SSH key install the client and run "TODO"
+				</span>
+			</div>
+			<div
+				className="layout horizontal center-justified wrap"
+				style={css.buttons}
+			>
+				<a
+					className="pt-button pt-large pt-intent-primary pt-icon-download"
+					style={css.button}
+					href="https://docs.pritunl.com"
+				>
+					Install Client
+				</a>
+				<a
+					className="pt-button pt-large pt-intent-danger pt-icon-delete"
+					style={css.button}
+					href="/logout"
+				>
+					Logout
+				</a>
+			</div>
+		</div>;
+	}
+}
