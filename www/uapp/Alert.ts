79133f04f3033b2be1a6cd8a950dd199db6dcb4e
Zachary Huff <zach.huff.386@gmail.com>
2017-11-24 01:49:04 -0500

    Add timeout to alert

--- www/uapp/Alert.ts
+++ www/uapp/Alert.ts
@@ -6,28 +6,60 @@ let toaster = Blueprint.Toaster.create({
 	position: Blueprint.Position.BOTTOM,
 });
 
-export function info(message: string): void {
+export function success(message: string, timeout?: number): void {
+	if (timeout === undefined) {
+		timeout = 5000;
+	}
+
+	toaster.show({
+		intent: Blueprint.Intent.SUCCESS,
+		message: message,
+		timeout: timeout,
+	});
+}
+
+export function info(message: string, timeout?: number): void {
+	if (timeout === undefined) {
+		timeout = 5000;
+	}
+
 	toaster.show({
 		intent: Blueprint.Intent.PRIMARY,
 		message: message,
+		timeout: timeout,
 	});
 }
 
-export function warning(message: string): void {
+export function warning(message: string, timeout?: number): void {
+	if (timeout === undefined) {
+		timeout = 5000;
+	}
+
 	toaster.show({
 		intent: Blueprint.Intent.WARNING,
 		message: message,
+		timeout: timeout,
 	});
 }
 
-export function error(message: string): void {
+export function error(message: string, timeout?: number): void {
+	if (timeout === undefined) {
+		timeout = 5000;
+	}
+
 	toaster.show({
 		intent: Blueprint.Intent.DANGER,
 		message: message,
+		timeout: timeout,
 	});
 }
 
-export function errorRes(res: SuperAgent.Response, message: string): void {
+export function errorRes(res: SuperAgent.Response, message: string,
+		timeout?: number): void {
+	if (timeout === undefined) {
+		timeout = 5000;
+	}
+
 	try {
 		message = res.body.error_msg || message;
 	} catch(err) {
@@ -36,5 +68,6 @@ export function errorRes(res: SuperAgent.Response, message: string): void {
 	toaster.show({
 		intent: Blueprint.Intent.DANGER,
 		message: message,
+		timeout: timeout,
 	});
 }
