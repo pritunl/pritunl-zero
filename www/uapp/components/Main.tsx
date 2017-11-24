7e181d422a030b4e90ee7fc9de26a9639abb3cb6
Zachary Huff <zach.huff.386@gmail.com>
2017-11-23 22:31:43 -0500

    Add ssh token to main token

--- www/uapp/components/Main.tsx
+++ www/uapp/components/Main.tsx
@@ -19,9 +19,20 @@ const css = {
 
 export default class Main extends React.Component<{}, {}> {
 	render(): JSX.Element {
+		let sshToken = '';
+		let query = window.location.search.substring(1);
+		let vals = query.split('&');
+		for (let val of vals) {
+			let keyval = val.split('=');
+			console.log(val);
+			if (keyval[0] === 'ssh-token') {
+				sshToken = keyval[1];
+				break;
+			}
+		}
+
 		return <div className="pt-card pt-elevation-2" style={css.card}>
-			<Session/>
-			<Validate token="TODO"/>
+			{sshToken ? <Validate token={sshToken}/> : <Session/>}
 		</div>;
 	}
 }
