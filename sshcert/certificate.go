8be9d4691835b2aba81050a320a5574865117235
Zachary Huff <zach.huff.386@gmail.com>
2017-11-25 03:04:15 -0500

    Add info to ssh certificate

--- sshcert/certificate.go
+++ sshcert/certificate.go
@@ -2,17 +2,27 @@ package sshcert
 
 import (
 	"github.com/dropbox/godropbox/container/set"
+	"github.com/pritunl/pritunl-zero/agent"
 	"github.com/pritunl/pritunl-zero/database"
 	"gopkg.in/mgo.v2/bson"
 	"time"
 )
 
+type Info struct {
+	Serial     string    `bson:"serial" json:"serial"`
+	Expires    time.Time `bson:"expires" json:"expires"`
+	Principals []string  `bson:"principals" json:"principals"`
+	Extensions []string  `bson:"extensions" json:"extensions"`
+}
+
 type Certificate struct {
-	Id           bson.ObjectId   `bson:"_id,omitempty" json:"id"`
-	UserId       bson.ObjectId   `bson:"user_id" json:"user_id"`
-	AuthorityIds []bson.ObjectId `bson:"authority_ids" json:"authority_ids"`
-	Timestamp    time.Time       `bson:"timestamp" json:"timestamp"`
-	Certificates []string        `bson:"certificates" json:"certificates"`
+	Id               bson.ObjectId   `bson:"_id,omitempty" json:"id"`
+	UserId           bson.ObjectId   `bson:"user_id" json:"user_id"`
+	AuthorityIds     []bson.ObjectId `bson:"authority_ids" json:"authority_ids"`
+	Timestamp        time.Time       `bson:"timestamp" json:"timestamp"`
+	Certificates     []string        `bson:"certificates" json:"-"`
+	CertificatesInfo []*Info         `bson:"certificates_info" json:"certificates_info"`
+	Agent            *agent.Agent    `bson:"agent" json:"agent"`
 }
 
 func (c *Certificate) Commit(db *database.Database) (err error) {
