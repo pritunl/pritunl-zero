14318287f78d66d6fbe5357ef47f2b92936b7ee0
Zachary Huff <zach.huff.386@gmail.com>
2017-11-25 01:12:42 -0500

    Add info to authority types

--- www/app/types/AuthorityTypes.ts
+++ www/app/types/AuthorityTypes.ts
@@ -2,12 +2,19 @@
 export const SYNC = 'authority.sync';
 export const CHANGE = 'authority.change';
 
+export interface Info {
+	key_alg?: string;
+}
+
 export interface Authority {
 	id: string;
 	name?: string;
 	type?: string[];
+	info?: Info;
+	expire?: number;
 	match_roles?: boolean;
 	roles?: string[];
+	public_key?: string;
 }
 
 export type Authorities = Authority[];
