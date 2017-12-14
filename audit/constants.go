d8eeff9beecd15bb4b63b3ede00e10e6b328d118
Zachary Huff <zach.huff.386@gmail.com>
2017-12-13 22:07:35 -0500

    Add keybase to audit constants

--- audit/constants.go
+++ audit/constants.go
@@ -1,10 +1,12 @@
 package audit
 
 const (
-	AdminLogin       = "admin_login"
-	AdminLoginFailed = "admin_login_failed"
-	Login            = "login"
-	LoginFailed      = "login_failed"
-	SshAprove        = "ssh_aprove"
-	SshDeny          = "ssh_deny"
+	AdminLogin               = "admin_login"
+	AdminLoginFailed         = "admin_login_failed"
+	Login                    = "login"
+	LoginFailed              = "login_failed"
+	SshAprove                = "ssh_aprove"
+	SshDeny                  = "ssh_deny"
+	KeybaseAssociationAprove = "keybase_association_aprove"
+	KeybaseAssociationDeny   = "keybase_association_deny"
 )
