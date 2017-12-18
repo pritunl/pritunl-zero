4c7ac5f91b526d6c0e0887f73cf536a381b32635
Zachary Huff <zach.huff.386@gmail.com>
2017-12-18 01:12:05 -0500

    Add keybase info

---
+++ keybase/info.go
@@ -0,0 +1,106 @@
+package keybase
+
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/dropbox/godropbox/errors"
+	"github.com/pritunl/pritunl-zero/errortypes"
+	"net/http"
+	"time"
+)
+
+var (
+	client = &http.Client{
+		Timeout: 20 * time.Second,
+	}
+)
+
+type infoStatus struct {
+	Code int    `json:"code"`
+	Name string `json:"name"`
+}
+
+type infoBasics struct {
+	Username string `json:"username"`
+}
+
+type infoProof struct {
+	ProofType string `json:"proof_type"`
+	Name      string `json:"nametag"`
+}
+
+type infoProofTypes struct {
+	Github  []infoProof `json:"github"`
+	Twitter []infoProof `json:"twitter"`
+}
+
+type infoProofs struct {
+	ByProofType infoProofTypes `json:"by_proof_type"`
+}
+
+type infoPrimary struct {
+	Url    string `json:"url"`
+	Width  int    `json:"width"`
+	Height int    `json:"height"`
+	Source string `json:"source"`
+}
+
+type infoPictures struct {
+	Primary infoPrimary `json:"primary"`
+}
+
+type infoThem struct {
+	Basics   infoBasics   `json:"basics"`
+	Proofs   infoProofs   `json:"proofs_summary"`
+	Pictures infoPictures `json:"pictures"`
+}
+
+type infoResp struct {
+	Status infoStatus `json:"status"`
+	Them   infoThem   `json:"them"`
+}
+
+func getInfo(username string) (data *infoResp, err error) {
+	req, err := http.NewRequest(
+		"GET",
+		fmt.Sprintf("https://keybase.io/_/api/1.0/user/lookup.json?"+
+			"username=%s&fields=basics,proofs_summary,pictures", username),
+		nil,
+	)
+	if err != nil {
+		err = &errortypes.RequestError{
+			errors.Wrap(err, "keybase: Info request failed"),
+		}
+		return
+	}
+
+	resp, err := client.Do(req)
+	if err != nil {
+		err = &errortypes.RequestError{
+			errors.Wrap(err, "keybase: Info request failed"),
+		}
+		return
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != 200 {
+		err = &errortypes.RequestError{
+			errors.Wrapf(err, "keybase: Info request bad status %d",
+				resp.StatusCode),
+		}
+		return
+	}
+
+	data = &infoResp{}
+	err = json.NewDecoder(resp.Body).Decode(data)
+	if err != nil {
+		err = &errortypes.ParseError{
+			errors.Wrap(
+				err, "keybase: Failed to parse info response",
+			),
+		}
+		return
+	}
+
+	return
+}
