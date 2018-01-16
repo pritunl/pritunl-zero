d82fd7ea8a682fc75ff11c80353e2104b6e8f0a4
Zachary Huff <zach.huff.386@gmail.com>
2018-01-15 20:17:18 -0500

    Add secondary types to constants

--- secondary/constants.go
+++ secondary/constants.go
@@ -8,4 +8,10 @@ const (
 	Phone    = "phone"
 	Passcode = "passcode"
 	Sms      = "sms"
+
+	Admin     = "admin"
+	User      = "user"
+	Proxy     = "proxy"
+	Authority = "authority"
+	Keybase   = "keybase"
 )
