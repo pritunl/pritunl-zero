792d0930157b63e73864ea7ba995971fab21af3e
Zachary Huff <zach.huff.386@gmail.com>
2018-01-19 01:54:53 -0500

    Store redirect path in handler

--- phandlers/redirect.go
+++ phandlers/redirect.go
@@ -1,13 +1,16 @@
 package phandlers
 
 import (
+	"fmt"
 	"github.com/gin-gonic/gin"
+	"net/url"
 )
 
 func redirect(c *gin.Context) {
 	if c.Request.Header.Get("Upgrade") == "websocket" {
 		c.AbortWithStatus(404)
 	} else {
-		c.Redirect(302, "/")
+		c.Redirect(302, fmt.Sprintf("/?redirect_url=%s",
+			url.QueryEscape(c.Request.URL.Path)))
 	}
 }
