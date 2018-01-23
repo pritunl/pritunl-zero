229d517c440d159673868b2f8855a14561341c5d
Zachary Huff <zach.huff.386@gmail.com>
2018-01-22 19:25:10 -0500

    Add to fixed to utils

--- utils/math.go
+++ utils/math.go
@@ -1,5 +1,9 @@
 package utils
 
+import (
+	"math"
+)
+
 func Max(x, y int) int {
 	if x > y {
 		return x
@@ -12,4 +16,9 @@ func Min(x, y int) int {
 		return x
 	}
 	return y
+}
+
+func ToFixed(x float64, p int) float64 {
+	y := math.Pow(10, float64(p))
+	return float64(int(x*y+math.Copysign(0.5, x*y))) / y
 }
