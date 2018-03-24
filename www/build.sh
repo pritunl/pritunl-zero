4808d43bf8bf22c1879bd7752026b942da5a14e9
Zachary Huff <zach.huff.386@gmail.com>
2018-03-23 20:30:32 -0400

    Update to react 16

--- www/build.sh
+++ www/build.sh
@@ -3,14 +3,15 @@ rm -rf dist/static
 mkdir -p dist/static
 cp styles/global.css dist/static/
 cp node_modules/normalize.css/normalize.css dist/static/
-cp node_modules/@blueprintjs/core/dist/blueprint.css dist/static/
-cp node_modules/@blueprintjs/datetime/dist/blueprint-datetime.css dist/static/
-cp node_modules/@blueprintjs/core/resources/icons/icons-16.eot dist/static/
-cp node_modules/@blueprintjs/core/resources/icons/icons-16.ttf dist/static/
-cp node_modules/@blueprintjs/core/resources/icons/icons-16.woff dist/static/
-cp node_modules/@blueprintjs/core/resources/icons/icons-20.eot dist/static/
-cp node_modules/@blueprintjs/core/resources/icons/icons-20.ttf dist/static/
-cp node_modules/@blueprintjs/core/resources/icons/icons-20.woff dist/static/
+cp node_modules/@blueprintjs/core/lib/css/blueprint.css dist/static/
+cp node_modules/@blueprintjs/datetime/lib/css/blueprint-datetime.css dist/static/
+cp node_modules/@blueprintjs/icons/lib/css/blueprint-icons.css dist/static/
+cp node_modules/@blueprintjs/icons/resources/icons/icons-16.eot dist/static/
+cp node_modules/@blueprintjs/icons/resources/icons/icons-16.ttf dist/static/
+cp node_modules/@blueprintjs/icons/resources/icons/icons-16.woff dist/static/
+cp node_modules/@blueprintjs/icons/resources/icons/icons-20.eot dist/static/
+cp node_modules/@blueprintjs/icons/resources/icons/icons-20.ttf dist/static/
+cp node_modules/@blueprintjs/icons/resources/icons/icons-20.woff dist/static/
 cp jspm_packages/system.js dist/static/
 sed -i 's|../resources/icons/||g' dist/static/blueprint.css
 jspm bundle app/App.js
