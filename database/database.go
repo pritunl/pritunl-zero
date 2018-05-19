56199e984508e045ff73ed7ad61a8a8e84518530
Zachary Huff <zach.huff.386@gmail.com>
2018-05-19 13:03:01 -0400

    Add log message to database connect

--- database/database.go
+++ database/database.go
@@ -151,6 +151,10 @@ func Connect() (err error) {
 		return
 	}
 
+	logrus.WithFields(logrus.Fields{
+		"mongodb_host": mgoUrl.Host,
+	}).Info("database: Connecting to MongoDB server")
+
 	vals := mgoUrl.Query()
 	mgoSsl := vals.Get("ssl")
 	mgoSslCerts := vals.Get("ssl_ca_certs")
