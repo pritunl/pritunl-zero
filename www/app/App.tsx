72f289a2e1295998e4b2bf4e6aea24c23fa7616e
Zachary Huff <zach.huff.386@gmail.com>
2018-06-12 01:51:21 -0400

    Init alert in app

--- www/app/App.tsx
+++ www/app/App.tsx
@@ -3,11 +3,13 @@ import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import * as Blueprint from '@blueprintjs/core';
 import Main from './components/Main';
+import * as Alert from './Alert';
 import * as Event from './Event';
 import * as Csrf from './Csrf';
 
 Csrf.load().then((): void => {
 	Blueprint.FocusStyleManager.onlyShowFocusOnTabs();
+	Alert.init();
 	Event.init();
 
 	ReactDOM.render(
