f05ff451767bb7238c7f843362df496cdb5ade5a
Zachary Huff <zach.huff.386@gmail.com>
2018-06-13 16:02:03 -0400

    Add utils to uapp

---
+++ www/uapp/Constants.ts
@@ -0,0 +1,8 @@
+/// <reference path="./References.d.ts"/>
+import * as MobileDetect from 'mobile-detect';
+
+let md = new MobileDetect(window.navigator.userAgent);
+
+export const mobile = !!md.mobile();
+
+export const loadDelay = 700;
