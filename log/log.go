145c83f9c1e86b02e42d1c0b9e5ed180712efb2c
Zachary Huff <zach.huff.386@gmail.com>
2018-06-14 18:50:25 -0400

    Delay log event

--- log/log.go
+++ log/log.go
@@ -5,9 +5,12 @@ import (
 	"github.com/pritunl/pritunl-zero/database"
 	"github.com/pritunl/pritunl-zero/errortypes"
 	"github.com/pritunl/pritunl-zero/event"
+	"github.com/pritunl/pritunl-zero/requires"
 	"gopkg.in/mgo.v2/bson"
 	"time"
 )
+
+var published = false
 
 type Entry struct {
 	Id        bson.ObjectId          `bson:"_id,omitempty" json:"id"`
@@ -34,7 +37,34 @@ func (e *Entry) Insert(db *database.Database) (err error) {
 		return
 	}
 
-	event.PublishDispatch(db, "log.change")
+	published = true
 
 	return
+}
+
+func publish() {
+	db := database.GetDatabase()
+	defer db.Close()
+
+	event.PublishDispatch(db, "log.change")
+}
+
+func initSender() {
+	for {
+		time.Sleep(1500 * time.Millisecond)
+		if published {
+			published = false
+			publish()
+		}
+	}
+}
+
+func init() {
+	module := requires.New("log")
+	module.After("logger")
+
+	module.Handler = func() (err error) {
+		go initSender()
+		return
+	}
 }
