17f97f8a08ba9a291b9fb8f9b1483ee211371a5c
Zachary Huff <zach.huff.386@gmail.com>
2018-06-21 19:47:29 -0400

    Update hsm constants

--- authority/constants.go
+++ authority/constants.go
@@ -1,6 +1,8 @@
 package authority
 
 const (
-	Local      = "local"
-	PritunlHsm = "pritunl_hsm"
+	Local        = "local"
+	PritunlHsm   = "pritunl_hsm"
+	Connected    = "connected"
+	Disconnected = "disconnected"
 )
