57ff130d8e877e4b27681c71e1f158eba769f465
Zachary Huff <zach.huff.386@gmail.com>
2018-06-21 00:42:34 -0400

    Add hsm response timeout to system settings

--- settings/system.go
+++ settings/system.go
@@ -23,6 +23,7 @@ type system struct {
 	AcmeKeyAlgorithm     string `bson:"acme_key_algorithm" default:"rsa"`
 	SshPubKeyLen         int    `bson:"ssh_pub_key_len" default:"5000"`
 	SshHostTokenLen      int    `bson:"ssh_host_token_len" default:"10"`
+	HsmResponseTimeout   int    `bson:"hsm_response_timeout" default:"10"`
 }
 
 func newSystem() interface{} {
