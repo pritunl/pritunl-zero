c57a6e084daec827cbb3c57f6d82497ead6d5b2a
Zachary Huff <zach.huff.386@gmail.com>
2018-07-02 01:48:14 -0400

    Set state in uapp init

--- www/uapp/App.tsx
+++ www/uapp/App.tsx
@@ -2,6 +2,7 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import * as Blueprint from '@blueprintjs/core';
+import * as StateActions from './actions/StateActions';
 import Main from './components/Main';
 import * as Alert from './Alert';
 import * as Csrf from './Csrf';
@@ -11,6 +12,18 @@ document.body.className = 'pt-dark';
 Csrf.load().then((): void => {
 	Blueprint.FocusStyleManager.onlyShowFocusOnTabs();
 	Alert.init();
+
+	let query = window.location.search.substring(1);
+
+	let vals = query.split('&');
+	for (let val of vals) {
+		let keyval = val.split('=');
+		if (keyval[0] === 'ssh-token') {
+			StateActions.setSshToken(keyval[1]);
+		} else if (keyval[0] === 'device') {
+			StateActions.setSshDevice(keyval[1]);
+		}
+	}
 
 	ReactDOM.render(
 		<div><Main/></div>,
