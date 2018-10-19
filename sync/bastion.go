097f83562f4271bb638f08428bad3bae7c2b9fac
Zachary Huff <zach.huff.386@gmail.com>
2018-10-19 19:25:38 -0400

    Add bastion sync

--- sync/bastion.go
+++ sync/bastion.go
@@ -2,6 +2,9 @@ package sync
 
 import (
 	"github.com/Sirupsen/logrus"
+	"github.com/pritunl/pritunl-zero/authority"
+	"github.com/pritunl/pritunl-zero/bastion"
+	"github.com/pritunl/pritunl-zero/database"
 	"github.com/pritunl/pritunl-zero/node"
 	"github.com/pritunl/pritunl-zero/settings"
 	"github.com/pritunl/pritunl-zero/utils"
@@ -21,6 +24,37 @@ func bastionInit() (err error) {
 		"pull", settings.System.BastionDockerImage)
 	if err != nil {
 		return
+	}
+
+	return
+}
+
+func bastionSync() (err error) {
+	db := database.GetDatabase()
+	defer db.Close()
+
+	authrs := []*authority.Authority{}
+	nodeAuthrs := node.Self.Authorities
+
+	if nodeAuthrs != nil && len(nodeAuthrs) > 0 {
+		authrs, err = authority.GetMulti(db, nodeAuthrs)
+		if err != nil {
+			return
+		}
+	}
+
+	for _, authr := range authrs {
+		bast := bastion.Get(authr.Id)
+		if bast == nil || !bast.State() {
+			bast = bastion.New(authr.Id)
+
+			e := bast.Start(db, authr)
+			if e != nil {
+				logrus.WithFields(logrus.Fields{
+					"error": e,
+				}).Error("sync: Failed to start bastion")
+			}
+		}
 	}
 
 	return
@@ -50,6 +84,17 @@ func bastionRunner() {
 		}
 
 		break
+	}
+
+	for {
+		time.Sleep(1 * time.Second)
+
+		err := bastionSync()
+		if err != nil {
+			logrus.WithFields(logrus.Fields{
+				"error": err,
+			}).Error("sync: Failed to sync bastion host")
+		}
 	}
 }
 
