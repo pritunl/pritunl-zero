dff57ed47f49d058427c154faf736b0ea2387103
Zachary Huff <zach.huff.386@gmail.com>
2018-10-20 00:07:02 -0400

    Add docker utils to bastion

--- bastion/utils.go
+++ bastion/utils.go
@@ -17,3 +17,42 @@ func DockerMatchContainer(a, b string) bool {
 func DockerGetName(authrId bson.ObjectId) string {
 	return fmt.Sprintf("pritunl-bastion-%s", authrId.Hex())
 }
+
+func DockerGetRunning() (running map[string]bson.ObjectId, err error) {
+	running = map[string]bson.ObjectId{}
+
+	output, err := utils.ExecOutput("",
+		"docker", "ps", "-a", "--format", "{{.Names}}:{{.ID}}")
+	if err != nil {
+		return
+	}
+
+	for _, line := range strings.Split(output, "\n") {
+		fields := strings.SplitN(strings.TrimSpace(line), ":", 2)
+		if len(fields) != 2 {
+			continue
+		}
+
+		name := fields[0]
+		containerId := fields[1]
+
+		if len(name) != 40 || !strings.HasPrefix(name, "pritunl-bastion-") {
+			continue
+		}
+
+		authrId := bson.ObjectIdHex(name[16:])
+
+		running[containerId] = authrId
+	}
+
+	return
+}
+
+func DockerRemove(containerId string) (err error) {
+	_, err = utils.ExecOutputLogged(nil, "docker", "rm", "-f", containerId)
+	if err != nil {
+		return
+	}
+
+	return
+}
