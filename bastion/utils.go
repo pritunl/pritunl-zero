9ed83d19766b1ab90c9107142611398c51583727
Zachary Huff <zach.huff.386@gmail.com>
2018-10-20 00:04:33 -0400

    Add docker match container to bastion utils

--- bastion/utils.go
+++ bastion/utils.go
@@ -7,6 +7,13 @@ import (
 	"strings"
 )
 
+func DockerMatchContainer(a, b string) bool {
+	if len(b) > len(a) {
+		a, b = b, a
+	}
+	return strings.HasPrefix(a, b)
+}
+
 func DockerGetName(authrId bson.ObjectId) string {
 	return fmt.Sprintf("pritunl-bastion-%s", authrId.Hex())
 }
