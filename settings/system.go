1eaef8ed9b289fb77ee56681cd22561f45bc4f67
Zachary Huff <zach.huff.386@gmail.com>
2018-10-23 03:35:17 -0400

    Add bastion host cert to system settings

--- settings/system.go
+++ settings/system.go
@@ -9,22 +9,23 @@ const (
 var System *system
 
 type system struct {
-	Id                   string `bson:"_id"`
-	Name                 string `bson:"name"`
-	DatabaseVersion      int    `bson:"database_version"`
-	Demo                 bool   `bson:"demo"`
-	License              string `bson:"license"`
-	CookieAuthKey        []byte `bson:"cookie_auth_key"`
-	CookieCryptoKey      []byte `bson:"cookie_crypto_key"`
-	ProxyCookieAuthKey   []byte `bson:"proxy_cookie_auth_key"`
-	ProxyCookieCryptoKey []byte `bson:"proxy_cookie_crypto_key"`
-	UserCookieAuthKey    []byte `bson:"user_cookie_auth_key"`
-	UserCookieCryptoKey  []byte `bson:"user_cookie_crypto_key"`
-	AcmeKeyAlgorithm     string `bson:"acme_key_algorithm" default:"rsa"`
-	SshPubKeyLen         int    `bson:"ssh_pub_key_len" default:"5000"`
-	SshHostTokenLen      int    `bson:"ssh_host_token_len" default:"10"`
-	HsmResponseTimeout   int    `bson:"hsm_response_timeout" default:"10"`
-	BastionDockerImage   string `bson:"bastion_docker_image" default:"pritunl/pritunl-bastion"`
+	Id                             string `bson:"_id"`
+	Name                           string `bson:"name"`
+	DatabaseVersion                int    `bson:"database_version"`
+	Demo                           bool   `bson:"demo"`
+	License                        string `bson:"license"`
+	CookieAuthKey                  []byte `bson:"cookie_auth_key"`
+	CookieCryptoKey                []byte `bson:"cookie_crypto_key"`
+	ProxyCookieAuthKey             []byte `bson:"proxy_cookie_auth_key"`
+	ProxyCookieCryptoKey           []byte `bson:"proxy_cookie_crypto_key"`
+	UserCookieAuthKey              []byte `bson:"user_cookie_auth_key"`
+	UserCookieCryptoKey            []byte `bson:"user_cookie_crypto_key"`
+	AcmeKeyAlgorithm               string `bson:"acme_key_algorithm" default:"rsa"`
+	SshPubKeyLen                   int    `bson:"ssh_pub_key_len" default:"5000"`
+	SshHostTokenLen                int    `bson:"ssh_host_token_len" default:"10"`
+	HsmResponseTimeout             int    `bson:"hsm_response_timeout" default:"10"`
+	DisableBastionHostCertificates bool   `bson:"disable_bastion_host_certificates"`
+	BastionDockerImage             string `bson:"bastion_docker_image" default:"pritunl/pritunl-bastion"`
 }
 
 func newSystem() interface{} {
