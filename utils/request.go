a4c3157c398ed344f462f1b7d1447f20457657cf
Zachary Huff <zach.huff.386@gmail.com>
2018-12-28 02:10:13 -0500

    Add format host port to request utils

--- utils/request.go
+++ utils/request.go
@@ -71,6 +71,13 @@ func StripPort(hostport string) string {
 	return hostport[:colon]
 }
 
+func FormatHostPort(hostname string, port int) string {
+	if strings.Contains(hostname, ":") {
+		hostname = "[" + hostname + "]"
+	}
+	return fmt.Sprintf("%s:%d", hostname, port)
+}
+
 func ParseObjectId(strId string) (objId bson.ObjectId, ok bool) {
 	bytId, err := hex.DecodeString(strId)
 	if err != nil {
