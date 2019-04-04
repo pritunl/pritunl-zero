fe550e2f69eb7cdf8033d288adb61f799f0e5d81
Zachary Huff <zach.huff.386@gmail.com>
2019-04-04 15:48:56 -0400

    Add web redirect server to node component

--- www/app/components/Node.tsx
+++ www/app/components/Node.tsx
@@ -636,6 +636,15 @@ export default class Node extends React.Component<Props, State> {
 							/>
 						</div>
 					</label>
+					<PageSwitch
+						disabled={this.state.disabled}
+						label="Web redirect server"
+						help="Enable redirect server for HTTP requests to HTTPS. Required for Lets Encrypt certificates."
+						checked={!node.no_redirect_server}
+						onToggle={(): void => {
+							this.set('no_redirect_server', !node.no_redirect_server);
+						}}
+					/>
 					<label
 						className="bp3-label"
 						style={css.label}
