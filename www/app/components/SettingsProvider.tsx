480bf5c28b2a9f091ff2ee860f0d44b3c5d13990
Zachary Huff <zach.huff.386@gmail.com>
2019-04-07 15:36:08 -0400

    Fix label in settings provider component

--- www/app/components/SettingsProvider.tsx
+++ www/app/components/SettingsProvider.tsx
@@ -20,6 +20,10 @@ interface State {
 }
 
 const css = {
+	label: {
+		fontSize: '16px',
+		margin: '0 0 7px 0',
+	} as React.CSSProperties,
 	card: {
 		marginBottom: '5px',
 	} as React.CSSProperties,
@@ -319,7 +323,7 @@ export default class SettingsProvider extends React.Component<Props, State> {
 		}
 
 		return <div className="bp3-card" style={css.card}>
-			<h6>{label}</h6>
+			<h6 style={css.label}>{label}</h6>
 			<PageInfo
 				fields={[
 					{
