b50e50cb016354e78e4ce41135c1b1cd08b7d537
Zachary Huff <zach.huff.386@gmail.com>
2019-11-27 04:15:39 -0500

    Skip secondary on disabled policies

--- validator/validator.go
+++ validator/validator.go
@@ -80,6 +80,10 @@ func ValidateAdmin(db *database.Database, usr *user.User,
 		}
 
 		for _, polcy := range policies {
+			if polcy.Disabled {
+				continue
+			}
+
 			if polcy.AdminDeviceSecondary {
 				deviceAuth = true
 			}
@@ -145,6 +149,10 @@ func ValidateUser(db *database.Database, usr *user.User,
 		}
 
 		for _, polcy := range policies {
+			if polcy.Disabled {
+				continue
+			}
+
 			if polcy.UserDeviceSecondary {
 				deviceAuth = true
 			}
@@ -236,6 +244,10 @@ func ValidateProxy(db *database.Database, usr *user.User,
 		}
 
 		for _, polcy := range policies {
+			if polcy.Disabled {
+				continue
+			}
+
 			if polcy.ProxyDeviceSecondary {
 				deviceAuth = true
 			}
