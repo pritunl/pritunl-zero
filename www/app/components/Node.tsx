ed03dc60713f7a39db6ee88387d2b040f57e1709
Zachary Huff <zach.huff.386@gmail.com>
2020-03-09 10:49:53 -0400

    Handle no certificates in node component

--- www/app/components/Node.tsx
+++ www/app/components/Node.tsx
@@ -516,15 +516,26 @@ export default class Node extends React.Component<Props, State> {
 			);
 		}
 
+		let hasCertificates = false;
 		let certificatesSelect: JSX.Element[] = [];
 		if (this.props.certificates.length) {
 			for (let certificate of this.props.certificates) {
+				hasCertificates = true;
+
 				certificatesSelect.push(
 					<option key={certificate.id} value={certificate.id}>
 						{certificate.name}
 					</option>,
 				);
 			}
+		}
+
+		if (!hasCertificates) {
+			certificatesSelect = [
+				<option key="null" value="">
+					No Certificates
+				</option>,
+			];
 		}
 
 		return <div
