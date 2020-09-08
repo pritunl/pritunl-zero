43493ff929e847e69a975768edbd0d446d4c9815
Zachary Huff <zach.huff.386@gmail.com>
2020-09-08 10:36:09 -0400

    Add paging to services store

--- www/app/stores/ServicesStore.ts
+++ www/app/stores/ServicesStore.ts
@@ -6,6 +6,10 @@ import * as GlobalTypes from '../types/GlobalTypes';
 
 class ServicesStore extends EventEmitter {
 	_services: ServiceTypes.ServicesRo = Object.freeze([]);
+	_page: number;
+	_pageCount: number;
+	_filter: ServiceTypes.Filter = null;
+	_count: number;
 	_map: {[key: string]: number} = {};
 	_token = Dispatcher.register((this._callback).bind(this));
 
@@ -21,6 +25,26 @@ class ServicesStore extends EventEmitter {
 			});
 		});
 		return services;
+	}
+
+	get page(): number {
+		return this._page || 0;
+	}
+
+	get pageCount(): number {
+		return this._pageCount || 20;
+	}
+
+	get pages(): number {
+		return Math.ceil(this.count / this.pageCount);
+	}
+
+	get filter(): ServiceTypes.Filter {
+		return this._filter;
+	}
+
+	get count(): number {
+		return this._count || 0;
 	}
 
 	service(id: string): ServiceTypes.ServiceRo {
@@ -43,21 +67,48 @@ class ServicesStore extends EventEmitter {
 		this.removeListener(GlobalTypes.CHANGE, callback);
 	}
 
-	_sync(services: ServiceTypes.Service[]): void {
+	_traverse(page: number): void {
+		this._page = Math.min(this.pages, page);
+	}
+
+	_filterCallback(filter: ServiceTypes.Filter): void {
+		if ((this._filter !== null && filter === null) ||
+			(this._filter === {} && filter !== null) || (
+				filter && this._filter && (
+					filter.name !== this._filter.name
+				))) {
+			this._traverse(0);
+		}
+		this._filter = filter;
+		this.emitChange();
+	}
+
+	_sync(services: ServiceTypes.Service[], count: number): void {
 		this._map = {};
 		for (let i = 0; i < services.length; i++) {
 			services[i] = Object.freeze(services[i]);
 			this._map[services[i].id] = i;
 		}
 
+		this._count = count;
 		this._services = Object.freeze(services);
+		this._page = Math.min(this.pages, this.page);
+
 		this.emitChange();
 	}
 
 	_callback(action: ServiceTypes.ServiceDispatch): void {
 		switch (action.type) {
+			case ServiceTypes.TRAVERSE:
+				this._traverse(action.data.page);
+				break;
+
+			case ServiceTypes.FILTER:
+				this._filterCallback(action.data.filter);
+				break;
+
 			case ServiceTypes.SYNC:
-				this._sync(action.data.services);
+				this._sync(action.data.services, action.data.count);
 				break;
 		}
 	}
