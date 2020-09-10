2fce6126f4cf44a4caab5cadd76e7870130815c5
Zachary Huff <zach.huff.386@gmail.com>
2020-09-09 22:57:23 -0400

    Add pages to services component

--- www/app/components/Services.tsx
+++ www/app/components/Services.tsx
@@ -6,18 +6,48 @@ import ServicesStore from '../stores/ServicesStore';
 import AuthoritiesStore from '../stores/AuthoritiesStore';
 import * as ServiceActions from '../actions/ServiceActions';
 import * as AuthorityActions from '../actions/AuthorityActions';
-import NonState from './NonState';
 import Service from './Service';
+import ServicesFilter from './ServicesFilter';
+import ServicesPage from './ServicesPage';
 import Page from './Page';
 import PageHeader from './PageHeader';
+import NonState from './NonState';
+import ConfirmButton from './ConfirmButton';
+
+interface Selected {
+	[key: string]: boolean;
+}
+
+interface Opened {
+	[key: string]: boolean;
+}
 
 interface State {
 	services: ServiceTypes.ServicesRo;
+	filter: ServiceTypes.Filter;
 	authorities: AuthorityTypes.AuthoritiesRo;
+	selected: Selected;
+	opened: Opened;
+	newOpened: boolean;
+	lastSelected: string;
 	disabled: boolean;
 }
 
 const css = {
+	items: {
+		width: '100%',
+		marginTop: '-5px',
+		display: 'table',
+		borderSpacing: '0 5px',
+	} as React.CSSProperties,
+	itemsBox: {
+		width: '100%',
+		overflowY: 'auto',
+	} as React.CSSProperties,
+	placeholder: {
+		opacity: 0,
+		width: '100%',
+	} as React.CSSProperties,
 	header: {
 		marginTop: '-19px',
 	} as React.CSSProperties,
@@ -37,9 +67,22 @@ export default class Services extends React.Component<{}, State> {
 		super(props, context);
 		this.state = {
 			services: ServicesStore.services,
+			filter: ServicesStore.filter,
 			authorities: AuthoritiesStore.authorities,
+			selected: {},
+			opened: {},
+			newOpened: false,
+			lastSelected: null,
 			disabled: false,
 		};
+	}
+
+	get selected(): boolean {
+		return !!Object.keys(this.state.selected).length;
+	}
+
+	get opened(): boolean {
+		return !!Object.keys(this.state.opened).length;
 	}
 
 	componentDidMount(): void {
@@ -55,23 +98,136 @@ export default class Services extends React.Component<{}, State> {
 	}
 
 	onChange = (): void => {
+		let services = ServicesStore.services;
+		let selected: Selected = {};
+		let curSelected = this.state.selected;
+		let opened: Opened = {};
+		let curOpened = this.state.opened;
+
+		services.forEach((service: ServiceTypes.Service): void => {
+			if (curSelected[service.id]) {
+				selected[service.id] = true;
+			}
+			if (curOpened[service.id]) {
+				opened[service.id] = true;
+			}
+		});
+
 		this.setState({
 			...this.state,
-			services: ServicesStore.services,
+			services: services,
+			filter: ServicesStore.filter,
 			authorities: AuthoritiesStore.authorities,
+			selected: selected,
+			opened: opened,
+		});
+	}
+
+	onDelete = (): void => {
+		this.setState({
+			...this.state,
+			disabled: true,
+		});
+		ServiceActions.removeMulti(
+				Object.keys(this.state.selected)).then((): void => {
+			this.setState({
+				...this.state,
+				selected: {},
+				disabled: false,
+			});
+		}).catch((): void => {
+			this.setState({
+				...this.state,
+				disabled: false,
+			});
 		});
 	}
 
 	render(): JSX.Element {
 		let servicesDom: JSX.Element[] = [];
 
-		this.state.services.forEach((service: ServiceTypes.ServiceRo): void => {
+		this.state.services.forEach((
+				service: ServiceTypes.ServiceRo): void => {
 			servicesDom.push(<Service
 				key={service.id}
 				service={service}
 				authorities={this.state.authorities}
+				selected={!!this.state.selected[service.id]}
+				open={!!this.state.opened[service.id]}
+				onSelect={(shift: boolean): void => {
+					let selected = {
+						...this.state.selected,
+					};
+
+					if (shift) {
+						let services = this.state.services;
+						let start: number;
+						let end: number;
+
+						for (let i = 0; i < services.length; i++) {
+							let usr = services[i];
+
+							if (usr.id === service.id) {
+								start = i;
+							} else if (usr.id === this.state.lastSelected) {
+								end = i;
+							}
+						}
+
+						if (start !== undefined && end !== undefined) {
+							if (start > end) {
+								end = [start, start = end][0];
+							}
+
+							for (let i = start; i <= end; i++) {
+								selected[services[i].id] = true;
+							}
+
+							this.setState({
+								...this.state,
+								lastSelected: service.id,
+								selected: selected,
+							});
+
+							return;
+						}
+					}
+
+					if (selected[service.id]) {
+						delete selected[service.id];
+					} else {
+						selected[service.id] = true;
+					}
+
+					this.setState({
+						...this.state,
+						lastSelected: service.id,
+						selected: selected,
+					});
+				}}
+				onOpen={(): void => {
+					let opened = {
+						...this.state.opened,
+					};
+
+					if (opened[service.id]) {
+						delete opened[service.id];
+					} else {
+						opened[service.id] = true;
+					}
+
+					this.setState({
+						...this.state,
+						opened: opened,
+					});
+				}}
 			/>);
 		});
+
+		let filterClass = 'bp3-button bp3-intent-primary bp3-icon-filter ';
+		if (this.state.filter) {
+			filterClass += 'bp3-active';
+		}
 
 		return <Page>
 			<PageHeader>
@@ -79,6 +235,42 @@ export default class Services extends React.Component<{}, State> {
 					<h2 style={css.heading}>Services</h2>
 					<div className="flex"/>
 					<div style={css.buttons}>
+						<button
+							className={filterClass}
+							style={css.button}
+							type="button"
+							onClick={(): void => {
+								if (this.state.filter === null) {
+									ServiceActions.filter({});
+								} else {
+									ServiceActions.filter(null);
+								}
+							}}
+						>
+							Filters
+						</button>
+						<button
+							className="bp3-button bp3-intent-warning bp3-icon-chevron-up"
+							style={css.button}
+							disabled={!this.opened}
+							type="button"
+							onClick={(): void => {
+								this.setState({
+									...this.state,
+									opened: {},
+								});
+							}}
+						>
+							Collapse All
+						</button>
+						<ConfirmButton
+							label="Delete Selected"
+							className="bp3-intent-danger bp3-icon-delete"
+							progressClassName="bp3-intent-danger"
+							style={css.button}
+							disabled={!this.selected || this.state.disabled}
+							onConfirm={this.onDelete}
+						/>
 						<button
 							className="bp3-button bp3-intent-success bp3-icon-add"
 							style={css.button}
@@ -109,14 +301,35 @@ export default class Services extends React.Component<{}, State> {
 					</div>
 				</div>
 			</PageHeader>
-			<div>
-				{servicesDom}
+			<ServicesFilter
+				filter={this.state.filter}
+				onFilter={(filter): void => {
+					ServiceActions.filter(filter);
+				}}
+				authorities={this.state.authorities}
+			/>
+			<div style={css.itemsBox}>
+				<div style={css.items}>
+					{servicesDom}
+					<tr className="bp3-card bp3-row" style={css.placeholder}>
+						<td colSpan={5} style={css.placeholder}/>
+					</tr>
+				</div>
 			</div>
 			<NonState
 				hidden={!!servicesDom.length}
-				iconClass="bp3-icon-cloud"
+				iconClass="bp3-icon-compressed"
 				title="No services"
 				description="Add a new service to get started."
+			/>
+			<ServicesPage
+				onPage={(): void => {
+					this.setState({
+						...this.state,
+						selected: {},
+						lastSelected: null,
+					});
+				}}
 			/>
 		</Page>;
 	}
