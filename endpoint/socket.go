db7592eec683a44b457ff291c67c7a9269b02ccf
Zachary Huff <zach.huff.386@gmail.com>
2021-04-23 13:12:29 -0400

    Add socket to endpoint

---
+++ endpoint/socket.go
@@ -0,0 +1,65 @@
+package endpoint
+
+import (
+	"context"
+	"net/http"
+	"sync"
+	"time"
+
+	"github.com/dropbox/godropbox/container/set"
+	"github.com/gorilla/websocket"
+)
+
+var (
+	Upgrader = websocket.Upgrader{
+		HandshakeTimeout: 30 * time.Second,
+		ReadBufferSize:   1024,
+		WriteBufferSize:  1024,
+		CheckOrigin: func(r *http.Request) bool {
+			return true
+		},
+	}
+	WebSockets     = set.NewSet()
+	WebSocketsLock = sync.Mutex{}
+)
+
+type WebSocket struct {
+	Conn   *websocket.Conn
+	Ticker *time.Ticker
+	Cancel context.CancelFunc
+	Closed bool
+}
+
+func (w *WebSocket) Close() {
+	w.Closed = true
+	func() {
+		defer func() {
+			recover()
+		}()
+		w.Cancel()
+	}()
+	func() {
+		defer func() {
+			recover()
+		}()
+		w.Ticker.Stop()
+	}()
+	func() {
+		defer func() {
+			recover()
+		}()
+		w.Conn.Close()
+	}()
+}
+
+func WebSocketsStop() {
+	WebSocketsLock.Lock()
+	for socketInf := range WebSockets.Iter() {
+		func() {
+			socket := socketInf.(*WebSocket)
+			socket.Close()
+		}()
+	}
+	WebSockets = set.NewSet()
+	WebSocketsLock.Unlock()
+}
