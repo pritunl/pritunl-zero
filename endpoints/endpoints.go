eb51ed22f8baeaaaf699156486bade737f58f7d2
Zachary Huff <zach.huff.386@gmail.com>
2021-05-16 15:06:36 -0400

    Change endpoint id type

--- endpoints/endpoints.go
+++ endpoints/endpoints.go
@@ -2,9 +2,17 @@ package endpoints
 
 import (
 	"crypto/md5"
+	"fmt"
+	"time"
 
+	"github.com/dropbox/godropbox/errors"
 	"github.com/pritunl/mongo-go-driver/bson/primitive"
 	"github.com/pritunl/pritunl-zero/database"
+	"github.com/pritunl/pritunl-zero/errortypes"
+)
+
+const (
+	BinaryMD5 byte = 0x05
 )
 
 type Doc interface {
@@ -13,11 +21,17 @@ type Doc interface {
 	Print()
 }
 
-func GenerateId(endpointId, clientId primitive.ObjectID) []byte {
+func GenerateId(endpointId primitive.ObjectID,
+	timestamp time.Time) primitive.Binary {
+
 	hash := md5.New()
 	hash.Write([]byte(endpointId.Hex()))
-	hash.Write([]byte(clientId.Hex()))
-	return hash.Sum(nil)
+	hash.Write([]byte(fmt.Sprintf("%d", timestamp.Unix())))
+
+	return primitive.Binary{
+		Subtype: BinaryMD5,
+		Data:    hash.Sum(nil),
+	}
 }
 
 func GetObj(typ string) Doc {
