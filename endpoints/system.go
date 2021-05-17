16e960cece0fbec8b7757a09929a2e1f0be1dd11
Zachary Huff <zach.huff.386@gmail.com>
2021-05-17 10:27:26 -0400

    Add aggregate to system endpoints

--- endpoints/system.go
+++ endpoints/system.go
@@ -6,6 +6,7 @@ import (
 
 	"github.com/pritunl/mongo-go-driver/bson"
 	"github.com/pritunl/mongo-go-driver/bson/primitive"
+	"github.com/pritunl/mongo-go-driver/mongo/options"
 	"github.com/pritunl/pritunl-zero/database"
 )
 
@@ -19,6 +20,13 @@ type System struct {
 	MemUsage  float64 `bson:"mu" json:"mu"`
 	SwapTotal int     `bson:"st" json:"st"`
 	SwapUsage float64 `bson:"su" json:"su"`
+}
+
+type SystemAgg struct {
+	Id        int64   `bson:"_id"`
+	CpuUsage  float64 `bson:"cu"`
+	MemUsage  float64 `bson:"mu"`
+	SwapUsage float64 `bson:"su"`
 }
 
 func (d *System) GetCollection(db *database.Database) *database.Collection {
@@ -46,20 +54,37 @@ func (d *System) Print() {
 }
 
 type SystemChart struct {
-	CpuUsage []*Chart `json:"cpu_usage"`
-	MemUsage []*Chart `json:"mem_usage"`
+	CpuUsage  []*Chart `json:"cpu_usage"`
+	MemUsage  []*Chart `json:"mem_usage"`
+	SwapUsage []*Chart `json:"swap_usage"`
 }
 
-func GetSystemChart(db *database.Database, endpoint primitive.ObjectID,
+func GetSystemChartSingle(db *database.Database, endpoint primitive.ObjectID,
 	start, end time.Time) (chart *SystemChart, err error) {
 
 	coll := db.EndpointsSystem()
 	cpuUsage := []*Chart{}
 	memUsage := []*Chart{}
+	swapUsage := []*Chart{}
+
+	timeQuery := bson.D{
+		{"$gte", start},
+	}
+	if !end.IsZero() {
+		timeQuery = append(timeQuery, bson.E{"$lte", end})
+	}
 
 	cursor, err := coll.Find(
 		db,
-		&bson.M{},
+		&bson.M{
+			"e": endpoint,
+			"t": timeQuery,
+		},
+		&options.FindOptions{
+			Sort: &bson.D{
+				{"t", 1},
+			},
+		},
 	)
 	if err != nil {
 		err = database.ParseError(err)
@@ -83,6 +108,10 @@ func GetSystemChart(db *database.Database, endpoint primitive.ObjectID,
 			X: doc.Timestamp.Unix() * 1000,
 			Y: doc.MemUsage,
 		})
+		swapUsage = append(swapUsage, &Chart{
+			X: doc.Timestamp.Unix() * 1000,
+			Y: doc.MemUsage,
+		})
 	}
 
 	err = cursor.Err()
@@ -92,8 +121,117 @@ func GetSystemChart(db *database.Database, endpoint primitive.ObjectID,
 	}
 
 	chart = &SystemChart{
-		CpuUsage: cpuUsage,
-		MemUsage: memUsage,
+		CpuUsage:  cpuUsage,
+		MemUsage:  memUsage,
+		SwapUsage: swapUsage,
+	}
+
+	return
+}
+
+func GetSystemChart(db *database.Database, endpoint primitive.ObjectID,
+	start, end time.Time, interval time.Duration) (
+	chart *SystemChart, err error) {
+
+	if interval == 1*time.Minute {
+		chart, err = GetSystemChartSingle(db, endpoint, start, end)
+		return
+	}
+
+	coll := db.EndpointsSystem()
+	cpuUsage := []*Chart{}
+	memUsage := []*Chart{}
+	swapUsage := []*Chart{}
+
+	timeQuery := bson.D{
+		{"$gte", start},
+	}
+	if !end.IsZero() {
+		timeQuery = append(timeQuery, bson.E{"$lte", end})
+	}
+
+	cursor, err := coll.Aggregate(db, []*bson.M{
+		&bson.M{
+			"$match": &bson.M{
+				"e": endpoint,
+				"t": timeQuery,
+			},
+		},
+		&bson.M{
+			"$group": &bson.M{
+				"_id": &bson.M{
+					"$let": &bson.M{
+						"vars": &bson.M{
+							"t": &bson.D{{"$toLong", "$t"}},
+						},
+						"in": &bson.M{
+							"$subtract": &bson.A{
+								"$$t",
+								&bson.M{
+									"$mod": &bson.A{
+										"$$t",
+										interval.Milliseconds(),
+									},
+								},
+							},
+						},
+					},
+				},
+				"cu": &bson.D{
+					{"$avg", "$cu"},
+				},
+				"mu": &bson.D{
+					{"$avg", "$mu"},
+				},
+				"su": &bson.D{
+					{"$avg", "$su"},
+				},
+			},
+		},
+		&bson.M{
+			"$sort": &bson.M{
+				"_id": 1,
+			},
+		},
+	})
+	if err != nil {
+		err = database.ParseError(err)
+		return
+	}
+	defer cursor.Close(db)
+
+	for cursor.Next(db) {
+		doc := &SystemAgg{}
+		err = cursor.Decode(doc)
+		if err != nil {
+			err = database.ParseError(err)
+			return
+		}
+
+		cpuUsage = append(cpuUsage, &Chart{
+			X: doc.Id,
+			Y: doc.CpuUsage,
+		})
+		memUsage = append(memUsage, &Chart{
+			X: doc.Id,
+			Y: doc.MemUsage,
+		})
+		swapUsage = append(swapUsage, &Chart{
+			X: doc.Id,
+			Y: doc.MemUsage,
+		})
+	}
+
+	err = cursor.Err()
+	if err != nil {
+		err = database.ParseError(err)
+		return
+	}
+
+	chart = &SystemChart{
+		CpuUsage:  cpuUsage,
+		MemUsage:  memUsage,
+		SwapUsage: swapUsage,
 	}
 
 	return
