3245bc1fb2b1215e70950a4884d36139ef84d49e
Zachary Huff <zach.huff.386@gmail.com>
2021-05-19 13:10:47 -0400

    Add chart init to app

--- www/app/App.tsx
+++ www/app/App.tsx
@@ -1,4 +1,6 @@
 /// <reference path="References.d.ts"/>
+import 'chartjs-adapter-moment';
+import * as ChartJs from 'chart.js';
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import * as Blueprint from '@blueprintjs/core';
@@ -6,6 +8,40 @@ import Main from './components/Main';
 import * as Alert from './Alert';
 import * as Event from './Event';
 import * as Csrf from './Csrf';
+
+ChartJs.Chart.register(ChartJs.LineController);
+ChartJs.Chart.register(ChartJs.CategoryScale);
+ChartJs.Chart.register(ChartJs.LinearScale);
+ChartJs.Chart.register(ChartJs.TimeScale);
+ChartJs.Chart.register(ChartJs.PointElement);
+ChartJs.Chart.register(ChartJs.LineElement);
+ChartJs.Chart.register(ChartJs.Title);
+ChartJs.Chart.register(ChartJs.Tooltip);
+ChartJs.Chart.register(ChartJs.Filler);
+
+class LineTracerController extends ChartJs.LineController {
+	draw(): void {
+		super.draw();
+
+		let chart = this.chart as any;
+		if (chart.tooltip._active && chart.tooltip._active.length) {
+			let ctx = this.chart.ctx;
+			let x = chart.tooltip.caretX;
+			let topY = chart.scales.y.top;
+			let bottomY = chart.scales.y.bottom;
+
+			ctx.save();
+			ctx.beginPath();
+			ctx.moveTo(x, topY);
+			ctx.lineTo(x, bottomY);
+			ctx.lineWidth = 1;
+			ctx.strokeStyle = 'rgba(255, 255, 255, 0.6)';
+			ctx.stroke();
+			ctx.restore();
+		}
+	}
+}
+(ChartJs.Chart as any).registry.controllers.items.line = LineTracerController;
 
 Csrf.load().then((): void => {
 	Blueprint.FocusStyleManager.onlyShowFocusOnTabs();
