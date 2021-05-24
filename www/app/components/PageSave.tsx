9ef7bc355f0accfc6d5db0588ec0eb60a89fb277
Zachary Huff <zach.huff.386@gmail.com>
2021-05-23 22:55:22 -0400

    Add elements to page save component

--- www/app/components/PageSave.tsx
+++ www/app/components/PageSave.tsx
@@ -41,8 +41,9 @@ export default class PageSave extends React.Component<Props, {}> {
 		return <div
 			className="layout horizontal"
 			style={style}
-			hidden={this.props.hidden}
+			hidden={this.props.hidden && !this.props.children}
 		>
+			{this.props.children}
 			<div className="flex"/>
 			<div className="layout horizontal">
 				<span style={css.message} hidden={!this.props.message}>
@@ -52,6 +53,7 @@ export default class PageSave extends React.Component<Props, {}> {
 					<button
 						className="bp3-button bp3-icon-cross"
 						style={css.button}
+						hidden={this.props.hidden}
 						type="button"
 						disabled={!this.props.changed || this.props.disabled}
 						onClick={this.props.onCancel}
@@ -61,6 +63,7 @@ export default class PageSave extends React.Component<Props, {}> {
 					<button
 						className="bp3-button bp3-intent-success bp3-icon-tick"
 						style={css.button}
+						hidden={this.props.hidden}
 						type="button"
 						disabled={!this.props.changed || this.props.disabled}
 						onClick={this.props.onSave}
