243f911b3ad46fc6b2040c1cf4b37e9fb662d790
Zachary Huff <zach.huff.386@gmail.com>
2021-08-05 16:35:16 -0400

    Add diskio to chart types

--- www/app/types/ChartTypes.ts
+++ www/app/types/ChartTypes.ts
@@ -89,6 +89,71 @@ export function getChartLabels(resource: string, data: any): Labels {
 				resource_max: 100,
 				datasets: diskDatasets,
 			};
+		case 'diskio0':
+		case 'diskio1':
+			let diskioData = data as EndpointTypes.NetworkChart;
+			let diskioDatasets: Datasets = [];
+
+			for (let key of Object.keys(diskioData).sort()) {
+				let keys = key.split('-');
+				let diskDevice = keys.slice(0, keys.length-1).join('-');
+				let dataType = keys[keys.length-1];
+
+				let label = '';
+
+				if (resource === 'diskio0') {
+					switch (dataType) {
+						case 'br':
+							label = 'Read';
+							break;
+						case 'bw':
+							label = 'Written';
+							break;
+						default:
+							continue;
+					}
+				} else {
+					switch (dataType) {
+						case 'tr':
+							label = 'Read';
+							break;
+						case 'tw':
+							label = 'Write';
+							break;
+						case 'ti':
+							label = 'I/O';
+							break;
+						default:
+							continue;
+					}
+				}
+
+				diskioDatasets.push({
+					label: diskDevice + ' ' + label,
+				} as Dataset);
+			}
+
+			if (resource === 'diskio0') {
+				return {
+					title: 'Disk I/O',
+					resource_label: 'Activity',
+					resource_type: 'bytes',
+					resource_suffix: '',
+					resource_fixed: 2,
+					resource_min: 0,
+					datasets: diskioDatasets,
+				};
+			} else {
+				return {
+					title: 'Disk I/O Wait',
+					resource_label: 'Waiting',
+					resource_type: 'milliseconds',
+					resource_suffix: '',
+					resource_fixed: 2,
+					resource_min: 0,
+					datasets: diskioDatasets,
+				};
+			}
 		case 'network':
 			let netData = data as EndpointTypes.NetworkChart;
 			let netDatasets: Datasets = [];
@@ -151,6 +216,27 @@ export function getChartData(resource: string, data: any): Chart {
 			}
 
 			return diskChart;
+		case 'diskio0':
+		case 'diskio1':
+			let diskioData = data as EndpointTypes.DiskIoChart;
+			let diskioChart: Chart = [];
+
+			for (let key of Object.keys(diskioData).sort()) {
+				let keys = key.split('-');
+				let dataType = keys[keys.length-1];
+
+				if (resource === 'diskio0') {
+					if (dataType === 'br' || dataType === 'bw') {
+						diskioChart.push(diskioData[key]);
+					}
+				} else {
+					if (dataType === 'tr' || dataType === 'tw' || dataType === 'ti') {
+						diskioChart.push(diskioData[key]);
+					}
+				}
+			}
+
+			return diskioChart;
 		case 'network':
 			let netData = data as EndpointTypes.NetworkChart;
 			let netChart: Chart = [];
