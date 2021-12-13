a539e2b516f84b124732340bbddefeda2b567774
Zachary Huff <zach.huff.386@gmail.com>
2021-12-13 16:25:42 -0500

    Add username and password to elastic settings

--- settings/elastic.go
+++ settings/elastic.go
@@ -5,6 +5,8 @@ var Elastic *elastic
 type elastic struct {
 	Id            string   `bson:"_id"`
 	Addresses     []string `bson:"addresses"`
+	Username      string   `bson:"username"`
+	Password      string   `bson:"password"`
 	ProxyRequests bool     `bson:"proxy_requests"`
 }
 
