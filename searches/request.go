b36e2ead52ffe118526a7a4011ae58339366cd50
Zachary Huff <zach.huff.386@gmail.com>
2021-12-14 05:01:21 -0500

    Add request searches

---
+++ searches/request.go
@@ -0,0 +1,117 @@
+package searches
+
+import (
+	"net/http"
+	"net/url"
+	"time"
+
+	"github.com/dropbox/godropbox/container/set"
+	"github.com/pritunl/pritunl-zero/search"
+)
+
+var (
+	RequestTypes = set.NewSet(
+		"application/xml",
+		"application/json",
+		"application/x-www-form-urlencoded",
+	)
+)
+
+type Request struct {
+	User      string      `json:"user"`
+	Username  string      `json:"username"`
+	Session   string      `json:"session"`
+	Address   string      `json:"address"`
+	Timestamp time.Time   `json:"timestamp"`
+	Scheme    string      `json:"scheme"`
+	Host      string      `json:"host"`
+	Path      string      `json:"path"`
+	Query     url.Values  `json:"query"`
+	Header    http.Header `json:"header"`
+	Body      string      `json:"body"`
+}
+
+func (r *Request) Index() (err error) {
+	search.Index("zero-requests", r)
+	return
+}
+
+func init() {
+	mappings := []*search.Mapping{
+		&search.Mapping{
+			Field: "user",
+			Type:  search.Keyword,
+			Store: false,
+			Index: true,
+		},
+		&search.Mapping{
+			Field: "username",
+			Type:  search.Keyword,
+			Store: false,
+			Index: true,
+		},
+		&search.Mapping{
+			Field: "session",
+			Type:  search.Keyword,
+			Store: false,
+			Index: true,
+		},
+		&search.Mapping{
+			Field: "address",
+			Type:  search.Ip,
+			Store: false,
+			Index: true,
+		},
+		&search.Mapping{
+			Field: "timestamp",
+			Type:  search.Date,
+			Store: false,
+			Index: true,
+		},
+		&search.Mapping{
+			Field: "scheme",
+			Type:  search.Keyword,
+			Store: false,
+			Index: true,
+		},
+		&search.Mapping{
+			Field: "host",
+			Type:  search.Keyword,
+			Store: false,
+			Index: true,
+		},
+		&search.Mapping{
+			Field: "path",
+			Type:  search.Keyword,
+			Store: false,
+			Index: true,
+		},
+		&search.Mapping{
+			Field: "query",
+			Type:  search.Object,
+			Index: false,
+		},
+		&search.Mapping{
+			Field: "body",
+			Type:  search.Text,
+			Store: false,
+			Index: false,
+		},
+		&search.Mapping{
+			Field: "header.User-Agent",
+			Type:  search.Keyword,
+			Store: false,
+			Index: true,
+		},
+		&search.Mapping{
+			Field: "header.X-Forwarded-User",
+			Type:  search.Keyword,
+			Store: false,
+			Index: true,
+		},
+	}
+
+	search.AddMappings("zero-requests", mappings)
+
+	return
+}
