3f9b239f470f5aba20d429040c5bf5f1fccdbd9d
Zachary Huff <zach.huff.386@gmail.com>
2021-12-18 14:19:00 -0500

    Add log limit to search utils

--- search/utils.go
+++ search/utils.go
@@ -4,6 +4,19 @@ import (
 	"time"
 )
 
+var (
+	lastLog time.Time
+)
+
 func dateSuffix() string {
 	return time.Now().Format("-2006-01-02")
+}
+
+func logLimit() bool {
+	if time.Since(lastLog) > 30*time.Second {
+		lastLog = time.Now()
+		return true
+	} else {
+		return false
+	}
 }
