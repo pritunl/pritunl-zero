174105d54db3901545a311a5b7dbccba05cb5442
Zachary Huff <zach.huff.386@gmail.com>
2022-01-17 16:43:01 -0500

    Add parse webauthn error to utils

---
+++ utils/webauthn.go
@@ -0,0 +1,27 @@
+package utils
+
+import (
+	"fmt"
+
+	"github.com/dropbox/godropbox/errors"
+	"github.com/pritunl/pritunl-cloud/errortypes"
+	"github.com/pritunl/webauthn/protocol"
+)
+
+func ParseWebauthnError(err error) (newErr error) {
+	if e, ok := err.(*protocol.Error); ok {
+		newErr = &errortypes.AuthenticationError{
+			errors.Wrapf(
+				err, "secondary: Webauthn error %s - %s - %s",
+				e.Type, e.DevInfo, e.Details,
+			),
+		}
+	} else {
+		newErr = &errortypes.AuthenticationError{
+			errors.Wrap(err, fmt.Sprintf(
+				"secondary: Webauthn unknown error")),
+		}
+	}
+
+	return
+}
