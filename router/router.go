92c256ab5f48ef18a33b5b1333967b56559a2de7
Zachary Huff <zach.huff.386@gmail.com>
2022-05-17 14:56:03 -0400

    Update cipher suite in router

--- router/router.go
+++ router/router.go
@@ -287,8 +287,19 @@ func (r *Router) startWeb() {
 		}
 	} else {
 		tlsConfig := &tls.Config{
-			MinVersion:     tls.VersionTLS12,
-			MaxVersion:     tls.VersionTLS13,
+			MinVersion: tls.VersionTLS12,
+			MaxVersion: tls.VersionTLS13,
+			CipherSuites: []uint16{
+				tls.TLS_AES_128_GCM_SHA256,                        // 0x1301
+				tls.TLS_AES_256_GCM_SHA384,                        // 0x1302
+				tls.TLS_CHACHA20_POLY1305_SHA256,                  // 0x1303
+				tls.TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256,       // 0xc02b
+				tls.TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256,         // 0xc02f
+				tls.TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384,       // 0xc02c
+				tls.TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384,         // 0xc030
+				tls.TLS_ECDHE_ECDSA_WITH_CHACHA20_POLY1305_SHA256, // 0xcca9
+				tls.TLS_ECDHE_RSA_WITH_CHACHA20_POLY1305_SHA256,   // 0xcca8
+			},
 			GetCertificate: r.certificates.GetCertificate,
 		}
 		tlsConfig.Certificates = []tls.Certificate{}
