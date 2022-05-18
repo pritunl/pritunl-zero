94bacd0bc22ca14b08f094fb53054509a6bf3d52
Zachary Huff <zach.huff.386@gmail.com>
2022-05-18 16:36:04 -0400

    Update www build

--- www/build.sh
+++ www/build.sh
@@ -16,6 +16,7 @@ cp node_modules/@blueprintjs/icons/resources/icons/icons-20.ttf dist-dev/static/
 cp node_modules/@blueprintjs/icons/resources/icons/icons-20.woff dist-dev/static/
 sed -i 's|../../resources/icons/||g' dist-dev/static/blueprint-icons.css
 
+npm link webpack
 webpack --config webpack.dev.config
 
 cp index.html dist-dev/index.html
@@ -48,10 +49,10 @@ APP_HASH=`md5sum dist/static/app.js | cut -c1-6`
 UAPP_HASH=`md5sum dist/static/uapp.js | cut -c1-6`
 
 mv dist/static/app.js dist/static/app.${APP_HASH}.js
-#mv dist/static/app.js.map dist/static/app.${APP_HASH}.js.map
+mv dist/static/app.js.map dist/static/app.${APP_HASH}.js.map
 
 mv dist/static/uapp.js dist/static/uapp.${UAPP_HASH}.js
-#mv dist/static/uapp.js.map dist/static/uapp.${UAPP_HASH}.js.map
+mv dist/static/uapp.js.map dist/static/uapp.${UAPP_HASH}.js.map
 
 sed -i -e "s|static/app.js|static/app.${APP_HASH}.js|g" dist/index.html
 sed -i -e "s|static/uapp.js|static/uapp.${UAPP_HASH}.js|g" dist/uindex.html
