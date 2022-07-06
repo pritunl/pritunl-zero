8800db4626c87709f391299cc5ed614776956b13
Zachary Huff <zach.huff.386@gmail.com>
2022-07-06 02:15:38 -0400

    Add app id to auth jumpcloud

--- auth/jumpcloud.go
+++ auth/jumpcloud.go
@@ -24,10 +24,76 @@ type jumpcloudResponse struct {
 }
 
 type jumpcloudUser struct {
+	Id            string `json:"id"`
+	IdAlt         string `json:"_id"`
 	Email         string `json:"email"`
 	AccountLocked bool   `json:"account_locked"`
 	Suspended     bool   `json:"suspended"`
 	Activated     bool   `json:"activated"`
+}
+
+type jumpcloudApp struct {
+	Id string `json:"id"`
+}
+
+func jumpcloudCheckApp(provider *settings.Provider, userId string) (
+	attached bool, err error) {
+
+	reqUrl := &url.URL{
+		Scheme: "https",
+		Host:   "console.jumpcloud.com",
+		Path:   fmt.Sprintf("/api/v2/users/%s/applications", userId),
+	}
+
+	req, err := http.NewRequest(
+		"GET",
+		reqUrl.String(),
+		nil,
+	)
+	if err != nil {
+		err = &errortypes.RequestError{
+			errors.Wrap(err, "auth: Failed to create jumpcloud request"),
+		}
+		return
+	}
+
+	req.Header.Add("Accept", "application/json")
+	req.Header.Add("X-Api-Key", provider.JumpCloudSecret)
+
+	resp, err := client.Do(req)
+	if err != nil {
+		err = &errortypes.RequestError{
+			errors.Wrap(err, "auth: Jumpcloud request failed"),
+		}
+		return
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != 200 {
+		err = &errortypes.RequestError{
+			errors.Wrapf(err, "auth: Jumpcloud server error %d",
+				resp.StatusCode),
+		}
+		return
+	}
+
+	data := []*jumpcloudApp{}
+	err = json.NewDecoder(resp.Body).Decode(&data)
+	if err != nil {
+		err = &errortypes.ParseError{
+			errors.Wrap(err, "auth: Failed to parse jumpcloud response"),
+		}
+		return
+	}
+
+	for _, app := range data {
+		if app.Id == provider.JumpCloudAppId {
+			attached = true
+			return
+		}
+	}
+
+	return
 }
 
 func JumpcloudSync(db *database.Database, usr *user.User,
@@ -77,7 +143,7 @@ func JumpcloudSync(db *database.Database, usr *user.User,
 	}
 
 	data := &jumpcloudResponse{}
-	err = json.NewDecoder(resp.Body).Decode(&data)
+	err = json.NewDecoder(resp.Body).Decode(data)
 	if err != nil {
 		err = &errortypes.ParseError{
 			errors.Wrap(err, "auth: Failed to parse jumpcloud response"),
@@ -85,6 +151,7 @@ func JumpcloudSync(db *database.Database, usr *user.User,
 		return
 	}
 
+	userId := ""
 	if data.TotalCount > 0 && data.Results != nil {
 		for _, authUser := range data.Results {
 			if authUser.Email != usr.Username {
@@ -101,14 +168,42 @@ func JumpcloudSync(db *database.Database, usr *user.User,
 
 				return
 			} else {
-				active = true
-				return
+				if authUser.Id != "" {
+					userId = authUser.Id
+				} else {
+					userId = authUser.IdAlt
+				}
+				break
 			}
 		}
 	}
 
-	err = &errortypes.NotFoundError{
-		errors.Wrap(err, "auth: Jumpcloud user not found"),
+	if userId == "" {
+		err = &errortypes.NotFoundError{
+			errors.Wrap(err, "auth: Jumpcloud user not found"),
+		}
+		return
+	}
+
+	if provider.JumpCloudAppId != "" {
+		attached, e := jumpcloudCheckApp(provider, userId)
+		if e != nil {
+			err = e
+			return
+		}
+
+		if attached {
+			active = true
+		} else {
+			logrus.WithFields(logrus.Fields{
+				"user_id":  usr.Id.Hex(),
+				"username": usr.Username,
+			}).Info("auth: Jumpcloud user not bound to application")
+
+			return
+		}
+	} else {
+		active = true
 	}
 
 	return
