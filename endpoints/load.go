5ad26a534f7a83844aeee56f4cee040cac5526c5
Zachary Huff <zach.huff.386@gmail.com>
2022-07-09 07:02:41 -0400

    Update load endpoint query

--- endpoints/load.go
+++ endpoints/load.go
@@ -42,20 +42,12 @@ func (d *Load) StaticData() *bson.M {
 	return nil
 }
 
-type LoadChart struct {
-	Load1  []*ChartFloat `json:"load1"`
-	Load5  []*ChartFloat `json:"load5"`
-	Load15 []*ChartFloat `json:"load15"`
-}
-
 func GetLoadChartSingle(c context.Context, db *database.Database,
 	endpoint primitive.ObjectID, start, end time.Time) (
-	chart *LoadChart, err error) {
+	chartData ChartData, err error) {
 
 	coll := db.EndpointsLoad()
-	cpuUsage := []*ChartFloat{}
-	memUsage := []*ChartFloat{}
-	swapUsage := []*ChartFloat{}
+	chart := NewChart(start, end, time.Minute)
 
 	timeQuery := bson.D{
 		{"$gte", start},
@@ -90,18 +82,11 @@ func GetLoadChartSingle(c context.Context, db *database.Database,
 			return
 		}
 
-		cpuUsage = append(cpuUsage, &ChartFloat{
-			X: doc.Timestamp.Unix() * 1000,
-			Y: doc.Load1,
-		})
-		memUsage = append(memUsage, &ChartFloat{
-			X: doc.Timestamp.Unix() * 1000,
-			Y: doc.Load5,
-		})
-		swapUsage = append(swapUsage, &ChartFloat{
-			X: doc.Timestamp.Unix() * 1000,
-			Y: doc.Load15,
-		})
+		timestamp := doc.Timestamp.UnixMilli()
+
+		chart.Add("load1", timestamp, doc.Load1)
+		chart.Add("load5", timestamp, doc.Load5)
+		chart.Add("load15", timestamp, doc.Load15)
 	}
 
 	err = cursor.Err()
@@ -110,28 +95,22 @@ func GetLoadChartSingle(c context.Context, db *database.Database,
 		return
 	}
 
-	chart = &LoadChart{
-		Load1:  cpuUsage,
-		Load5:  memUsage,
-		Load15: swapUsage,
-	}
+	chartData = chart.Export()
 
 	return
 }
 
 func GetLoadChart(c context.Context, db *database.Database,
 	endpoint primitive.ObjectID, start, end time.Time,
-	interval time.Duration) (chart *LoadChart, err error) {
+	interval time.Duration) (chartData ChartData, err error) {
 
 	if interval == 1*time.Minute {
-		chart, err = GetLoadChartSingle(c, db, endpoint, start, end)
+		chartData, err = GetLoadChartSingle(c, db, endpoint, start, end)
 		return
 	}
 
 	coll := db.EndpointsLoad()
-	cpuUsage := []*ChartFloat{}
-	memUsage := []*ChartFloat{}
-	swapUsage := []*ChartFloat{}
+	chart := NewChart(start, end, interval)
 
 	timeQuery := bson.D{
 		{"$gte", start},
@@ -198,18 +177,9 @@ func GetLoadChart(c context.Context, db *database.Database,
 			return
 		}
 
-		cpuUsage = append(cpuUsage, &ChartFloat{
-			X: doc.Id,
-			Y: doc.Load1,
-		})
-		memUsage = append(memUsage, &ChartFloat{
-			X: doc.Id,
-			Y: doc.Load5,
-		})
-		swapUsage = append(swapUsage, &ChartFloat{
-			X: doc.Id,
-			Y: doc.Load15,
-		})
+		chart.Add("load1", doc.Id, doc.Load1)
+		chart.Add("load5", doc.Id, doc.Load5)
+		chart.Add("load15", doc.Id, doc.Load15)
 	}
 
 	err = cursor.Err()
@@ -218,11 +188,7 @@ func GetLoadChart(c context.Context, db *database.Database,
 		return
 	}
 
-	chart = &LoadChart{
-		Load1:  cpuUsage,
-		Load5:  memUsage,
-		Load15: swapUsage,
-	}
+	chartData = chart.Export()
 
 	return
 }
