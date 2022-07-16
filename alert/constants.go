ef429bedbff6154177fa6b2298d16a249f75779c
Zachary Huff <zach.huff.386@gmail.com>
2022-07-16 15:37:48 -0400

    Add types to alert constants

--- alert/constants.go
+++ alert/constants.go
@@ -7,3 +7,10 @@ const (
 	Medium  = 5
 	High    = 10
 )
+
+const (
+	SystemHighMemory    = "system_high_memory"
+	SystemHighSwap      = "system_high_swap"
+	SystemHighHugePages = "system_high_hugepages"
+	DiskHighUsage       = "disk_high_usage"
+)
