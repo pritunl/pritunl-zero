41ac179cfeebdf9d2f96bfcdf225e9a2cb30074f
Zachary Huff <zach.huff.386@gmail.com>
2022-07-25 11:54:00 -0400

    Add headers to middlewear

--- middlewear/middlewear.go
+++ middlewear/middlewear.go
@@ -42,6 +42,15 @@ func Database(c *gin.Context) {
 	db.Close()
 }
 
+func Headers(c *gin.Context) {
+	headers := c.Writer.Header()
+
+	//headers.Add("X-Frame-Options", "DENY")
+	//headers.Add("X-XSS-Protection", "1; mode=block")
+	//headers.Add("X-Content-Type-Options", "nosniff")
+	headers.Add("X-Robots-Tag", "noindex")
+}
+
 func SessionAdmin(c *gin.Context) {
 	db := c.MustGet("db").(*database.Database)
 
