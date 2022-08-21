ce18596f29965a4064f5d6bdb8a1e5c844cb8d7d
Zachary Huff <zach.huff.386@gmail.com>
2022-08-21 19:38:15 -0400

    Add endpoint kmsg component

---
+++ www/app/components/EndpointKmsg.tsx
@@ -0,0 +1,226 @@
+/// <reference path="../References.d.ts"/>
+import * as React from 'react';
+import * as Theme from '../Theme';
+import * as EndpointTypes from '../types/EndpointTypes';
+import * as EndpointActions from '../actions/EndpointActions';
+import {Ace} from "ace-builds";
+import AceEditor from "react-ace";
+
+import "ace-builds/src-noconflict/mode-text";
+import "ace-builds/src-noconflict/theme-dracula";
+import "ace-builds/src-noconflict/theme-eclipse";
+
+interface Props {
+	endpoint: string;
+	disabled: boolean;
+}
+
+interface State {
+	data: string;
+	loading: boolean;
+	cancelable: boolean;
+}
+
+const css = {
+	header: {
+		fontSize: '20px',
+		marginTop: '-10px',
+		paddingBottom: '2px',
+		marginBottom: '10px',
+		borderBottomStyle: 'solid',
+	} as React.CSSProperties,
+	heading: {
+		margin: '19px 0 0 0',
+	} as React.CSSProperties,
+	button: {
+		margin: '8px 0 0 8px',
+	} as React.CSSProperties,
+	buttons: {
+		marginTop: '8px',
+	} as React.CSSProperties,
+	group: {
+		flex: 1,
+		minWidth: '250px',
+		margin: '0 10px',
+	} as React.CSSProperties,
+	editorGroup: {
+		marginBottom: '15px',
+	} as React.CSSProperties,
+};
+
+export default class EndpointKmsg extends React.Component<Props, State> {
+	loaded: boolean;
+	editor: Ace.Editor;
+
+	constructor(props: any, context: any) {
+		super(props, context);
+		this.state = {
+			data: '',
+			loading: false,
+			cancelable: false,
+		};
+	}
+
+	componentDidMount(): void {
+		Theme.addChangeListener(this.onChange);
+	}
+
+	componentWillUnmount(): void {
+		Theme.removeChangeListener(this.onChange);
+	}
+
+	onChange = (): void => {
+		this.setState({
+			...this.state,
+		});
+	}
+
+	setLoading(): void {
+		this.setState({
+			...this.state,
+			loading: true,
+			cancelable: true,
+		});
+	}
+
+	setLoaded(): void {
+		this.setState({
+			...this.state,
+			loading: false,
+			cancelable: false,
+		});
+	}
+
+	update(): void {
+		let loading = true;
+		this.setLoading();
+
+		EndpointActions.log(
+			this.props.endpoint,
+			'kmsg',
+		).then((data: EndpointTypes.LogData): void => {
+			if (loading) {
+				loading = false;
+				this.setLoaded();
+			}
+
+			this.setState({
+				...this.state,
+				data: data.join(''),
+			});
+
+			setTimeout((): void => {
+				if (this.editor) {
+					this.editor.scrollToLine(Number.POSITIVE_INFINITY,
+						false, true, null);
+				}
+			}, 100);
+			setTimeout((): void => {
+				if (this.editor) {
+					this.editor.scrollToLine(Number.POSITIVE_INFINITY,
+						false, true, null);
+				}
+			}, 200);
+			setTimeout((): void => {
+				if (this.editor) {
+					this.editor.scrollToLine(Number.POSITIVE_INFINITY,
+						false, true, null);
+				}
+			}, 300);
+			setTimeout((): void => {
+				if (this.editor) {
+					this.editor.scrollToLine(Number.POSITIVE_INFINITY,
+						false, true, null);
+				}
+			}, 400);
+		}).catch((): void => {
+			if (loading) {
+				loading = false;
+				this.setLoaded();
+			}
+		});
+	}
+
+	render(): JSX.Element {
+		if (this.props.disabled) {
+			return <div/>;
+		}
+
+		if (!this.loaded) {
+			this.loaded = true;
+			setTimeout((): void => {
+				this.update();
+			});
+		}
+
+		let refreshDisabled = false;
+		let refreshLabel = '';
+		let refreshClass = 'bp3-button';
+		if (Object.entries(this.state.cancelable).length) {
+			refreshLabel = 'Cancel';
+			refreshClass += ' bp3-intent-warning bp3-icon-delete'
+		} else {
+			if (Object.entries(this.state.loading).length) {
+				refreshDisabled = true;
+			}
+			refreshLabel = 'Refresh';
+			refreshClass += ' bp3-intent-success bp3-icon-refresh'
+		}
+
+		return <div>
+			<div className="layout horizontal wrap bp3-border" style={css.header}>
+				<h3 style={css.heading}>Dmesg</h3>
+				<div className="flex"/>
+				<div style={css.buttons}>
+					<button
+						className={refreshClass}
+						style={css.button}
+						disabled={refreshDisabled}
+						type="button"
+						onClick={(): void => {
+							if (Object.entries(this.state.cancelable).length) {
+								EndpointActions.dataCancel();
+							} else {
+								this.update();
+							}
+						}}
+					>
+						{refreshLabel}
+					</button>
+				</div>
+			</div>
+			<div className="layout horizontal wrap" style={css.editorGroup}>
+				<AceEditor
+					name={this.props.endpoint + "-dmesg"}
+					theme={Theme.editorTheme()}
+					height="400px"
+					width="100%"
+					mode="text"
+					fontSize="12px"
+					wrapEnabled={true}
+					showPrintMargin={false}
+					showGutter={true}
+					readOnly={true}
+					value={this.state.data}
+					editorProps={{
+						$blockScrolling: true,
+					}}
+					setOptions={{
+						showFoldWidgets: false,
+					}}
+					onLoad={(editor: Ace.Editor): void => {
+						this.editor = editor;
+						this.editor.scrollToLine(Number.POSITIVE_INFINITY,
+							false, false, null);
+					}}
+					onChange={(): void => {
+						if (this.editor) {
+							this.editor.scrollToLine(Number.POSITIVE_INFINITY,
+								false, true, null);
+						}
+					}}
+				/>
+			</div>
+		</div>;
+	}
+}
