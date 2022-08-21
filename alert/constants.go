a2918b9aac34484e00262ece22352828dc935f24
Zachary Huff <zach.huff.386@gmail.com>
2022-08-20 22:53:03 -0400

    Add alert constants

---
+++ alert/constants.go
@@ -0,0 +1,14 @@
+package alert
+
+const (
+	Low    = 1
+	Medium = 5
+	High   = 10
+)
+
+const (
+	SystemHighMemory    = "system_high_memory"
+	SystemHighSwap      = "system_high_swap"
+	SystemHighHugePages = "system_high_hugepages"
+	DiskHighUsage       = "disk_high_usage"
+)
