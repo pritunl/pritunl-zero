9a96dab04d90c018812b1ce3c259d8c41366c02e
Zachary Huff <zach.huff.386@gmail.com>
2022-08-23 14:38:53 -0400

    Use estimated count in log utils

--- log/utils.go
+++ log/utils.go
@@ -29,10 +29,18 @@ func GetAll(db *database.Database, query *bson.M, page, pageCount int64) (
 	coll := db.Logs()
 	entries = []*Entry{}
 
-	count, err = coll.CountDocuments(db, query)
-	if err != nil {
-		err = database.ParseError(err)
-		return
+	if len(*query) == 0 {
+		count, err = coll.EstimatedDocumentCount(db)
+		if err != nil {
+			err = database.ParseError(err)
+			return
+		}
+	} else {
+		count, err = coll.CountDocuments(db, query)
+		if err != nil {
+			err = database.ParseError(err)
+			return
+		}
 	}
 
 	opts := options.FindOptions{
