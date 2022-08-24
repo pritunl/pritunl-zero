cf3050711c33bf97a0d96826f0ef4ef5a796d587
Zachary Huff <zach.huff.386@gmail.com>
2022-08-23 22:52:01 -0400

    Add endpoint to settings

---
+++ settings/endpoint.go
@@ -0,0 +1,23 @@
+package settings
+
+var Endpoint *endpoint
+
+type endpoint struct {
+	Id               string `bson:"_id"`
+	Name             string `bson:"name"`
+	KmsgDisplayLimit int64  `bson:"kmsg_display_limit" default:"5000"`
+}
+
+func newEndpoint() interface{} {
+	return &endpoint{
+		Id: "endpoint",
+	}
+}
+
+func updateEndpoint(data interface{}) {
+	Endpoint = data.(*endpoint)
+}
+
+func init() {
+	register("endpoint", newEndpoint, updateEndpoint)
+}
