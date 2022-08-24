206cce4cae5505fb6bee0a54a94259b7db47c4f3
Zachary Huff <zach.huff.386@gmail.com>
2022-08-23 22:51:43 -0400

    Add alerts type component

---
+++ www/app/types/AlertTypes.ts
@@ -0,0 +1,40 @@
+/// <reference path="../References.d.ts"/>
+export const SYNC = 'alert.sync';
+export const SYNC_NAMES = 'alert.sync_names';
+export const TRAVERSE = 'alert.traverse';
+export const FILTER = 'alert.filter';
+export const CHANGE = 'alert.change';
+
+export interface Alert {
+	id: string;
+	name?: string;
+	roles?: string[];
+	resource?: string;
+	level?: number;
+	value_int?: number;
+	value_str?: string;
+}
+
+export interface Filter {
+	id?: string;
+	name?: string;
+	role?: string;
+}
+
+export type Alerts = Alert[];
+
+export type AlertRo = Readonly<Alert>;
+export type AlertsRo = ReadonlyArray<AlertRo>;
+
+export interface AlertDispatch {
+	type: string;
+	data?: {
+		id?: string;
+		alert?: Alert;
+		alerts?: Alerts;
+		page?: number;
+		pageCount?: number;
+		filter?: Filter;
+		count?: number;
+	};
+}
