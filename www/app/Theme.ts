3f60125201bd8f9e3a37b2d5227e1a63911fd3ac
Zachary Huff <zach.huff.386@gmail.com>
2022-08-24 22:58:19 -0400

    Add editor theme to app theme

--- www/app/Theme.ts
+++ www/app/Theme.ts
@@ -3,6 +3,11 @@ import * as SuperAgent from 'superagent';
 import * as Alert from './Alert';
 import * as Csrf from './Csrf';
 
+export interface Callback {
+	(): void;
+}
+
+let callbacks: Set<Callback> = new Set<Callback>();
 export let theme = 'dark';
 
 export function save(): Promise<void> {
@@ -35,17 +40,39 @@ export function save(): Promise<void> {
 export function light(): void {
 	theme = 'light';
 	document.body.className = '';
+	callbacks.forEach((callback: Callback): void => {
+		callback();
+	});
 }
 
 export function dark(): void {
 	theme = 'dark';
 	document.body.className = 'bp3-dark';
+	callbacks.forEach((callback: Callback): void => {
+		callback();
+	});
 }
 
 export function toggle(): void {
 	if (theme === 'light') {
-		dark()
+		dark();
 	} else {
 		light();
 	}
+}
+
+export function editorTheme(): string {
+	if (theme === "light") {
+		return "eclipse";
+	} else {
+		return "dracula";
+	}
+}
+
+export function addChangeListener(callback: Callback): void {
+	callbacks.add(callback);
+}
+
+export function removeChangeListener(callback: () => void): void {
+	callbacks.delete(callback);
 }
