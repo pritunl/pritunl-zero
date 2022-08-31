3a304e6d0c37a34ca862d908e04927ea060858e3
Zachary Huff <zach.huff.386@gmail.com>
2022-08-30 20:49:44 -0400

    Update dist

--- mhandlers/check.go
+++
@@ -1,9 +0,0 @@
-package mhandlers
-
-import (
-	"github.com/gin-gonic/gin"
-)
-
-func checkGet(c *gin.Context) {
-	c.String(200, "ok")
-}
---
+++ mhandlers/healthcheck.go
@@ -0,0 +1,9 @@
+package mhandlers
+
+import (
+	"github.com/gin-gonic/gin"
+)
+
+func checkGet(c *gin.Context) {
+	c.String(200, "ok")
+}
