2a9c93984dd4da628856f86ada87d79f03545774
Zachary Huff <zach.huff.386@gmail.com>
2022-08-31 20:55:54 -0400

    Add check header component

---
+++ www/app/components/CheckHeader.tsx
@@ -0,0 +1,77 @@
+/// <reference path="../References.d.ts"/>
+import * as React from 'react';
+import * as CheckTypes from '../types/CheckTypes';
+
+interface Props {
+	header: CheckTypes.Header;
+	onChange: (state: CheckTypes.Header) => void;
+	onRemove: () => void;
+}
+
+const css = {
+	group: {
+		width: '100%',
+		maxWidth: '310px',
+		marginTop: '5px',
+	} as React.CSSProperties,
+	header: {
+		width: '100%',
+		borderRadius: '0 3px 3px 0',
+	} as React.CSSProperties,
+	headerBox: {
+		flex: '1',
+	} as React.CSSProperties,
+};
+
+export default class CheckHeader extends React.Component<Props, {}> {
+	clone(): CheckTypes.Header {
+		return {
+			...this.props.header,
+		};
+	}
+
+	render(): JSX.Element {
+		let header = this.props.header;
+
+		return <div className="bp3-control-group" style={css.group}>
+			<div style={css.headerBox}>
+				<input
+					className="bp3-input"
+					style={css.header}
+					type="text"
+					autoCapitalize="off"
+					spellCheck={false}
+					placeholder="Key"
+					value={header.key || ''}
+					onChange={(evt): void => {
+						let state = this.clone();
+						state.key = evt.target.value;
+						this.props.onChange(state);
+					}}
+				/>
+			</div>
+			<div style={css.headerBox}>
+				<input
+					className="bp3-input"
+					style={css.header}
+					type="text"
+					autoCapitalize="off"
+					spellCheck={false}
+					placeholder="Value"
+					value={header.value || ''}
+					onChange={(evt): void => {
+						let state = this.clone();
+						state.value = evt.target.value;
+						this.props.onChange(state);
+					}}
+				/>
+			</div>
+			<button
+				className="bp3-button bp3-minimal bp3-intent-danger bp3-icon-remove"
+				onClick={(): void => {
+					this.props.onRemove();
+				}}
+			/>
+		</div>;
+	}
+}
