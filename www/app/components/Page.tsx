cf38ec6686d1f2d4bec47ec37af97b2613ab0aa5
Zachary Huff <zach.huff.386@gmail.com>
2022-09-21 12:45:05 -0400

    Add wide to page component

--- www/app/components/Page.tsx
+++ www/app/components/Page.tsx
@@ -1,6 +1,10 @@
 /// <reference path="../References.d.ts"/>
 import * as React from 'react';
 
+interface Props {
+	wide?: boolean;
+}
+
 const css = {
 	page: {
 		margin: '0 auto',
@@ -8,11 +12,17 @@ const css = {
 		minWidth: '200px',
 		maxWidth: '1100px',
 	} as React.CSSProperties,
+	pageWide: {
+		margin: '0 auto',
+		padding: '30px 20px',
+		minWidth: '200px',
+		maxWidth: '1250px',
+	} as React.CSSProperties,
 };
 
-export default class Page extends React.Component<{}, {}> {
+export default class Page extends React.Component<Props, {}> {
 	render(): JSX.Element {
-		return <div style={css.page}>
+		return <div style={this.props.wide ? css.pageWide : css.page}>
 			{this.props.children}
 		</div>;
 	}
