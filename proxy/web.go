f29f7235e6405337e7916ecf0d4d460ee8a9009c
Zachary Huff <zach.huff.386@gmail.com>
2022-09-22 12:21:47 -0400

    Add header timeout to proxy web

--- proxy/web.go
+++ proxy/web.go
@@ -120,6 +120,8 @@ func newWeb(proxyProto string, proxyPort int, host *Host,
 		settings.Router.HandshakeTimeout) * time.Second
 	continueTimeout := time.Duration(
 		settings.Router.ContinueTimeout) * time.Second
+	headerTimeout := time.Duration(
+		settings.Router.HeaderTimeout) * time.Second
 
 	tlsConfig := &tls.Config{
 		MinVersion: tls.VersionTLS12,
@@ -166,8 +168,11 @@ func newWeb(proxyProto string, proxyPort int, host *Host,
 					KeepAlive: dialKeepAlive,
 					DualStack: true,
 				}).DialContext,
+				MaxResponseHeaderBytes: int64(
+					settings.Router.MaxResponseHeaderBytes),
 				MaxIdleConns:          maxIdleConns,
 				MaxIdleConnsPerHost:   maxIdleConnsPerHost,
+				ResponseHeaderTimeout: headerTimeout,
 				IdleConnTimeout:       idleConnTimeout,
 				TLSHandshakeTimeout:   handshakeTimeout,
 				ExpectContinueTimeout: continueTimeout,
