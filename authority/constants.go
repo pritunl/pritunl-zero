4a9e2c8732c378e182b8fbf4626d6f1a694054db
Zachary Huff <zach.huff.386@gmail.com>
2023-03-25 13:02:09 -0400

    Add username strip domain to authority constants

--- authority/constants.go
+++ authority/constants.go
@@ -6,9 +6,10 @@ const (
 	Connected    = "connected"
 	Disconnected = "disconnected"
 
-	UserId     = "user_id"
-	Username   = "username"
-	UsernameId = "username_id"
+	UserId              = "user_id"
+	Username            = "username"
+	UsernameId          = "username_id"
+	UsernameStripDomain = "username_strip_domain"
 
 	RSA4096 = "rsa4096"
 	ECP384  = "ecp384"
