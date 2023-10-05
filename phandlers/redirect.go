90c461e95c79df52eed2abd4aaf2d9cff40c4b0f
Zachary Huff <zach.huff.386@gmail.com>
2023-10-05 14:51:25 -0400

    Add query string to proxy redirect url

--- phandlers/redirect.go
+++ phandlers/redirect.go
@@ -12,7 +12,21 @@ func redirect(c *gin.Context) {
 	if strings.ToLower(c.Request.Header.Get("Upgrade")) == "websocket" {
 		c.AbortWithStatus(404)
 	} else {
+		var urlBuf strings.Builder
+
+		path := c.Request.URL.EscapedPath()
+		if path == "" || path[0] != '/' {
+			urlBuf.WriteByte('/')
+		}
+
+		urlBuf.WriteString(path)
+
+		if c.Request.URL.RawQuery != "" {
+			urlBuf.WriteByte('?')
+			urlBuf.WriteString(c.Request.URL.RawQuery)
+		}
+
 		c.Redirect(302, fmt.Sprintf("/?redirect_url=%s",
-			url.QueryEscape(c.Request.URL.Path)))
+			url.QueryEscape(urlBuf.String())))
 	}
 }
