2063b4d869cb60c544d21701455d1c74a74a35a2
Zachary Huff <zach.huff.386@gmail.com>
2024-01-14 18:19:18 -0500

    Add dns timeouts to acme settings

--- settings/acme.go
+++ settings/acme.go
@@ -3,8 +3,11 @@ package settings
 var Acme *acme
 
 type acme struct {
-	Id  string `bson:"_id"`
-	Url string `bson:"url" default:"https://acme-v01.api.letsencrypt.org"`
+	Id           string `bson:"_id"`
+	Url          string `bson:"url" default:"https://acme-v01.api.letsencrypt.org"`
+	DnsRetryRate int    `bson:"dns_retry_rate" default:"3"`
+	DnsTimeout   int    `bson:"dns_timeout" default:"45"`
+	DnsDelay     int    `bson:"dns_delay" default:"15"`
 }
 
 func newAcme() interface{} {
