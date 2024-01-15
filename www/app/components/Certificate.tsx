cc2522514b9dd503ade0aaff5425bde18ef15749
Zachary Huff <zach.huff.386@gmail.com>
2024-01-14 23:07:32 -0500

    Add secrets to certificate component

--- www/app/components/Certificate.tsx
+++ www/app/components/Certificate.tsx
@@ -1,6 +1,7 @@
 /// <reference path="../References.d.ts"/>
 import * as React from 'react';
 import * as CertificateTypes from '../types/CertificateTypes';
+import * as SecretTypes from '../types/SecretTypes';
 import * as CertificateActions from '../actions/CertificateActions';
 import * as MiscUtils from '../utils/MiscUtils';
 import CertificateDomain from './CertificateDomain';
@@ -14,6 +15,7 @@ import Help from './Help';
 
 interface Props {
 	certificate: CertificateTypes.CertificateRo;
+	secrets: SecretTypes.SecretsRo;
 }
 
 interface State {
@@ -247,6 +249,26 @@ export default class Certificate extends React.Component<Props, State> {
 
 		let info: CertificateTypes.Info = this.props.certificate.info || {};
 
+		let hasSecrets = false;
+		let secretsSelect: JSX.Element[] = [];
+		if (this.props.secrets.length) {
+			secretsSelect.push(<option key="null" value="">Select Secret</option>);
+
+			for (let secret of this.props.secrets) {
+				hasSecrets = true;
+				secretsSelect.push(
+					<option
+						key={secret.id}
+						value={secret.id}
+					>{secret.name}</option>,
+				);
+			}
+		}
+
+		if (!hasSecrets) {
+			secretsSelect = [<option key="null" value="">No Secrets</option>];
+		}
+
 		let domains: JSX.Element[] = [];
 		for (let i = 0; i < cert.acme_domains.length; i++) {
 			let index = i;
@@ -375,6 +397,7 @@ export default class Certificate extends React.Component<Props, State> {
 					/>
 					<PageSelect
 						label="Type"
+						disabled={this.state.disabled}
 						help="Certificate type, use text to provide a certificate. LetsEncrypt provides free certificates that automatically renew."
 						value={cert.type}
 						onChange={(val): void => {
@@ -383,6 +406,43 @@ export default class Certificate extends React.Component<Props, State> {
 					>
 						<option value="text">Text</option>
 						<option value="lets_encrypt">LetsEncrypt</option>
+					</PageSelect>
+					<PageSelect
+						label="LetsEncrypt Verification Type"
+						disabled={this.state.disabled}
+						hidden={cert.type != "lets_encrypt"}
+						help="Verification type for LetsEncrypt certificate. HTTP verification will use a HTTP request on port 80 from the host. DNS will use a DNS API provider to set a DNS TXT record."
+						value={cert.acme_type}
+						onChange={(val): void => {
+							this.set('acme_type', val);
+						}}
+					>
+						<option value="acme_http">HTTP</option>
+						<option value="acme_dns">DNS TXT</option>
+					</PageSelect>
+					<PageSelect
+						label="LetsEncrypt Verification Provider"
+						disabled={this.state.disabled}
+						hidden={cert.acme_type != "acme_dns"}
+						help="API provider for LetsEncrypt verification."
+						value={cert.acme_auth}
+						onChange={(val): void => {
+							this.set('acme_auth', val);
+						}}
+					>
+						<option value="acme_aws">AWS</option>
+					</PageSelect>
+					<PageSelect
+						disabled={this.state.disabled}
+						hidden={cert.acme_type != "acme_dns"}
+						label="LetsEncrypt Verification Secret"
+						help="Secret containing API keys to use for LetsEncrypt verification."
+						value={cert.acme_secret}
+						onChange={(val): void => {
+							this.set('acme_secret', val);
+						}}
+					>
+						{secretsSelect}
 					</PageSelect>
 				</div>
 			</div>
