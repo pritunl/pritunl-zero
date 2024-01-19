561717b7d077710aada381489725757eab8ad51b
Zachary Huff <zach.huff.386@gmail.com>
2024-01-19 18:42:13 -0500

    Add secret types

---
+++ www/app/types/SecretTypes.ts
@@ -0,0 +1,26 @@
+/// <reference path="../References.d.ts"/>
+export const SYNC = 'secret.sync';
+export const CHANGE = 'secret.change';
+
+export interface Secret {
+	id?: string;
+	name?: string;
+	comment?: string;
+	type?: string;
+	key?: string;
+	value?: string;
+}
+
+export type Secrets = Secret[];
+
+export type SecretRo = Readonly<Secret>;
+export type SecretsRo = ReadonlyArray<SecretRo>;
+
+export interface SecretDispatch {
+	type: string;
+	data?: {
+		id?: string;
+		secret?: Secret;
+		secrets?: Secrets;
+	};
+}
