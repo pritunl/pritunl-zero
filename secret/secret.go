404b7e139c68293abee7fa1674680ad9a9abf918
Zachary Huff <zach.huff.386@gmail.com>
2024-01-21 06:25:21 -0500

    Add private key to secret

--- secret/secret.go
+++ secret/secret.go
@@ -1,21 +1,26 @@
 package secret
 
 import (
+	"strings"
+
 	"github.com/dropbox/godropbox/container/set"
 	"github.com/dropbox/godropbox/errors"
 	"github.com/pritunl/mongo-go-driver/bson/primitive"
 	"github.com/pritunl/pritunl-zero/database"
 	"github.com/pritunl/pritunl-zero/errortypes"
+	"github.com/pritunl/pritunl-zero/utils"
 )
 
 type Secret struct {
-	Id      primitive.ObjectID `bson:"_id,omitempty" json:"id"`
-	Name    string             `bson:"name" json:"name"`
-	Comment string             `bson:"comment" json:"comment"`
-	Type    string             `bson:"type" json:"type"`
-	Key     string             `bson:"key" json:"key"`
-	Region  string             `bson:"region" json:"region"`
-	Value   string             `bson:"value" json:"value"`
+	Id         primitive.ObjectID `bson:"_id,omitempty" json:"id"`
+	Name       string             `bson:"name" json:"name"`
+	Comment    string             `bson:"comment" json:"comment"`
+	Type       string             `bson:"type" json:"type"`
+	Key        string             `bson:"key" json:"key"`
+	Value      string             `bson:"value" json:"value"`
+	Region     string             `bson:"region" json:"region"`
+	PublicKey  string             `bson:"public_key" json:"public_key"`
+	PrivateKey string             `bson:"private_key" json:"-"`
 }
 
 func (c *Secret) Validate(db *database.Database) (
@@ -30,11 +35,38 @@ func (c *Secret) Validate(db *database.Database) (
 		}
 
 		break
+	case Cloudflare:
+		c.Value = ""
+		c.Region = ""
+
+		break
+	case OracleCloud:
+		break
 	default:
 		errData = &errortypes.ErrorData{
 			Error:   "invalid_secret_type",
 			Message: "Secret type invalid",
 		}
+		return
+	}
+
+	if c.PrivateKey == "" {
+		privKey, pubKey, e := utils.GenerateRsaKey()
+		if e != nil {
+			err = e
+			return
+		}
+
+		c.PublicKey = strings.TrimSpace(string(pubKey))
+		c.PrivateKey = strings.TrimSpace(string(privKey))
+	}
+
+	return
+}
+
+func (c *Secret) GetOracleProvider() (prov *OracleProvider, err error) {
+	prov, err = NewOracleProvider(c)
+	if err != nil {
 		return
 	}
 
