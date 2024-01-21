aac846146b0435b3fc00717cb88b7604615cf7e5
Zachary Huff <zach.huff.386@gmail.com>
2024-01-21 06:25:32 -0500

    Add load private key to secret utils

--- secret/utils.go
+++ secret/utils.go
@@ -1,9 +1,18 @@
 package secret
 
 import (
+	"bytes"
+	"crypto/md5"
+	"crypto/rsa"
+	"crypto/x509"
+	"encoding/pem"
+	"fmt"
+
+	"github.com/dropbox/godropbox/errors"
 	"github.com/pritunl/mongo-go-driver/bson"
 	"github.com/pritunl/mongo-go-driver/bson/primitive"
 	"github.com/pritunl/pritunl-zero/database"
+	"github.com/pritunl/pritunl-zero/errortypes"
 )
 
 func Get(db *database.Database, secrId primitive.ObjectID) (
@@ -60,6 +69,56 @@ func Remove(db *database.Database, secrId primitive.ObjectID) (err error) {
 		err = database.ParseError(err)
 		return
 	}
+
+	return
+}
+
+func loadPrivateKey(secr *Secret) (
+	key *rsa.PrivateKey, fingerprint string, err error) {
+
+	block, _ := pem.Decode([]byte(secr.PrivateKey))
+	if block == nil {
+		err = &errortypes.ParseError{
+			errors.New("secret: Failed to decode private key"),
+		}
+		return
+	}
+
+	if block.Type != "RSA PRIVATE KEY" {
+		err = &errortypes.ParseError{
+			errors.New("secret: Invalid private key type"),
+		}
+		return
+	}
+
+	key, err = x509.ParsePKCS1PrivateKey(block.Bytes)
+	if err != nil {
+		err = &errortypes.ParseError{
+			errors.Wrap(err, "secret: Failed to parse rsa key"),
+		}
+		return
+	}
+
+	pubKey, err := x509.MarshalPKIXPublicKey(key.Public())
+	if err != nil {
+		err = &errortypes.ParseError{
+			errors.Wrap(err, "secret: Failed to marshal public key"),
+		}
+		return
+	}
+
+	keyHash := md5.New()
+	keyHash.Write(pubKey)
+	fingerprint = fmt.Sprintf("%x", keyHash.Sum(nil))
+	fingerprintBuf := bytes.Buffer{}
+
+	for i, run := range fingerprint {
+		fingerprintBuf.WriteRune(run)
+		if i%2 == 1 && i != len(fingerprint)-1 {
+			fingerprintBuf.WriteRune(':')
+		}
+	}
+	fingerprint = fingerprintBuf.String()
 
 	return
 }
