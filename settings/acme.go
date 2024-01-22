ad5e3259adbe9831b24673c692cd39cd330084c7
Zachary Huff <zach.huff.386@gmail.com>
2024-01-22 09:04:32 -0500

    Add dns ttl to acme settings

--- settings/acme.go
+++ settings/acme.go
@@ -3,11 +3,14 @@ package settings
 var Acme *acme
 
 type acme struct {
-	Id           string `bson:"_id"`
-	Url          string `bson:"url" default:"https://acme-v01.api.letsencrypt.org"`
-	DnsRetryRate int    `bson:"dns_retry_rate" default:"3"`
-	DnsTimeout   int    `bson:"dns_timeout" default:"45"`
-	DnsDelay     int    `bson:"dns_delay" default:"15"`
+	Id                string `bson:"_id"`
+	Url               string `bson:"url" default:"https://acme-v01.api.letsencrypt.org"`
+	DnsRetryRate      int    `bson:"dns_retry_rate" default:"3"`
+	DnsTimeout        int    `bson:"dns_timeout" default:"45"`
+	DnsDelay          int    `bson:"dns_delay" default:"15"`
+	DnsAwsTtl         int    `bson:"dns_aws_ttl" default:"30"`
+	DnsCloudflareTtl  int    `bson:"dns_cloudflare_ttl" default:"60"`
+	DnsOracleCloudTtl int    `bson:"dns_oracle_cloud_ttl" default:"30"`
 }
 
 func newAcme() interface{} {
