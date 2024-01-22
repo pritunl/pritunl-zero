1d7c7dd5dabd66a84a6a964543a3a4aa30bec5e3
Zachary Huff <zach.huff.386@gmail.com>
2024-01-22 09:03:45 -0500

    Add oracle cloud to certificate

--- certificate/certificate.go
+++ certificate/certificate.go
@@ -78,6 +78,8 @@ func (c *Certificate) Validate(db *database.Database) (
 			break
 		case AcmeCloudflare:
 			break
+		case AcmeOracleCloud:
+			break
 		default:
 			errData = &errortypes.ErrorData{
 				Error:   "acme_auth_invalid",
