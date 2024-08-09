e18dc19a502abf6afbee0162e74bd722da084279
Zachary Huff <zach.huff.386@gmail.com>
2024-08-09 18:30:12 -0400

    Remove extract domain from acme utils

--- acme/utils.go
+++ acme/utils.go
@@ -177,16 +177,3 @@ func newEcCsr(domains []string) (csr []byte, keyPem []byte, err error) {
 
 	return
 }
-
-func extractDomain(domain string) string {
-	domain = strings.Trim(domain, ".")
-	parts := strings.Split(domain, ".")
-	if len(parts) >= 2 {
-		return parts[len(parts)-2] + "." + parts[len(parts)-1]
-	}
-	return domain
-}
-
-func cleanDomain(domain string) string {
-	return strings.Trim(domain, ".")
-}
