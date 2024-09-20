7a4ba8af1d3fa0b053f65096cc2a52ed08357ab3
Zachary Huff <zach.huff.386@gmail.com>
2024-09-20 04:00:10 -0400

    Add filter to utils

---
+++ utils/filter.go
@@ -0,0 +1,92 @@
+package utils
+
+import (
+	"github.com/dropbox/godropbox/container/set"
+)
+
+const nameSafeLimit = 128
+
+var nameSafeChar = set.NewSet(
+	'a',
+	'b',
+	'c',
+	'd',
+	'e',
+	'f',
+	'g',
+	'h',
+	'i',
+	'j',
+	'k',
+	'l',
+	'm',
+	'n',
+	'o',
+	'p',
+	'q',
+	'r',
+	's',
+	't',
+	'u',
+	'v',
+	'w',
+	'x',
+	'y',
+	'z',
+	'A',
+	'B',
+	'C',
+	'D',
+	'E',
+	'F',
+	'G',
+	'H',
+	'I',
+	'J',
+	'K',
+	'L',
+	'M',
+	'N',
+	'O',
+	'P',
+	'Q',
+	'R',
+	'S',
+	'T',
+	'U',
+	'V',
+	'W',
+	'X',
+	'Y',
+	'Z',
+	'0',
+	'1',
+	'2',
+	'3',
+	'4',
+	'5',
+	'6',
+	'7',
+	'8',
+	'9',
+	'-',
+)
+
+func FilterName(s string) string {
+	if len(s) == 0 {
+		return ""
+	}
+
+	if len(s) > nameSafeLimit {
+		s = s[:nameSafeLimit]
+	}
+
+	ns := ""
+	for _, c := range s {
+		if safeChars.Contains(c) {
+			ns += string(c)
+		}
+	}
+
+	return ns
+}
