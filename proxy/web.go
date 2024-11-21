e1767073010e92e8b3778a1bb27cadbd4c683380
Zachary Huff <zach.huff.386@gmail.com>
2024-11-21 16:16:29 -0500

    Add support for http2 to web proxy

--- proxy/web.go
+++ proxy/web.go
@@ -14,7 +14,9 @@ import (
 	"strings"
 	"time"
 
+	"github.com/dropbox/godropbox/errors"
 	"github.com/pritunl/pritunl-zero/authorizer"
+	"github.com/pritunl/pritunl-zero/errortypes"
 	"github.com/pritunl/pritunl-zero/logger"
 	"github.com/pritunl/pritunl-zero/node"
 	"github.com/pritunl/pritunl-zero/searches"
@@ -22,6 +24,7 @@ import (
 	"github.com/pritunl/pritunl-zero/settings"
 	"github.com/pritunl/pritunl-zero/utils"
 	"github.com/sirupsen/logrus"
+	"golang.org/x/net/http2"
 )
 
 type web struct {
@@ -127,6 +130,11 @@ func newWeb(proxyProto string, proxyPort int, host *Host,
 		MinVersion: tls.VersionTLS12,
 		MaxVersion: tls.VersionTLS13,
 	}
+
+	if host.Service.Http2 {
+		tlsConfig.NextProtos = []string{"h2"}
+	}
+
 	if settings.Router.SkipVerify || net.ParseIP(server.Hostname) != nil {
 		tlsConfig.InsecureSkipVerify = true
 	}
@@ -154,32 +162,68 @@ func newWeb(proxyProto string, proxyPort int, host *Host,
 		},
 	}
 
+	var dialer2 *net.Dialer
+	if host.Service.Http2 {
+		dialer2 = &net.Dialer{
+			Timeout:   dialTimeout,
+			KeepAlive: dialKeepAlive,
+			DualStack: true,
+		}
+	}
+
+	transportFix := &TransportFix{
+		transport: &http.Transport{
+			Proxy: http.ProxyFromEnvironment,
+			DialContext: (&net.Dialer{
+				Timeout:   dialTimeout,
+				KeepAlive: dialKeepAlive,
+				DualStack: true,
+			}).DialContext,
+			MaxResponseHeaderBytes: int64(
+				settings.Router.MaxResponseHeaderBytes),
+			MaxIdleConns:          maxIdleConns,
+			MaxIdleConnsPerHost:   maxIdleConnsPerHost,
+			ResponseHeaderTimeout: headerTimeout,
+			IdleConnTimeout:       idleConnTimeout,
+			TLSHandshakeTimeout:   handshakeTimeout,
+			ExpectContinueTimeout: continueTimeout,
+			TLSClientConfig:       tlsConfig,
+		},
+	}
+
+	if host.Service.Http2 {
+		transportFix.transport2 = &http2.Transport{
+			TLSClientConfig: tlsConfig,
+			DialTLS: func(network, addr string, cfg *tls.Config) (
+				conn net.Conn, err error) {
+
+				dialConn, err := dialer2.Dial(network, addr)
+				if err != nil {
+					err = &errortypes.RequestError{
+						errors.Wrap(err, "proxy: Transport dialer error"),
+					}
+					return
+				}
+
+				if server.Protocol == "http" {
+					conn = dialConn
+				} else {
+					conn = tls.Client(dialConn, cfg)
+				}
+				return
+			},
+			AllowHTTP: true,
+		}
+	}
+
 	w = &web{
 		reqHost:     host.Domain.Host,
 		serverProto: server.Protocol,
 		serverHost:  utils.FormatHostPort(server.Hostname, server.Port),
 		proxyProto:  proxyProto,
 		proxyPort:   proxyPort,
-		Transport: &TransportFix{
-			transport: &http.Transport{
-				Proxy: http.ProxyFromEnvironment,
-				DialContext: (&net.Dialer{
-					Timeout:   dialTimeout,
-					KeepAlive: dialKeepAlive,
-					DualStack: true,
-				}).DialContext,
-				MaxResponseHeaderBytes: int64(
-					settings.Router.MaxResponseHeaderBytes),
-				MaxIdleConns:          maxIdleConns,
-				MaxIdleConnsPerHost:   maxIdleConnsPerHost,
-				ResponseHeaderTimeout: headerTimeout,
-				IdleConnTimeout:       idleConnTimeout,
-				TLSHandshakeTimeout:   handshakeTimeout,
-				ExpectContinueTimeout: continueTimeout,
-				TLSClientConfig:       tlsConfig,
-			},
-		},
-		ErrorLog: log.New(writer, "", 0),
+		Transport:   transportFix,
+		ErrorLog:    log.New(writer, "", 0),
 	}
 
 	return
