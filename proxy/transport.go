5ba6d9ee893af0d93ac8566800b372240b0f27ee
Zachary Huff <zach.huff.386@gmail.com>
2024-11-21 22:56:19 -0500

    Add transport2 to proxy

--- proxy/transport.go
+++ proxy/transport.go
@@ -5,10 +5,12 @@ import (
 
 	"github.com/dropbox/godropbox/errors"
 	"github.com/pritunl/pritunl-zero/node"
+	"golang.org/x/net/http2"
 )
 
 type TransportFix struct {
-	transport *http.Transport
+	transport  *http.Transport
+	transport2 *http2.Transport
 }
 
 func (t *TransportFix) RoundTrip(r *http.Request) (
@@ -16,9 +18,16 @@ func (t *TransportFix) RoundTrip(r *http.Request) (
 
 	r.Header.Set("X-Forwarded-For", node.Self.GetRemoteAddr(r))
 
-	res, err = t.transport.RoundTrip(r)
-	if err != nil {
-		return
+	if t.transport2 != nil {
+		res, err = t.transport2.RoundTrip(r)
+		if err != nil {
+			return
+		}
+	} else {
+		res, err = t.transport.RoundTrip(r)
+		if err != nil {
+			return
+		}
 	}
 
 	if res.StatusCode == http.StatusSwitchingProtocols {
