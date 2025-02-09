80106a785d3d51f5c95628f7ebc5676cbfa18468
Zachary Huff <zach.huff.386@gmail.com>
2025-02-09 10:48:30 -0500

    Add write error to proxy utils

--- proxy/utils.go
+++ proxy/utils.go
@@ -1,16 +1,43 @@
 package proxy
 
 import (
+	"fmt"
+	"io"
 	"net/http"
 	"strings"
 
+	"github.com/pritunl/pritunl-zero/errortypes"
 	"github.com/pritunl/pritunl-zero/node"
 	"github.com/pritunl/pritunl-zero/utils"
 	"github.com/sirupsen/logrus"
 )
 
-func WriteErrorLog(w http.ResponseWriter, r *http.Request, code int, err error) {
-	http.Error(w, utils.GetStatusMessage(code), code)
+func WriteError(w http.ResponseWriter, r *http.Request, code int, err error) {
+	var msg string
+	if r != nil && r.Body != nil {
+		defer r.Body.Close()
+
+		body, readErr := io.ReadAll(io.LimitReader(r.Body, 4096))
+		if readErr == nil && len(body) > 0 {
+			msg = string(body)
+		}
+	}
+
+	if msg == "" {
+		if reqErr, ok := err.(*errortypes.RequestError); ok {
+			msg = fmt.Sprintf("%d: %s", code, reqErr.GetMessage())
+		} else {
+			msg = utils.GetStatusMessage(code)
+		}
+	}
+
+	http.Error(w, msg, code)
+}
+
+func WriteErrorLog(w http.ResponseWriter, r *http.Request, code int,
+	err error) {
+
+	WriteError(w, r, code, err)
 
 	logrus.WithFields(logrus.Fields{
 		"client": node.Self.GetRemoteAddr(r),
