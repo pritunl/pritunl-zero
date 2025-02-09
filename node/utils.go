0a7a50cfefe658a634447ce2bfb900dd7298724d
Zachary Huff <zach.huff.386@gmail.com>
2025-02-09 16:16:04 -0500

    Add get all paged to node utils

--- node/utils.go
+++ node/utils.go
@@ -3,7 +3,9 @@ package node
 import (
 	"github.com/pritunl/mongo-go-driver/bson"
 	"github.com/pritunl/mongo-go-driver/bson/primitive"
+	"github.com/pritunl/mongo-go-driver/mongo/options"
 	"github.com/pritunl/pritunl-zero/database"
+	"github.com/pritunl/pritunl-zero/utils"
 )
 
 func Get(db *database.Database, nodeId primitive.ObjectID) (
@@ -25,6 +27,71 @@ func GetAll(db *database.Database) (nodes []*Node, err error) {
 	nodes = []*Node{}
 
 	cursor, err := coll.Find(db, bson.M{})
+	if err != nil {
+		err = database.ParseError(err)
+		return
+	}
+	defer cursor.Close(db)
+
+	for cursor.Next(db) {
+		nde := &Node{}
+		err = cursor.Decode(nde)
+		if err != nil {
+			err = database.ParseError(err)
+			return
+		}
+
+		nde.SetActive()
+		nodes = append(nodes, nde)
+	}
+
+	err = cursor.Err()
+	if err != nil {
+		err = database.ParseError(err)
+		return
+	}
+
+	return
+}
+
+func GetAllPaged(db *database.Database, query *bson.M,
+	page, pageCount int64) (nodes []*Node, count int64, err error) {
+
+	coll := db.Nodes()
+	nodes = []*Node{}
+
+	if len(*query) == 0 {
+		count, err = coll.EstimatedDocumentCount(db)
+		if err != nil {
+			err = database.ParseError(err)
+			return
+		}
+	} else {
+		count, err = coll.CountDocuments(db, query)
+		if err != nil {
+			err = database.ParseError(err)
+			return
+		}
+	}
+
+	maxPage := count / pageCount
+	if count == pageCount {
+		maxPage = 0
+	}
+	page = utils.Min64(page, maxPage)
+	skip := utils.Min64(page*pageCount, count)
+
+	cursor, err := coll.Find(
+		db,
+		query,
+		&options.FindOptions{
+			Sort: &bson.D{
+				{"name", 1},
+			},
+			Skip:  &skip,
+			Limit: &pageCount,
+		},
+	)
 	if err != nil {
 		err = database.ParseError(err)
 		return
