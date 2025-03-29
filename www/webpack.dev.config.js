dc3cf3f1a1075240c643cc252a9e26a337dcac58
Zachary Huff <zach.huff.386@gmail.com>
2025-03-29 06:57:33 -0400

    Update webpack dev config

--- www/webpack.dev.config.js
+++ www/webpack.dev.config.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const webpack = require('webpack');
 const TerserPlugin = require('terser-webpack-plugin');
+const MonacoWebpackPlugin = require('monaco-editor-webpack-plugin');
 
 module.exports = {
   mode: 'development',
@@ -21,8 +22,9 @@ module.exports = {
   },
   output: {
     path: path.resolve(__dirname, 'dist-dev', 'static'),
-    publicPath: '',
+    publicPath: '/static/',
     filename: '[name].js',
+    globalObject: 'self',
   },
   module: {
     rules: [
@@ -31,11 +33,34 @@ module.exports = {
         enforce: 'pre',
         use: ['source-map-loader'],
       },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(ttf|woff|woff2|eot|svg)$/,
+        type: 'asset/resource',
+      },
     ],
   },
   plugins: [
     new webpack.DefinePlugin({
       'process.env': JSON.stringify({}),
     }),
+    new MonacoWebpackPlugin({
+      publicPath: '/static/',
+      languages: ['markdown', 'yaml', 'python'],
+      features: ['all'],
+      customLanguages: [
+        {
+          label: 'yaml',
+          entry: 'monaco-yaml',
+          worker: {
+            id: 'monaco-yaml/yamlWorker',
+            entry: 'monaco-yaml/yaml.worker',
+          },
+        },
+      ],
+    })
   ],
 };
