7d66a205488a97250bf7e410fd3d2afd05076521
Zachary Huff <zach.huff.386@gmail.com>
2025-04-04 17:44:05 -0400

    Add disable policies command

---
+++ cmd/disable_policies.go
@@ -0,0 +1,41 @@
+package cmd
+
+import (
+	"github.com/pritunl/mongo-go-driver/bson"
+	"github.com/pritunl/pritunl-zero/database"
+	"github.com/sirupsen/logrus"
+	"github.com/spf13/cobra"
+)
+
+func init() {
+	RootCmd.AddCommand(DisablePoliciesCmd)
+}
+
+var DisablePoliciesCmd = &cobra.Command{
+	Use:   "disable-policies",
+	Short: "Disable all active policies",
+	Run: func(cmd *cobra.Command, args []string) {
+		Init()
+
+		db := database.GetDatabase()
+		defer db.Close()
+
+		coll := db.Policies()
+
+		_, err := coll.UpdateMany(db, &bson.M{}, &bson.M{
+			"$set": &bson.M{
+				"disabled": true,
+			},
+		})
+		if err != nil {
+			if _, ok := err.(*database.NotFoundError); ok {
+				err = nil
+			} else {
+				cobra.CheckErr(err)
+				return
+			}
+		}
+
+		logrus.Info("cmd: Policies disabled")
+	},
+}
