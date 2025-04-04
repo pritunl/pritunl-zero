daea8b32c98bf0264c5488cd20497e1340e4da24
Zachary Huff <zach.huff.386@gmail.com>
2025-04-04 17:44:41 -0400

    Add reset id command

---
+++ cmd/reset_id.go
@@ -0,0 +1,39 @@
+package cmd
+
+import (
+	"github.com/pritunl/mongo-go-driver/bson/primitive"
+	"github.com/pritunl/pritunl-zero/config"
+	"github.com/pritunl/pritunl-zero/logger"
+	"github.com/sirupsen/logrus"
+	"github.com/spf13/cobra"
+)
+
+func init() {
+	RootCmd.AddCommand(ResetIdCmd)
+}
+
+var ResetIdCmd = &cobra.Command{
+	Use:   "reset-id",
+	Short: "Reset node ID",
+	Run: func(cmd *cobra.Command, args []string) {
+		logger.Init()
+
+		err := config.Load()
+		if err != nil {
+			cobra.CheckErr(err)
+			return
+		}
+
+		config.Config.NodeId = primitive.NewObjectID().Hex()
+
+		err = config.Save()
+		if err != nil {
+			cobra.CheckErr(err)
+			return
+		}
+
+		logrus.WithFields(logrus.Fields{
+			"node_id": config.Config.NodeId,
+		}).Info("cmd: Reset node ID")
+	},
+}
