bdc046c985759c70a1a1ae5137a92e9fca9e1635
Zachary Huff <zach.huff.386@gmail.com>
2025-04-07 17:17:58 -0400

    Update cmd init

--- cmd/utils.go
+++ cmd/utils.go
@@ -3,11 +3,9 @@ package cmd
 import (
 	"github.com/pritunl/pritunl-zero/logger"
 	"github.com/pritunl/pritunl-zero/requires"
-	"github.com/pritunl/pritunl-zero/task"
 )
 
 func Init() {
 	logger.Init()
 	requires.Init()
-	task.Init()
 }
