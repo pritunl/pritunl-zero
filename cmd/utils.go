eb885d230c0ace899397019ed2f0aca1051f054a
Zachary Huff <zach.huff.386@gmail.com>
2025-04-11 18:51:51 -0400

    Add init minimal to cmd utils

--- cmd/utils.go
+++ cmd/utils.go
@@ -1,6 +1,8 @@
 package cmd
 
 import (
+	"github.com/pritunl/pritunl-zero/config"
+	"github.com/pritunl/pritunl-zero/database"
 	"github.com/pritunl/pritunl-zero/logger"
 	"github.com/pritunl/pritunl-zero/requires"
 )
@@ -8,4 +10,18 @@ import (
 func Init() {
 	logger.Init()
 	requires.Init()
+}
+
+func InitMinimal() {
+	logger.Init()
+
+	err := config.Load()
+	if err != nil {
+		panic(err)
+	}
+
+	err = database.Connect()
+	if err != nil {
+		panic(err)
+	}
 }
