dfdc5280f9bae61deed3afba499978d6802d7e3b
Zachary Huff <zach.huff.386@gmail.com>
2025-04-12 18:33:02 -0400

    Add paging to certificate actions

--- www/app/actions/CertificateActions.ts
+++ www/app/actions/CertificateActions.ts
@@ -6,6 +6,7 @@ import * as Alert from '../Alert';
 import * as Csrf from '../Csrf';
 import Loader from '../Loader';
 import * as CertificateTypes from '../types/CertificateTypes';
+import CertificatesStore from '../stores/CertificatesStore';
 import * as MiscUtils from '../utils/MiscUtils';
 
 let syncId: string;
@@ -19,6 +20,11 @@ export function sync(): Promise<void> {
 	return new Promise<void>((resolve, reject): void => {
 		SuperAgent
 			.get('/certificate')
+			.query({
+				...CertificatesStore.filter,
+				page: CertificatesStore.page,
+				page_count: CertificatesStore.pageCount,
+			})
 			.set('Accept', 'application/json')
 			.set('Csrf-Token', Csrf.token)
 			.end((err: any, res: SuperAgent.Response): void => {
@@ -45,12 +51,33 @@ export function sync(): Promise<void> {
 					type: CertificateTypes.SYNC,
 					data: {
 						certificates: res.body,
+						count: res.body.count,
 					},
 				});
 
 				resolve();
 			});
 	});
+}
+
+export function traverse(page: number): Promise<void> {
+	Dispatcher.dispatch({
+		type: CertificateTypes.TRAVERSE,
+		data: {
+			page: page,
+		},
+	});
+	return sync();
+}
+
+export function filter(filt: CertificateTypes.Filter): Promise<void> {
+	Dispatcher.dispatch({
+		type: CertificateTypes.FILTER,
+		data: {
+			filter: filt,
+		},
+	});
+	return sync();
 }
 
 export function commit(cert: CertificateTypes.Certificate): Promise<void> {
@@ -117,6 +144,35 @@ export function remove(certId: string): Promise<void> {
 	return new Promise<void>((resolve, reject): void => {
 		SuperAgent
 			.delete('/certificate/' + certId)
+			.set('Accept', 'application/json')
+			.set('Csrf-Token', Csrf.token)
+			.end((err: any, res: SuperAgent.Response): void => {
+				loader.done();
+
+				if (res && res.status === 401) {
+					window.location.href = '/login';
+					resolve();
+					return;
+				}
+
+				if (err) {
+					Alert.errorRes(res, 'Failed to delete certificates');
+					reject(err);
+					return;
+				}
+
+				resolve();
+			});
+	});
+}
+
+export function removeMulti(certificateIds: string[]): Promise<void> {
+	let loader = new Loader().loading();
+
+	return new Promise<void>((resolve, reject): void => {
+		SuperAgent
+			.delete('/certificate')
+			.send(certificateIds)
 			.set('Accept', 'application/json')
 			.set('Csrf-Token', Csrf.token)
 			.end((err: any, res: SuperAgent.Response): void => {
