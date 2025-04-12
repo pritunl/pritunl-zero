6f0a2c159818fd22aab7afb068f34fd43485afba
Zachary Huff <zach.huff.386@gmail.com>
2025-04-12 19:33:47 -0400

    Add paging to authority utils

--- authority/utils.go
+++ authority/utils.go
@@ -18,6 +18,7 @@ import (
 	"github.com/dropbox/godropbox/errors"
 	"github.com/pritunl/mongo-go-driver/bson"
 	"github.com/pritunl/mongo-go-driver/bson/primitive"
+	"github.com/pritunl/mongo-go-driver/mongo/options"
 	"github.com/pritunl/pritunl-zero/database"
 	"github.com/pritunl/pritunl-zero/errortypes"
 	"github.com/pritunl/pritunl-zero/utils"
@@ -461,6 +462,70 @@ func GetAll(db *database.Database) (authrs []*Authority, err error) {
 	return
 }
 
+func GetAllPaged(db *database.Database, query *bson.M,
+	page, pageCount int64) (authorities []*Authority, count int64, err error) {
+
+	coll := db.Authorities()
+	authorities = []*Authority{}
+
+	if len(*query) == 0 {
+		count, err = coll.EstimatedDocumentCount(db)
+		if err != nil {
+			err = database.ParseError(err)
+			return
+		}
+	} else {
+		count, err = coll.CountDocuments(db, query)
+		if err != nil {
+			err = database.ParseError(err)
+			return
+		}
+	}
+
+	maxPage := count / pageCount
+	if count == pageCount {
+		maxPage = 0
+	}
+	page = utils.Min64(page, maxPage)
+	skip := utils.Min64(page*pageCount, count)
+
+	cursor, err := coll.Find(
+		db,
+		query,
+		&options.FindOptions{
+			Sort: &bson.D{
+				{"name", 1},
+			},
+			Skip:  &skip,
+			Limit: &pageCount,
+		},
+	)
+	if err != nil {
+		err = database.ParseError(err)
+		return
+	}
+	defer cursor.Close(db)
+
+	for cursor.Next(db) {
+		authority := &Authority{}
+		err = cursor.Decode(authority)
+		if err != nil {
+			err = database.ParseError(err)
+			return
+		}
+
+		authorities = append(authorities, authority)
+	}
+
+	err = cursor.Err()
+	if err != nil {
+		err = database.ParseError(err)
+		return
+	}
+
+	return
+}
+
 func GetTokens(db *database.Database, tokens []string) (
 	authrs []*Authority, err error) {
 
@@ -542,6 +607,23 @@ func Remove(db *database.Database, authrId primitive.ObjectID) (
 		default:
 			return
 		}
+	}
+
+	return
+}
+
+func RemoveMulti(db *database.Database, authorityIds []primitive.ObjectID) (
+	err error) {
+	coll := db.Authorities()
+
+	_, err = coll.DeleteMany(db, &bson.M{
+		"_id": &bson.M{
+			"$in": authorityIds,
+		},
+	})
+	if err != nil {
+		err = database.ParseError(err)
+		return
 	}
 
 	return
