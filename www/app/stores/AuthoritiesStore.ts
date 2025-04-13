c1984b7b8ee8c010bdf007b0f69fd5881bb591d7
Zachary Huff <zach.huff.386@gmail.com>
2025-04-13 17:27:46 -0400

    Add paging to authorities store

--- www/app/stores/AuthoritiesStore.ts
+++ www/app/stores/AuthoritiesStore.ts
@@ -7,6 +7,10 @@ import * as GlobalTypes from '../types/GlobalTypes';
 class AuthoritiesStore extends EventEmitter {
 	_authorities: AuthorityTypes.AuthoritiesRo = Object.freeze([]);
 	_secrets: {[key: string]: string} = {};
+	_page: number;
+	_pageCount: number;
+	_filter: AuthorityTypes.Filter = null;
+	_count: number;
 	_map: {[key: string]: number} = {};
 	_token = Dispatcher.register((this._callback).bind(this));
 
@@ -16,13 +20,32 @@ class AuthoritiesStore extends EventEmitter {
 
 	get authoritiesM(): AuthorityTypes.Authorities {
 		let authorities: AuthorityTypes.Authorities = [];
-		this._authorities.forEach((
-				policy: AuthorityTypes.AuthorityRo): void => {
+		this._authorities.forEach((authority: AuthorityTypes.AuthorityRo): void => {
 			authorities.push({
-				...policy,
+				...authority,
 			});
 		});
 		return authorities;
+	}
+
+	get page(): number {
+		return this._page || 0;
+	}
+
+	get pageCount(): number {
+		return this._pageCount || 20;
+	}
+
+	get pages(): number {
+		return Math.ceil(this.count / this.pageCount);
+	}
+
+	get filter(): AuthorityTypes.Filter {
+		return this._filter;
+	}
+
+	get count(): number {
+		return this._count || 0;
 	}
 
 	authority(id: string): AuthorityTypes.AuthorityRo {
@@ -49,14 +72,33 @@ class AuthoritiesStore extends EventEmitter {
 		this.removeListener(GlobalTypes.CHANGE, callback);
 	}
 
-	_sync(authorities: AuthorityTypes.Authority[]): void {
+	_traverse(page: number): void {
+		this._page = Math.min(this.pages, page);
+	}
+
+	_filterCallback(filter: AuthorityTypes.Filter): void {
+		if ((this._filter !== null && filter === null) ||
+			(!Object.keys(this._filter || {}).length && filter !== null) || (
+				filter && this._filter && (
+					filter.name !== this._filter.name
+				))) {
+			this._traverse(0);
+		}
+		this._filter = filter;
+		this.emitChange();
+	}
+
+	_sync(authorities: AuthorityTypes.Authority[], count: number): void {
 		this._map = {};
 		for (let i = 0; i < authorities.length; i++) {
 			authorities[i] = Object.freeze(authorities[i]);
 			this._map[authorities[i].id] = i;
 		}
 
+		this._count = count;
 		this._authorities = Object.freeze(authorities);
+		this._page = Math.min(this.pages, this.page);
+
 		this.emitChange();
 	}
 
@@ -71,9 +113,18 @@ class AuthoritiesStore extends EventEmitter {
 
 	_callback(action: AuthorityTypes.AuthorityDispatch): void {
 		switch (action.type) {
-			case AuthorityTypes.SYNC:
-				this._sync(action.data.authorities);
+			case AuthorityTypes.TRAVERSE:
+				this._traverse(action.data.page);
 				break;
+
+			case AuthorityTypes.FILTER:
+				this._filterCallback(action.data.filter);
+				break;
+
+			case AuthorityTypes.SYNC:
+				this._sync(action.data.authorities, action.data.count);
+				break;
+
 			case AuthorityTypes.SYNC_SECRET:
 				this._syncSecret(action.data.id, action.data.secret);
 				break;
