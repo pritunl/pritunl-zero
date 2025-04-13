4123e72f768b7f79f171877db21285916ec224e9
Zachary Huff <zach.huff.386@gmail.com>
2025-04-13 17:30:17 -0400

    Add paging to secrets component

--- www/app/components/Secrets.tsx
+++ www/app/components/Secrets.tsx
@@ -3,17 +3,49 @@ import * as React from 'react';
 import * as SecretTypes from '../types/SecretTypes';
 import SecretsStore from '../stores/SecretsStore';
 import * as SecretActions from '../actions/SecretActions';
-import NonState from './NonState';
 import Secret from './Secret';
+import SecretNew from './SecretNew';
+import SecretsFilter from './SecretsFilter';
+import SecretsPage from './SecretsPage';
 import Page from './Page';
 import PageHeader from './PageHeader';
+import NonState from './NonState';
+import ConfirmButton from './ConfirmButton';
+
+interface Selected {
+	[key: string]: boolean;
+}
+
+interface Opened {
+	[key: string]: boolean;
+}
 
 interface State {
 	secrets: SecretTypes.SecretsRo;
+	filter: SecretTypes.Filter;
+	selected: Selected;
+	opened: Opened;
+	newOpened: boolean;
+	lastSelected: string;
 	disabled: boolean;
 }
 
 const css = {
+	items: {
+		width: '100%',
+		marginTop: '-5px',
+		display: 'table',
+		tableLayout: 'fixed',
+		borderSpacing: '0 5px',
+	} as React.CSSProperties,
+	itemsBox: {
+		width: '100%',
+		overflowY: 'auto',
+	} as React.CSSProperties,
+	placeholder: {
+		opacity: 0,
+		width: '100%',
+	} as React.CSSProperties,
 	header: {
 		marginTop: '-19px',
 	} as React.CSSProperties,
@@ -26,9 +58,6 @@ const css = {
 	buttons: {
 		marginTop: '8px',
 	} as React.CSSProperties,
-	noCerts: {
-		height: 'auto',
-	} as React.CSSProperties,
 };
 
 export default class Secrets extends React.Component<{}, State> {
@@ -36,8 +65,21 @@ export default class Secrets extends React.Component<{}, State> {
 		super(props, context);
 		this.state = {
 			secrets: SecretsStore.secrets,
+			filter: SecretsStore.filter,
+			selected: {},
+			opened: {},
+			newOpened: false,
+			lastSelected: null,
 			disabled: false,
 		};
+	}
+
+	get selected(): boolean {
+		return !!Object.keys(this.state.selected).length;
+	}
+
+	get opened(): boolean {
+		return !!Object.keys(this.state.opened).length;
 	}
 
 	componentDidMount(): void {
@@ -50,22 +92,156 @@ export default class Secrets extends React.Component<{}, State> {
 	}
 
 	onChange = (): void => {
+		let secrets = SecretsStore.secrets;
+		let selected: Selected = {};
+		let curSelected = this.state.selected;
+		let opened: Opened = {};
+		let curOpened = this.state.opened;
+
+		secrets.forEach((secret: SecretTypes.Secret): void => {
+			if (curSelected[secret.id]) {
+				selected[secret.id] = true;
+			}
+			if (curOpened[secret.id]) {
+				opened[secret.id] = true;
+			}
+		});
+
 		this.setState({
 			...this.state,
-			secrets: SecretsStore.secrets,
+			secrets: secrets,
+			filter: SecretsStore.filter,
+			selected: selected,
+			opened: opened,
+		});
+	}
+
+	onDelete = (): void => {
+		this.setState({
+			...this.state,
+			disabled: true,
+		});
+		SecretActions.removeMulti(
+				Object.keys(this.state.selected)).then((): void => {
+			this.setState({
+				...this.state,
+				selected: {},
+				disabled: false,
+			});
+		}).catch((): void => {
+			this.setState({
+				...this.state,
+				disabled: false,
+			});
 		});
 	}
 
 	render(): JSX.Element {
-		let certsDom: JSX.Element[] = [];
+		let secretsDom: JSX.Element[] = [];
 
 		this.state.secrets.forEach((
-				cert: SecretTypes.SecretRo): void => {
-			certsDom.push(<Secret
-				key={cert.id}
-				secret={cert}
+				secret: SecretTypes.SecretRo): void => {
+			secretsDom.push(<Secret
+				key={secret.id}
+				secret={secret}
+				selected={!!this.state.selected[secret.id]}
+				open={!!this.state.opened[secret.id]}
+				onSelect={(shift: boolean): void => {
+					let selected = {
+						...this.state.selected,
+					};
+
+					if (shift) {
+						let secrets = this.state.secrets;
+						let start: number;
+						let end: number;
+
+						for (let i = 0; i < secrets.length; i++) {
+							let usr = secrets[i];
+
+							if (usr.id === secret.id) {
+								start = i;
+							} else if (usr.id === this.state.lastSelected) {
+								end = i;
+							}
+						}
+
+						if (start !== undefined && end !== undefined) {
+							if (start > end) {
+								end = [start, start = end][0];
+							}
+
+							for (let i = start; i <= end; i++) {
+								selected[secrets[i].id] = true;
+							}
+
+							this.setState({
+								...this.state,
+								lastSelected: secret.id,
+								selected: selected,
+							});
+
+							return;
+						}
+					}
+
+					if (selected[secret.id]) {
+						delete selected[secret.id];
+					} else {
+						selected[secret.id] = true;
+					}
+
+					this.setState({
+						...this.state,
+						lastSelected: secret.id,
+						selected: selected,
+					});
+				}}
+				onOpen={(): void => {
+					let opened = {
+						...this.state.opened,
+					};
+
+					if (opened[secret.id]) {
+						delete opened[secret.id];
+					} else {
+						opened[secret.id] = true;
+					}
+
+					this.setState({
+						...this.state,
+						opened: opened,
+					});
+				}}
 			/>);
 		});
+
+		let filterClass = 'bp5-button bp5-intent-primary bp5-icon-filter ';
+		if (this.state.filter) {
+			filterClass += 'bp5-active';
+		}
+
+		let selectedNames: string[] = [];
+		for (let instId of Object.keys(this.state.selected)) {
+			let inst = SecretsStore.secret(instId);
+			if (inst) {
+				selectedNames.push(inst.name || instId);
+			} else {
+				selectedNames.push(instId);
+			}
+		}
+
+		let newSecretDom: JSX.Element;
+		if (this.state.newOpened) {
+			newSecretDom = <SecretNew
+				onClose={(): void => {
+					this.setState({
+						...this.state,
+						newOpened: false,
+					});
+				}}
+			/>;
+		}
 
 		return <Page>
 			<PageHeader>
@@ -74,39 +250,89 @@ export default class Secrets extends React.Component<{}, State> {
 					<div className="flex"/>
 					<div style={css.buttons}>
 						<button
-							className="bp5-button bp5-intent-success bp5-icon-add"
+							className={filterClass}
 							style={css.button}
-							disabled={this.state.disabled}
+							type="button"
+							onClick={(): void => {
+								if (this.state.filter === null) {
+									SecretActions.filter({});
+								} else {
+									SecretActions.filter(null);
+								}
+							}}
+						>
+							Filters
+						</button>
+						<button
+							className="bp5-button bp5-intent-warning bp5-icon-chevron-up"
+							style={css.button}
+							disabled={!this.opened}
 							type="button"
 							onClick={(): void => {
 								this.setState({
 									...this.state,
-									disabled: true,
+									opened: {},
 								});
-								SecretActions.create(null).then((): void => {
-									this.setState({
-										...this.state,
-										disabled: false,
-									});
-								}).catch((): void => {
-									this.setState({
-										...this.state,
-										disabled: false,
-									});
+							}}
+						>
+							Collapse All
+						</button>
+						<ConfirmButton
+							label="Delete Selected"
+							className="bp5-intent-danger bp5-icon-delete"
+							progressClassName="bp5-intent-danger"
+							safe={true}
+							style={css.button}
+							confirmMsg="Permanently delete the selected secrets"
+							confirmInput={true}
+							items={selectedNames}
+							disabled={!this.selected || this.state.disabled}
+							onConfirm={this.onDelete}
+						/>
+						<button
+							className="bp5-button bp5-intent-success bp5-icon-add"
+							style={css.button}
+							disabled={this.state.disabled || this.state.newOpened}
+							type="button"
+							onClick={(): void => {
+								this.setState({
+									...this.state,
+									newOpened: true,
 								});
 							}}
 						>New</button>
 					</div>
 				</div>
 			</PageHeader>
-			<div>
-				{certsDom}
+			<SecretsFilter
+				filter={this.state.filter}
+				onFilter={(filter): void => {
+					SecretActions.filter(filter);
+				}}
+			/>
+			<div style={css.itemsBox}>
+				<div style={css.items}>
+					{newSecretDom}
+					{secretsDom}
+					<tr className="bp5-card bp5-row" style={css.placeholder}>
+						<td colSpan={2} style={css.placeholder}/>
+					</tr>
+				</div>
 			</div>
 			<NonState
-				hidden={!!certsDom.length}
-				iconClass="bp5-icon-key"
+				hidden={!!secretsDom.length}
+				iconClass="bp5-icon-ip-address"
 				title="No secrets"
 				description="Add a new secret to get started."
+			/>
+			<SecretsPage
+				onPage={(): void => {
+					this.setState({
+						...this.state,
+						selected: {},
+						lastSelected: null,
+					});
+				}}
 			/>
 		</Page>;
 	}
