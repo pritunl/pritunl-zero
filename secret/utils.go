9f113b3cc15a995e7239aca2ece370c12082d7d5
Zachary Huff <zach.huff.386@gmail.com>
2025-04-15 19:16:20 -0400

    Add get all paged to secret utils

--- secret/utils.go
+++ secret/utils.go
@@ -11,8 +11,10 @@ import (
 	"github.com/dropbox/godropbox/errors"
 	"github.com/pritunl/mongo-go-driver/bson"
 	"github.com/pritunl/mongo-go-driver/bson/primitive"
+	"github.com/pritunl/mongo-go-driver/mongo/options"
 	"github.com/pritunl/pritunl-zero/database"
 	"github.com/pritunl/pritunl-zero/errortypes"
+	"github.com/pritunl/pritunl-zero/utils"
 )
 
 func Get(db *database.Database, secrId primitive.ObjectID) (
@@ -63,6 +65,70 @@ func GetAll(db *database.Database) (secrs []*Secret, err error) {
 		}
 
 		secrs = append(secrs, secr)
+	}
+
+	err = cursor.Err()
+	if err != nil {
+		err = database.ParseError(err)
+		return
+	}
+
+	return
+}
+
+func GetAllPaged(db *database.Database, query *bson.M,
+	page, pageCount int64) (secrets []*Secret, count int64, err error) {
+
+	coll := db.Secrets()
+	secrets = []*Secret{}
+
+	if len(*query) == 0 {
+		count, err = coll.EstimatedDocumentCount(db)
+		if err != nil {
+			err = database.ParseError(err)
+			return
+		}
+	} else {
+		count, err = coll.CountDocuments(db, query)
+		if err != nil {
+			err = database.ParseError(err)
+			return
+		}
+	}
+
+	maxPage := count / pageCount
+	if count == pageCount {
+		maxPage = 0
+	}
+	page = utils.Min64(page, maxPage)
+	skip := utils.Min64(page*pageCount, count)
+
+	cursor, err := coll.Find(
+		db,
+		query,
+		&options.FindOptions{
+			Sort: &bson.D{
+				{"name", 1},
+			},
+			Skip:  &skip,
+			Limit: &pageCount,
+		},
+	)
+	if err != nil {
+		err = database.ParseError(err)
+		return
+	}
+	defer cursor.Close(db)
+
+	for cursor.Next(db) {
+		secret := &Secret{}
+		err = cursor.Decode(secret)
+		if err != nil {
+			err = database.ParseError(err)
+			return
+		}
+
+		secrets = append(secrets, secret)
 	}
 
 	err = cursor.Err()
