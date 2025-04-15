58a90ed2c50ea39e1f31854e6df81ff276ce6657
Zachary Huff <zach.huff.386@gmail.com>
2025-04-15 19:15:56 -0400

    Add get all paged to policy utils

--- policy/utils.go
+++ policy/utils.go
@@ -185,6 +185,70 @@ func GetAll(db *database.Database) (policies []*Policy, err error) {
 	return
 }
 
+func GetAllPaged(db *database.Database, query *bson.M,
+	page, pageCount int64) (policies []*Policy, count int64, err error) {
+
+	coll := db.Policies()
+	policies = []*Policy{}
+
+	if len(*query) == 0 {
+		count, err = coll.EstimatedDocumentCount(db)
+		if err != nil {
+			err = database.ParseError(err)
+			return
+		}
+	} else {
+		count, err = coll.CountDocuments(db, query)
+		if err != nil {
+			err = database.ParseError(err)
+			return
+		}
+	}
+
+	maxPage := count / pageCount
+	if count == pageCount {
+		maxPage = 0
+	}
+	page = utils.Min64(page, maxPage)
+	skip := utils.Min64(page*pageCount, count)
+
+	cursor, err := coll.Find(
+		db,
+		query,
+		&options.FindOptions{
+			Sort: &bson.D{
+				{"name", 1},
+			},
+			Skip:  &skip,
+			Limit: &pageCount,
+		},
+	)
+	if err != nil {
+		err = database.ParseError(err)
+		return
+	}
+	defer cursor.Close(db)
+
+	for cursor.Next(db) {
+		policy := &Policy{}
+		err = cursor.Decode(policy)
+		if err != nil {
+			err = database.ParseError(err)
+			return
+		}
+
+		policies = append(policies, policy)
+	}
+
+	err = cursor.Err()
+	if err != nil {
+		err = database.ParseError(err)
+		return
+	}
+
+	return
+}
+
 func Remove(db *database.Database, policyId primitive.ObjectID) (err error) {
 	coll := db.Policies()
 
