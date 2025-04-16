018cba77a9b76ecc150a9ca7ba89de90398126f4
Zachary Huff <zach.huff.386@gmail.com>
2025-04-16 15:26:15 -0400

    Add min max 64 to math utils

--- utils/math.go
+++ utils/math.go
@@ -4,6 +4,20 @@ import (
 	"math"
 )
 
+func Max64(x, y int64) int64 {
+	if x > y {
+		return x
+	}
+	return y
+}
+
+func Min64(x, y int64) int64 {
+	if x < y {
+		return x
+	}
+	return y
+}
+
 func ToFixed(x float64, p int) float64 {
 	y := math.Pow(10, float64(p))
 	return float64(int(x*y+math.Copysign(0.5, x*y))) / y
