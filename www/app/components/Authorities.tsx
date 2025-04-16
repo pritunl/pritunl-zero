6436e70e41f59f48a29700fb4bf1356bd93e4554
Zachary Huff <zach.huff.386@gmail.com>
2025-04-16 15:27:03 -0400

    Add paging to authorities component

--- www/app/components/Authorities.tsx
+++ www/app/components/Authorities.tsx
@@ -6,19 +6,50 @@ import * as AuthorityActions from '../actions/AuthorityActions';
 import NodesStore from "../stores/NodesStore";
 import * as NodeActions from "../actions/NodeActions";
 import * as NodeTypes from "../types/NodeTypes";
-import NonState from './NonState';
 import Authority from './Authority';
+import AuthorityNew from './AuthorityNew';
+import AuthoritiesFilter from './AuthoritiesFilter';
+import AuthoritiesPage from './AuthoritiesPage';
 import Page from './Page';
 import PageHeader from './PageHeader';
+import NonState from './NonState';
+import ConfirmButton from './ConfirmButton';
+
+interface Selected {
+	[key: string]: boolean;
+}
+
+interface Opened {
+	[key: string]: boolean;
+}
 
 interface State {
 	authorities: AuthorityTypes.AuthoritiesRo;
 	nodes: NodeTypes.NodesRo;
+	filter: AuthorityTypes.Filter;
+	selected: Selected;
+	opened: Opened;
+	newOpened: boolean;
+	lastSelected: string;
 	disabled: boolean;
-	algorithm: string;
 }
 
 const css = {
+	items: {
+		width: '100%',
+		marginTop: '-5px',
+		display: 'table',
+		tableLayout: 'fixed',
+		borderSpacing: '0 5px',
+	} as React.CSSProperties,
+	itemsBox: {
+		width: '100%',
+		overflowY: 'auto',
+	} as React.CSSProperties,
+	placeholder: {
+		opacity: 0,
+		width: '100%',
+	} as React.CSSProperties,
 	header: {
 		marginTop: '-19px',
 	} as React.CSSProperties,
@@ -26,28 +57,10 @@ const css = {
 		margin: '19px 0 0 0',
 	} as React.CSSProperties,
 	button: {
-		margin: '8px 0 0 0',
+		margin: '8px 0 0 8px',
 	} as React.CSSProperties,
 	buttons: {
 		marginTop: '8px',
-	} as React.CSSProperties,
-	group: {
-		width: '100%',
-	} as React.CSSProperties,
-	select: {
-		width: '100%',
-	} as React.CSSProperties,
-	selectFirst: {
-		width: '100%',
-		borderTopLeftRadius: '3px',
-		borderBottomLeftRadius: '3px',
-	} as React.CSSProperties,
-	selectInner: {
-		width: '100%',
-	} as React.CSSProperties,
-	selectBox: {
-		margin: '8px 0 0 8px',
-		flex: '1',
 	} as React.CSSProperties,
 };
 
@@ -57,9 +70,21 @@ export default class Authorities extends React.Component<{}, State> {
 		this.state = {
 			authorities: AuthoritiesStore.authorities,
 			nodes: NodesStore.nodes,
+			filter: AuthoritiesStore.filter,
+			selected: {},
+			opened: {},
+			newOpened: false,
+			lastSelected: null,
 			disabled: false,
-			algorithm: '',
 		};
+	}
+
+	get selected(): boolean {
+		return !!Object.keys(this.state.selected).length;
+	}
+
+	get opened(): boolean {
+		return !!Object.keys(this.state.opened).length;
 	}
 
 	componentDidMount(): void {
@@ -74,11 +99,50 @@ export default class Authorities extends React.Component<{}, State> {
 		NodesStore.removeChangeListener(this.onChange);
 	}
 
+
 	onChange = (): void => {
+		let authorities = AuthoritiesStore.authorities;
+		let selected: Selected = {};
+		let curSelected = this.state.selected;
+		let opened: Opened = {};
+		let curOpened = this.state.opened;
+
+		authorities.forEach((authority: AuthorityTypes.Authority): void => {
+			if (curSelected[authority.id]) {
+				selected[authority.id] = true;
+			}
+			if (curOpened[authority.id]) {
+				opened[authority.id] = true;
+			}
+		});
+
 		this.setState({
 			...this.state,
-			authorities: AuthoritiesStore.authorities,
+			authorities: authorities,
 			nodes: NodesStore.nodes,
+			filter: AuthoritiesStore.filter,
+			selected: selected,
+			opened: opened,
+		});
+	}
+
+	onDelete = (): void => {
+		this.setState({
+			...this.state,
+			disabled: true,
+		});
+		AuthorityActions.removeMulti(
+				Object.keys(this.state.selected)).then((): void => {
+			this.setState({
+				...this.state,
+				selected: {},
+				disabled: false,
+			});
+		}).catch((): void => {
+			this.setState({
+				...this.state,
+				disabled: false,
+			});
 		});
 	}
 
@@ -89,10 +153,107 @@ export default class Authorities extends React.Component<{}, State> {
 				authority: AuthorityTypes.AuthorityRo): void => {
 			authoritiesDom.push(<Authority
 				key={authority.id}
-				nodes={this.state.nodes}
 				authority={authority}
+				nodes={this.state.nodes}
+				selected={!!this.state.selected[authority.id]}
+				open={!!this.state.opened[authority.id]}
+				onSelect={(shift: boolean): void => {
+					let selected = {
+						...this.state.selected,
+					};
+
+					if (shift) {
+						let authorities = this.state.authorities;
+						let start: number;
+						let end: number;
+
+						for (let i = 0; i < authorities.length; i++) {
+							let usr = authorities[i];
+
+							if (usr.id === authority.id) {
+								start = i;
+							} else if (usr.id === this.state.lastSelected) {
+								end = i;
+							}
+						}
+
+						if (start !== undefined && end !== undefined) {
+							if (start > end) {
+								end = [start, start = end][0];
+							}
+
+							for (let i = start; i <= end; i++) {
+								selected[authorities[i].id] = true;
+							}
+
+							this.setState({
+								...this.state,
+								lastSelected: authority.id,
+								selected: selected,
+							});
+
+							return;
+						}
+					}
+
+					if (selected[authority.id]) {
+						delete selected[authority.id];
+					} else {
+						selected[authority.id] = true;
+					}
+
+					this.setState({
+						...this.state,
+						lastSelected: authority.id,
+						selected: selected,
+					});
+				}}
+				onOpen={(): void => {
+					let opened = {
+						...this.state.opened,
+					};
+
+					if (opened[authority.id]) {
+						delete opened[authority.id];
+					} else {
+						opened[authority.id] = true;
+					}
+
+					this.setState({
+						...this.state,
+						opened: opened,
+					});
+				}}
 			/>);
 		});
+
+		let filterClass = 'bp5-button bp5-intent-primary bp5-icon-filter ';
+		if (this.state.filter) {
+			filterClass += 'bp5-active';
+		}
+
+		let selectedNames: string[] = [];
+		for (let instId of Object.keys(this.state.selected)) {
+			let inst = AuthoritiesStore.authority(instId);
+			if (inst) {
+				selectedNames.push(inst.name || instId);
+			} else {
+				selectedNames.push(instId);
+			}
+		}
+
+		let newAuthorityDom: JSX.Element;
+		if (this.state.newOpened) {
+			newAuthorityDom = <AuthorityNew
+				nodes={this.state.nodes}
+				onClose={(): void => {
+					this.setState({
+						...this.state,
+						newOpened: false,
+					});
+				}}
+			/>;
+		}
 
 		return <Page>
 			<PageHeader>
@@ -100,71 +261,90 @@ export default class Authorities extends React.Component<{}, State> {
 					<h2 style={css.heading}>Authorities</h2>
 					<div className="flex"/>
 					<div style={css.buttons}>
-						<div
-							className="bp5-control-group"
-							style={css.group}
+						<button
+							className={filterClass}
+							style={css.button}
+							type="button"
+							onClick={(): void => {
+								if (this.state.filter === null) {
+									AuthorityActions.filter({});
+								} else {
+									AuthorityActions.filter(null);
+								}
+							}}
 						>
-							<div style={css.selectBox}>
-								<div className="bp5-select" style={css.selectFirst}>
-									<select
-										style={css.selectInner}
-										value={this.state.algorithm}
-										onChange={(evt): void => {
-											this.setState({
-												...this.state,
-												algorithm: evt.target.value,
-											});
-										}}
-									>
-										<option
-											key="ecp384"
-											value="ecp384"
-										>EC P384</option>
-										<option
-											key="rsa4096"
-											value="rsa4096"
-										>RSA 4096</option>
-									</select>
-								</div>
-							</div>
-							<button
-								className="bp5-button bp5-intent-success bp5-icon-add"
-								style={css.button}
-								disabled={this.state.disabled}
-								type="button"
-								onClick={(): void => {
-									this.setState({
-										...this.state,
-										disabled: true,
-									});
-									AuthorityActions.create({
-										id: null,
-										algorithm: this.state.algorithm || 'ecp384',
-									}).then((): void => {
-										this.setState({
-											...this.state,
-											disabled: false,
-										});
-									}).catch((): void => {
-										this.setState({
-											...this.state,
-											disabled: false,
-										});
-									});
-								}}
-							>New</button>
-						</div>
+							Filters
+						</button>
+						<button
+							className="bp5-button bp5-intent-warning bp5-icon-chevron-up"
+							style={css.button}
+							disabled={!this.opened}
+							type="button"
+							onClick={(): void => {
+								this.setState({
+									...this.state,
+									opened: {},
+								});
+							}}
+						>
+							Collapse All
+						</button>
+						<ConfirmButton
+							label="Delete Selected"
+							className="bp5-intent-danger bp5-icon-delete"
+							progressClassName="bp5-intent-danger"
+							safe={true}
+							style={css.button}
+							confirmMsg="Permanently delete the selected authorities"
+							confirmInput={true}
+							items={selectedNames}
+							disabled={!this.selected || this.state.disabled}
+							onConfirm={this.onDelete}
+						/>
+						<button
+							className="bp5-button bp5-intent-success bp5-icon-add"
+							style={css.button}
+							disabled={this.state.disabled || this.state.newOpened}
+							type="button"
+							onClick={(): void => {
+								this.setState({
+									...this.state,
+									newOpened: true,
+								});
+							}}
+						>New</button>
 					</div>
 				</div>
 			</PageHeader>
-			<div>
-				{authoritiesDom}
+			<AuthoritiesFilter
+				filter={this.state.filter}
+				onFilter={(filter): void => {
+					AuthorityActions.filter(filter);
+				}}
+			/>
+			<div style={css.itemsBox}>
+				<div style={css.items}>
+					{newAuthorityDom}
+					{authoritiesDom}
+					<tr className="bp5-card bp5-row" style={css.placeholder}>
+						<td colSpan={2} style={css.placeholder}/>
+					</tr>
+				</div>
 			</div>
 			<NonState
 				hidden={!!authoritiesDom.length}
-				iconClass="bp5-icon-office"
+				iconClass="bp5-icon-ip-address"
 				title="No authorities"
 				description="Add a new authority to get started."
+			/>
+			<AuthoritiesPage
+				onPage={(): void => {
+					this.setState({
+						...this.state,
+						selected: {},
+						lastSelected: null,
+					});
+				}}
 			/>
 		</Page>;
 	}
