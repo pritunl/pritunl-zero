71fc8ba4115abcfe58d12efde70796c7fd4d17a7
Zachary Huff <zach.huff.386@gmail.com>
2025-04-17 17:38:35 -0400

    Add alert new to alerts component

--- www/app/components/Alerts.tsx
+++ www/app/components/Alerts.tsx
@@ -7,6 +7,7 @@ import AuthoritiesStore from '../stores/AuthoritiesStore';
 import * as AlertActions from '../actions/AlertActions';
 import * as AuthorityActions from '../actions/AuthorityActions';
 import Alert from './Alert';
+import AlertNew from './AlertNew';
 import AlertsFilter from './AlertsFilter';
 import AlertsPage from './AlertsPage';
 import Page from './Page';
@@ -38,6 +39,7 @@ const css = {
 		width: '100%',
 		marginTop: '-5px',
 		display: 'table',
+		tableLayout: 'fixed',
 		borderSpacing: '0 5px',
 	} as React.CSSProperties,
 	itemsBox: {
@@ -239,6 +241,19 @@ export default class Alerts extends React.Component<{}, State> {
 			}
 		}
 
+		let newAlertDom: JSX.Element;
+		if (this.state.newOpened) {
+			newAlertDom = <AlertNew
+				authorities={this.state.authorities}
+				onClose={(): void => {
+					this.setState({
+						...this.state,
+						newOpened: false,
+					});
+				}}
+			/>;
+		}
+
 		return <Page>
 			<PageHeader>
 				<div className="layout horizontal wrap" style={css.header}>
@@ -288,25 +303,12 @@ export default class Alerts extends React.Component<{}, State> {
 						<button
 							className="bp5-button bp5-intent-success bp5-icon-add"
 							style={css.button}
-							disabled={this.state.disabled}
+							disabled={this.state.disabled || this.state.newOpened}
 							type="button"
 							onClick={(): void => {
 								this.setState({
 									...this.state,
-									disabled: true,
-								});
-								AlertActions.create({
-									id: null,
-								}).then((): void => {
-									this.setState({
-										...this.state,
-										disabled: false,
-									});
-								}).catch((): void => {
-									this.setState({
-										...this.state,
-										disabled: false,
-									});
+									newOpened: true,
 								});
 							}}
 						>New</button>
@@ -322,9 +324,10 @@ export default class Alerts extends React.Component<{}, State> {
 			/>
 			<div style={css.itemsBox}>
 				<div style={css.items}>
+					{newAlertDom}
 					{alertsDom}
 					<tr className="bp5-card bp5-row" style={css.placeholder}>
-						<td colSpan={5} style={css.placeholder}/>
+						<td colSpan={2} style={css.placeholder}/>
 					</tr>
 				</div>
 			</div>
