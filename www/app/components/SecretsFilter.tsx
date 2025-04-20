f8c29f7326370db12195fa0e959fbbd785631198
Zachary Huff <zach.huff.386@gmail.com>
2025-04-20 18:54:25 -0400

    Add secrets filter component

---
+++ www/app/components/SecretsFilter.tsx
@@ -0,0 +1,88 @@
+/// <reference path="../References.d.ts"/>
+import * as React from 'react';
+import * as Constants from '../Constants';
+import * as SecretTypes from '../types/SecretTypes';
+import SearchInput from './SearchInput';
+
+interface Props {
+	filter: SecretTypes.Filter;
+	onFilter: (filter: SecretTypes.Filter) => void;
+}
+
+const css = {
+	filters: {
+		margin: '-15px 0 5px 0',
+	} as React.CSSProperties,
+	input: {
+		width: '200px',
+		margin: '5px',
+	} as React.CSSProperties,
+	shortInput: {
+		width: '180px',
+		margin: '5px',
+	} as React.CSSProperties,
+	role: {
+		width: '150px',
+		margin: '5px',
+	} as React.CSSProperties,
+	type: {
+		margin: '5px',
+	} as React.CSSProperties,
+	check: {
+		margin: '12px 5px 8px 5px',
+	} as React.CSSProperties,
+};
+
+export default class SecretsFilter extends React.Component<Props, {}> {
+	constructor(props: any, context: any) {
+		super(props, context);
+		this.state = {
+			menu: false,
+		};
+	}
+
+	render(): JSX.Element {
+		if (this.props.filter === null) {
+			return <div/>;
+		}
+
+		return <div className="layout horizontal wrap" style={css.filters}>
+			<SearchInput
+				style={css.input}
+				placeholder="Secret ID"
+				value={this.props.filter.id}
+				onChange={(val: string): void => {
+					let filter = {
+						...this.props.filter,
+					};
+
+					if (val) {
+						filter.id = val;
+					} else {
+						delete filter.id;
+					}
+
+					this.props.onFilter(filter);
+				}}
+			/>
+			<SearchInput
+				style={css.input}
+				placeholder="Name"
+				value={this.props.filter.name}
+				onChange={(val: string): void => {
+					let filter = {
+						...this.props.filter,
+					};
+
+					if (val) {
+						filter.name = val;
+					} else {
+						delete filter.name;
+					}
+
+					this.props.onFilter(filter);
+				}}
+			/>
+		</div>;
+	}
+}
