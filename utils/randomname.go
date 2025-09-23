d0bab84bdf53b46a0a9bf5467ea72476f134f7cd
Zachary Huff <zach.huff.386@gmail.com>
2025-09-23 17:43:46 -0400

    Update random names in utils

--- utils/randomname.go
+++ utils/randomname.go
@@ -6,50 +6,24 @@ import (
 )
 
 var (
-	randOne = []string{
-		"snowy",
-		"restless",
-		"calm",
-		"ancient",
-		"summer",
-		"evening",
-		"guarded",
-		"lively",
-		"thawing",
-		"autumn",
-		"thriving",
-		"patient",
-		"winter",
-		"pleasant",
-		"thundering",
-		"elegant",
-		"narrow",
-		"abundant",
-	}
-	randTwo = []string{
-		"waterfall",
-		"meadow",
-		"skies",
-		"waves",
-		"fields",
-		"stars",
-		"dreams",
-		"refuge",
-		"forest",
-		"plains",
-		"waters",
-		"plateau",
-		"thunder",
-		"volcano",
-		"wilderness",
-		"peaks",
-		"mountains",
-		"vineyards",
+	randElm = []string{
+		"copper",
+		"argon",
+		"xenon",
+		"radon",
+		"cobalt",
+		"nickel",
+		"carbon",
+		"helium",
+		"nitrogen",
+		"radium",
+		"lithium",
+		"silicon",
 	}
 )
 
 func RandName() (name string) {
-	name = fmt.Sprintf("%s-%s-%d", randOne[rand.Intn(18)],
-		randTwo[rand.Intn(18)], rand.Intn(8999)+1000)
+	name = fmt.Sprintf("%s-%d", randElm[rand.Intn(len(randElm))],
+		rand.Intn(8999)+1000)
 	return
 }
