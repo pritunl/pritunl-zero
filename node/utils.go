dc68f2519323aaca08d652bf06174cfe9607fe7e
Zachary Huff <zach.huff.386@gmail.com>
2025-10-30 14:23:06 -0400

    Add default page count to node

--- node/utils.go
+++ node/utils.go
@@ -87,6 +87,9 @@ func GetAllPaged(db *database.Database, query *bson.M,
 		}
 	}
 
+	if pageCount == 0 {
+		pageCount = 20
+	}
 	maxPage := count / pageCount
 	if count == pageCount {
 		maxPage = 0
