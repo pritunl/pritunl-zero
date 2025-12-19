5bdf8f765f272e78c10e565eaa36de69f3c40e6c
Zachary Huff <zach.huff.386@gmail.com>
2025-12-19 13:01:26 -0500

    Use set location in router redirect component

--- www/app/components/RouterRedirect.tsx
+++ www/app/components/RouterRedirect.tsx
@@ -1,5 +1,6 @@
 /// <reference path="../References.d.ts"/>
 import * as React from 'react';
+import * as Router from '../Router';
 
 interface Props {
 	to: string;
@@ -7,9 +8,7 @@ interface Props {
 
 export default class RouterRedirect extends React.Component<Props, {}> {
 	render(): JSX.Element {
-		window.location.hash = this.props.to
-		let evt = new Event("router_update")
-		window.dispatchEvent(evt)
+		Router.setLocation(this.props.to);
 		return <div></div>
 	}
 }
