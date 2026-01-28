84e90de520e161f7ee1c40821c957378bd76850a
Zachary Huff <zach.huff.386@gmail.com>
2026-01-28 13:17:11 -0500

    Add sync interval to app misc utils

--- www/app/utils/MiscUtils.tsx
+++ www/app/utils/MiscUtils.tsx
@@ -1,6 +1,59 @@
 /// <reference path="../References.d.ts"/>
 import React from "react";
 
+export class SyncInterval {
+  private timer: number | null = null;
+  private cancel: boolean = false;
+  private readonly interval: number;
+  private readonly action: () => Promise<any>;
+
+  constructor(action: () => Promise<any>, interval: number) {
+    this.action = action;
+    this.interval = interval;
+		this.start();
+  }
+
+  public start = async (): Promise<void> => {
+    if (this.timer !== null) {
+      clearTimeout(this.timer);
+      this.timer = null;
+    }
+
+    this.cancel = false;
+
+    const runSync = async (): Promise<void> => {
+      if (this.cancel) return;
+
+      try {
+        await this.action();
+
+        if (!this.cancel) {
+          this.timer = window.setTimeout(() => {
+            runSync();
+          }, this.interval);
+        }
+      } catch (error) {
+        console.error("Action error:", error);
+        if (!this.cancel) {
+          this.timer = window.setTimeout(() => {
+            runSync();
+          }, this.interval);
+        }
+      }
+    };
+
+    runSync();
+  };
+
+  public stop = (): void => {
+    this.cancel = true;
+    if (this.timer) {
+      clearTimeout(this.timer);
+      this.timer = null;
+    }
+  };
+}
+
 export function uuid(): string {
 	return (+new Date() + Math.floor(Math.random() * 999999)).toString(36);
 }
