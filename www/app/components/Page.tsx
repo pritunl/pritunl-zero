bc71cc9c766a72d5e0f4884dece78ce62c481796
Zachary Huff <zach.huff.386@gmail.com>
2026-02-06 18:38:56 -0500

    Add full to page component

--- www/app/components/Page.tsx
+++ www/app/components/Page.tsx
@@ -4,6 +4,7 @@ import * as React from 'react';
 interface Props {
 	children?: React.ReactNode
 	wide?: boolean;
+	full?: boolean;
 }
 
 const css = {
@@ -17,13 +18,24 @@ const css = {
 		margin: '0 auto',
 		padding: '30px 20px',
 		minWidth: '200px',
-		maxWidth: '1250px',
+		maxWidth: '1400px',
+	} as React.CSSProperties,
+	pageFull: {
+		height: '100%',
+		display: 'flex',
+		flexDirection: 'column',
+		overflow: 'hidden',
+		margin: '0 auto',
+		padding: '30px 20px 0 20px',
+		minWidth: '200px',
+		maxWidth: '1600px',
 	} as React.CSSProperties,
 };
 
 export default class Page extends React.Component<Props, {}> {
 	render(): JSX.Element {
-		return <div style={this.props.wide ? css.pageWide : css.page}>
+		return <div style={this.props.full ? css.pageFull :
+				(this.props.wide ? css.pageWide : css.page)}>
 			{this.props.children}
 		</div>;
 	}
