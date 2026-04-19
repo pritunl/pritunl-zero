5a721ed4381acf673407838c2e3ee3299f42ecb3
Zachary Huff <zach.huff.386@gmail.com>
2026-04-19 10:28:33 -0400

    Add dynamic to search input component

--- www/app/components/SearchInput.tsx
+++ www/app/components/SearchInput.tsx
@@ -1,5 +1,6 @@
 /// <reference path="../References.d.ts"/>
 import * as React from 'react';
+import * as Blueprint from '@blueprintjs/core';
 
 type OnChange = (val: string) => void;
 
@@ -7,24 +8,71 @@ interface Props {
 	style: React.CSSProperties;
 	placeholder: string;
 	value: string;
+	dynamic?: boolean;
+	exactDefault?: boolean;
 	onChange: OnChange;
 }
 
-export default class SearchInput extends React.Component<Props, {}> {
+interface State {
+	exact: boolean
+}
+
+export default class SearchInput extends React.Component<Props, State> {
+	constructor(props: any, context: any) {
+		super(props, context);
+		this.state = {
+			exact: !!this.props.exactDefault,
+		};
+	}
+
 	render(): JSX.Element {
-		return <div className="bp5-input-group" style={this.props.style}>
-			<span className="bp5-icon bp5-icon-search"/>
-			<input
-				className="bp5-input bp5-round"
+		let val = this.props.value || ""
+
+		if (this.props.dynamic) {
+			if (val.startsWith("~")) {
+				val = val.substring(1)
+			}
+		}
+
+		return <div style={this.props.style}>
+			<Blueprint.InputGroup
 				type="text"
 				autoCapitalize="off"
 				spellCheck={false}
 				placeholder={this.props.placeholder}
-				value={this.props.value || ''}
+				value={val}
 				onChange={(evt): void => {
-					this.props.onChange(evt.target.value);
+					if (this.props.dynamic && !this.state.exact && evt.target.value) {
+						this.props.onChange("~" + evt.target.value)
+					} else {
+						this.props.onChange(evt.target.value)
+					}
 				}}
+				leftElement={<span className="bp5-icon bp5-icon-search"/>}
+				rightElement={<Blueprint.Tooltip
+					content={this.state.exact ? "Exact match" : "Partial match"}
+				>
+					<Blueprint.Button
+						hidden={!this.props.dynamic}
+						icon={this.state.exact ? "link" : "unlink"}
+						onClick={() => {
+							let exact = !this.state.exact
+
+							this.setState({
+								...this.state,
+								exact: exact,
+							})
+
+							if (!exact && val) {
+								this.props.onChange("~" + val)
+							} else {
+								this.props.onChange(val)
+							}
+						}}
+						minimal={true}
+					/>
+				</Blueprint.Tooltip>}
 			/>
-		</div>;
+		</div>
 	}
 }
