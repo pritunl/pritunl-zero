347f69bf9e6be5d80b608a643e4a919da305e71b
Zachary Huff <zach.huff.386@gmail.com>
2026-05-14 12:17:43 -0400

    Update redirect path in phandlers utils

--- phandlers/utils.go
+++ phandlers/utils.go
@@ -4,15 +4,39 @@ import (
 	"net/url"
 
 	"github.com/gin-gonic/gin"
+	"github.com/pritunl/pritunl-zero/service"
 )
 
-func getRedirectPath(redirectUrl string) string {
-	if redirectUrl != "" {
-		parsed, e := url.Parse(redirectUrl)
-		if e == nil {
-			return parsed.Path
+func getRedirectPath(c *gin.Context, srvc *service.Service,
+	redirectUrl string) string {
+
+	host := ""
+	if c.Request.Host != "" {
+		host = c.Request.Host
+	} else if c.Request.URL.Host != "" {
+		host = c.Request.URL.Host
+	}
+
+	redirectHost := ""
+	if host != "" {
+		for _, domain := range srvc.Domains {
+			if domain.Domain == host {
+				redirectHost = domain.Domain
+			}
 		}
 	}
+
+	if redirectHost != "" && redirectUrl != "" {
+		parsed, e := url.Parse(redirectUrl)
+		if e == nil {
+			parsed.Scheme = "https"
+			parsed.Host = redirectHost
+			parsed.User = nil
+			parsed.Opaque = ""
+			return parsed.String()
+		}
+	}
+
 	return ""
 }
 
@@ -20,12 +44,12 @@ type redirectData struct {
 	Redirect string `json:"redirect"`
 }
 
-func redirectQuery(c *gin.Context, query string) {
+func redirectQuery(c *gin.Context, srvc *service.Service, query string) {
 	redirect := ""
 
 	vals, err := url.ParseQuery(query)
 	if err == nil {
-		redirect = getRedirectPath(vals.Get("redirect_url"))
+		redirect = getRedirectPath(c, srvc, vals.Get("redirect_url"))
 	}
 
 	if redirect != "" {
