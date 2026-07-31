3df919663c388553a05a2da8d85403049852ed0d
Zachary Huff <zach.huff.386@gmail.com>
2026-07-31 18:45:11 -0400

    Update decimals in chart types

--- www/app/types/ChartTypes.ts
+++ www/app/types/ChartTypes.ts
@@ -40,7 +40,7 @@ export function getChartLabels(resource: string, data: any): Labels {
 				resource_label: 'Percent',
 				resource_type: 'float',
 				resource_suffix: '%',
-				resource_fixed: 3,
+				resource_fixed: 2,
 				resource_min: 0,
 				resource_max: 100,
 				datasets: [
@@ -63,8 +63,8 @@ export function getChartLabels(resource: string, data: any): Labels {
 				title: 'Load Average',
 				resource_label: 'Load',
 				resource_type: 'float',
-				resource_suffix: '',
-				resource_fixed: 4,
+				resource_suffix: '%',
+				resource_fixed: 2,
 				resource_min: 0,
 				datasets: [
 					{
@@ -92,8 +92,8 @@ export function getChartLabels(resource: string, data: any): Labels {
 				title: 'Disks',
 				resource_label: 'Usage',
 				resource_type: 'float',
-				resource_suffix: '%',
-				resource_fixed: 3,
+				resource_suffix: '',
+				resource_fixed: 2,
 				resource_min: 0,
 				resource_max: 100,
 				hide_zero: true,
